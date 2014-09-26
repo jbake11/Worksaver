32f126ce7fe2913c8139e850b0b0a62387e9d034
jbake11 <jeffbake40@gmail.com>
2014-09-26 17:17:47 -0400

    attempt to fix conflict
    coppied all from master index

--- templates/worksaver/index.php
+++ templates/worksaver/index.php
@@ -14,81 +14,81 @@
         <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome-ie7.min.css" rel="stylesheet">
         <![endif]-->
         <link href="/templates/worksaver/css/main.css" rel="stylesheet">
-        <jdoc:include type="head" />
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div id='header' class="row">
-                <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
-                    <div class="flag">
-                        <img class="img-responsive" src="/templates/worksaver/images/flag.png" alt=""/>
+    <jdoc:include type="head" />
+</head>
+<body>
+    <div class="container-fluid">
+        <div id='header' class="row">
+            <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                <div class="flag">
+                    <img class="img-responsive" src="/templates/worksaver/images/flag.png" alt=""/>
+                </div>
+            </div>
+            <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                <img class="img-responsive" src="/templates/worksaver/images/biglogo.png" alt=""/>
+            </div>
+            <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                <form id="custom-search-form" class="form-search form-horizontal pull-right">
+                    <div class="input-append span12">
+                        <input type="text" class="search-query" placeholder="Search">
+                        <button type="submit" class="btn"><i class="icon-search"></i></button>
                     </div>
-                </div>
-                <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
-                    <img class="img-responsive" src="/templates/worksaver/images/biglogo.png" alt=""/>
-                </div>
-                <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
-                    <form id="custom-search-form" class="form-search form-horizontal pull-right">
-                        <div class="input-append span12">
-                            <input type="text" class="search-query" placeholder="Search">
-                            <button type="submit" class="btn"><i class="icon-search"></i></button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div id='navRow' class="row">
-                <div class='col-lg-12 col-md-12 hidden-sm hidden-xs'>
-                    <div class="navi">
-                        <nav>
-                            <?php if ($this->countModules('mainmenu')) : ?>
-                                <nav class="navigation" role="navigation">
-                                    <jdoc:include type="modules" name="mainmenu" style="none" />
-                                </nav>
-                            <?php endif; ?>
-                        </nav>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
-
-        <div class="container-fluid">
-            <div class="row" id="slideRow">
-                <div class="col-lg-9 col-md-9 col-sm-9 ">
-                    <?php if ($this->countModules('slider')) : ?>
-                        <jdoc:include type="modules" name="slider" style="xhtml" />
-                    <?php endif; ?>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-3">
-                    <a href="index.php" ><img src="/templates/worksaver/images/featured_item.jpg" alt=""/></a>
+        <div id='navRow' class="row">
+            <div class='col-lg-12 col-md-12 hidden-sm hidden-xs'>
+                <div class="navi">
+                    <nav>
+                        <?php if ($this->countModules('mainmenu')) : ?>
+                            <nav class="navigation" role="navigation">
+                                <jdoc:include type="modules" name="mainmenu" style="none" />
+                            </nav>
+                        <?php endif; ?>
+                    </nav>
                 </div>
             </div>
-        </div>
-<<<<<<< HEAD
-        <div class="container">
-            <div class="row">
-
-            </div>
-        </div>
-        <div class="container">
-            <div class="row" id="footer">
-=======
-        <div class="col-lg-3 col-md-3 col-sm-3">
-            <a href="index.php" ><img src="/templates/worksaver/images/featured_item.jpg" alt=""/></a>
         </div>
     </div>
-</div>
-<div class="container">
-    <div class="row">
-        <jdoc:include type="component" />
-    </div>
-</div>
-<div class="container">
-    <div class="row" id="footer">
->>>>>>> origin/master
 
+    <div class="container-fluid">
+        <div class="row" id="slideRow">
+            <div class="col-lg-9 col-md-9 col-sm-9 ">
+                <?php if ($this->countModules('slider')) : ?>
+                    <jdoc:include type="modules" name="slider" style="xhtml" />
+                <?php endif; ?>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <a href="index.php" ><img src="/templates/worksaver/images/featured_item.jpg" alt=""/></a>
             </div>
         </div>
+    </div>
+    <<<<<<< HEAD
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="row" id="footer">
+            =======
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <a href="index.php" ><img src="/templates/worksaver/images/featured_item.jpg" alt=""/></a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <jdoc:include type="component" />
+        </div>
+    </div>
+    <div class="container">
+        <div class="row" id="footer">
+            >>>>>>> origin/master
+
+        </div>
+    </div>
 
 
-    </body>
+</body>
 </html>
---
+++ templates/worksaver/js/slider.js
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+jQuery(document).ready(function ($) {
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length - 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
+});
+
