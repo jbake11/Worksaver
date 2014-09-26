e500744a2b1eabc6985dbae0f247dc8d52f06196
root <app4.biziports.com>
2014-09-26 11:02:19 -0500

    iadding joomlawq git

---
+++ logs/joomla_update.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-09-18 14:32:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-09-18T14:32:45+00:00	INFO	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2014-09-18T14:46:19+00:00	INFO	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2014-09-26T15:15:03+00:00	INFO	update	Update started by user Super User (96). Old version is 3.3.3.
+2014-09-26T15:15:03+00:00	INFO	update	Downloading update file from .
+2014-09-26T15:15:05+00:00	INFO	update	File Joomla_3.3.3_to_3.3.4-Stable-Patch_Package.zip successfully downloaded.
+2014-09-26T15:15:05+00:00	INFO	update	Starting installation of new version.
+2014-09-26T15:15:08+00:00	INFO 199.16.223.2	update	Finalising installation.
+2014-09-26T15:15:09+00:00	INFO 199.16.223.2	update	Ran query from file 3.3.4-2014-08-03. Query text: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2014-09-26T15:15:09+00:00	INFO 199.16.223.2	update	Deleting removed files and folders.
+2014-09-26T15:15:11+00:00	INFO 199.16.223.2	update	Cleaning up after installation.
+2014-09-26T15:15:11+00:00	INFO 199.16.223.2	update	Update to version 3.3.4 is complete.
