99e53c5dd1380e91666420e0fcf0125aec0a4053
jbake11 <jeffbake40@gmail.com>
2014-09-26 10:36:10 -0400

    THERE!!!!!!!!!!!!!!!!!!

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Worksaver</title>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <!-- Le styles -->
+        <link href="http://netdna.bootstrapcdn.com/bootswatch/latest/flatly/bootstrap.min.css" rel="stylesheet">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery-1.8.0.min.js"></script>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+        <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome-ie7.min.css" rel="stylesheet">
+        <![endif]-->
+        <link href="css/main.css" rel="stylesheet">
+        <script type="text/javascript" src="js/slider.js"></script>
+    </head>
+    <body>
+        <div class='container'>
+            <div id='head' class="container-fluid">
+                <div  class="row">
+                    <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                        <div class="flag ">
+                            <img class="img-responsive" src="images/flag.png" alt=""/>
+                        </div>
+                    </div>
+                    <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                        <img class="img-responsive" src="images/biglogo.png" alt=""/>
+                    </div>
+                    <div class='col-lg-4 col-md-4 col-sm-4 hidden-xs'>
+                        <form id="custom-search-form" class="form-search form-horizontal pull-right">
+                            <div class="input-append span12">
+                                <input type="text" class="search-query" placeholder="Search">
+                                <button type="submit" class="btn"><i class="icon-search"></i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class='navRow col-lg-12 col-md-12 hidden-sm hidden-xs'>
+                        <nav>
+                            <ul class="nav nav-pills">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="index.php">Company Profile</a></li>
+                                <li><a href="index.php">Products</a></li>
+                                <li><a href="index.php">Videos</a></li>
+                                <li><a href="index.php">Where to Buy</a></li>
+                                <li><a href="index.php">News & Shows</a></li>
+                                <li><a href="index.php">Manuals & Literature</a></li>
+                                <li><a href="index.php">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="row" id="row2">
+                    <div class="col-lg-8 col-md-8 col-sm-8 ">
+                        <div class="container">
+                            <div id="main_area">
+                                <!-- Slider -->
+                                <div class="row">
+                                    <div class="col-xs-12" id="slider">
+                                        <!-- Top part of the slider -->
+                                        <div class="row">
+                                            <div class="col-sm-8" id="carousel-bounding-box">
+                                                <div class="carousel slide" id="myCarousel">
+                                                    <!-- Carousel items -->
+                                                    <div class="carousel-inner">
+                                                        <div class="active item" data-slide-number="0">
+                                                            <img class="img-responsive" src="images/slider/slider_1_attachment.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="item" data-slide-number="1">
+                                                            <img class="img-responsive" src="images/slider/slider_2_stumpgrinder.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="item" data-slide-number="2">
+                                                            <img class="img-responsive" src="images/slider/slider_3_ATV.jpg" alt=""/>
+                                                        </div>
+                                                        <div class="item" data-slide-number="3">
+                                                            <img class="img-responsive" src="images/slider/slider_4_bailspear.jpg" alt=""/>
+                                                        </div>
+                                                        <!-- Carousel nav -->
+                                                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                                                            <span class="glyphicon glyphicon-chevron-left"></span>
+                                                        </a>
+                                                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                                                            <span class="glyphicon glyphicon-chevron-right"></span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" id="row3">
+                    <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs">
+
+                    </div>
+                    <div class="col-lg-9 col-md-9 col-sm-9">
+                        <div class="container-fluid">
+
+                            <!--Body Content Here-->
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row" id="row4">
+                    <div class="col-lg-12 col-md-12 col-sm-12 contentbottom">
+                        <footer id="footer">
+
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
