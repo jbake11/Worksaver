f6161c13c846dbbd9e5e4db6730a20057392e049
jbake11 <jeffbake40@gmail.com>
2014-09-29 19:44:11 -0400

    asd

--- templates/worksaver/index.php
+++ templates/worksaver/index.php
@@ -52,20 +52,20 @@
     </div>
 
     <?php if ($this->countModules('slider') || $this->countModules('featuredproduct')) : ?>
-    <div class="container-fluid">
-        <div class="row" id="slideRow">
-            <div class="col-lg-9 col-md-9 col-sm-9 ">
-                <?php if ($this->countModules('slider')) : ?>
-                    <jdoc:include type="modules" name="slider" style="xhtml" />
-                <?php endif; ?>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3">
-                <?php if ($this->countModules('featuredproduct')) : ?>
+        <div class="container-fluid">
+            <div class="row-fluid" id="slideRow">
+                <div class="col-lg-9 col-md-9 col-sm-9 ">
+                    <?php if ($this->countModules('slider')) : ?>
+                        <jdoc:include type="modules" name="slider" style="xhtml" />
+                    <?php endif; ?>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <?php if ($this->countModules('featuredproduct')) : ?>
                         <jdoc:include type="modules" name="featuredproduct" style="none" />
-                <?php endif; ?>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
-    </div>
     <?php endif; ?>
 
     <div class="container">
