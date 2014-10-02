fd3a645d5c35231a4e4ba90568d9f756a922ceae
jbake11 <jeffbake40@gmail.com>
2014-10-02 13:47:47 -0400

    images

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,8 +1,64 @@
-<?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+<table align="CENTER" border="0" cellpadding="5" cellspacing="0" width="95%">
+
+    <tbody><tr align="CENTER" valign="TOP"><td align="CENTER" valign="TOP" nowrap=""><font color="#000000" size="+2"><b>Land Management Tools</b></font>
+                <br><b><font size="3">For Front Loader, Skid Steer and Rear Hitch Mounting</font></b></td></tr></tbody></table>
+<p>
+
+    <a name="broom"></a>
+<table align="CENTER" border="1" width="85%">
+    <tbody><tr><td align="CENTER" bgcolor="#80FFFF" colspan="3"><b><font size="5">Rotary Brooms </font></b></td></tr>
+        <tr><td align="CENTER" bgcolor="#80FFFF" width="150"><b><font size="2">Product</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="22%"><b><font size="2">Model</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="95%"><b><font size="2">Description</font></b></td></tr>
+
+        <tr><td width="150"><a href="broom.html"><img src="../landscape/Broom 3.jpg" alt="Photo of Rotary Broom" width="200" length="200"></a></td>
+            <td width="22%"><b><font size="2">RMB-325P<br>RMB-325PW<br>RMB-326P<br>RMB-326PW<br>RMB-327P<br>RMB-327PW</font></b></td>
+            <td width="80%"><font size="2"><br><b>PTO Driven Rotary Brooms</b>&nbsp;&nbsp;&nbsp;PTO driven 3-pt. tractor mount in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.<br>Top quality rotary brooms for use by contractors, municipalities, institutions, and farmers to remove dirt, sand, gravel and trash from construction entrances, parking lots, bridges, driveways and sidewalks.
+
+                <br><br><a href="broompartsman.html"><b>3-PT. Broom Part List</b></a><br><a href="../manuals/broom.pdf"><b>3-PT. Rotary Broom Manuals</b></a><br><br>Click photo for more specs.</font></td></tr>
+
+        <tr><td width="150"><a href="broom.html"><img src="../landscape/Broom lit.jpg" alt="Photo of Rotary Broom" width="200" length="200"></a></td>
+            <td width="22%"><b><font size="2">SSMB-325P<br>SSMB-325PW<br>SSMB-326P<br>SSMB-326PW<br>SSMB-327P<br>SSMB-327PW</font></b></td>
+            <td width="80%"><font size="2"><br><b>Skid Steer Rotary Brooms</b>&nbsp;&nbsp;&nbsp;Hydraulically driven skid steer models in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.<br>Top quality rotary brooms for use by contractors, municipalities, institutions, and farmers to remove dirt, sand, gravel and trash from construction entrances, parking lots, bridges, driveways and sidewalks.
+
+                <br><br><a href="ssbroompartsman.html"><b>Skid Steer Broom Part List</b></a><br><a href="../manuals/SSBroom.pdf"><b>Skid Steer Rotary Broom Manuals</b></a><br><br>Click photo for more specs.</font></td></tr>
+    </tbody></table></p>
+
+<p>
+
+    <a name="snow"></a>
+<table align="CENTER" border="1" width="85%">
+    <tbody><tr><td align="CENTER" bgcolor="#80FFFF" colspan="3"><b><font size="5">Snow Blades</font></b></td></tr>
+        <tr><td align="CENTER" bgcolor="#80FFFF" width="150"><b><font size="2">Product</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="22%"><b><font size="2">Model</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="95%"><b><font size="2">Description</font></b></td></tr>
+
+        <tr><td width="150"><a href="snowblade.html"><img src="../ts/snowblade1.jpg" alt="Photo of Skid Steer Snow Blade" width="200" length="200"><br><img src="../ts/SBFL-2790-21.jpg" alt="Photo of Front Loader Snow Blade" width="200" length="200"></a></td>
+            <td width="22%"><b><font size="2">SBS-2160<br>SBS-2172<br>SBS-2790A<br>SBS-27108A<br><br>SBG-2160<br>SBG-2172<br>SBG-2790A<br>SBG-27108A<br><br>SBFL-2160<br>SBFL-2172<br>SBFL-2790A<br>SBFL-27108A<br></font></b></td>
+            <td width="80%"><font size="2"><br><b>Snow Blades for Skid Steers and Front Loaders.</b><br>Designed for 30-80 hp Tractors and Skid Steers.  All snow blades have trip springs for controlled break over in snow, or lock for light grading of loose dirt or gravel.  All have 3/8" x 6" reversible hard steel cutting edges and height adjustable skid shoes (6" x 1/2" thick) that rotate for long wear.  Manual angle adjstment is standard on 5-ft., 6-ft., and 7.5-ft. models (optional hydraulic. angle kits available.).  9-ft blades have hyddraulic angle adjustment and cross-over relief valve as standard equipment.  7.5 and 9-ft. models ave 5 deg lateral oscilation feature as standard equipment.
+
+                <br><br><br>Click photo for more specs., Manuals and Parts List</font></td></tr>
+    </tbody></table></p>
+
+<p>
+    <a name="stump"></a>
+<table align="CENTER" border="1" width="85%">
+    <tbody><tr><td align="CENTER" bgcolor="#80FFFF" colspan="3"><b><font size="5">Stump Grinder</font></b></td></tr>
+        <tr><td align="CENTER" bgcolor="#80FFFF" width="150"><b><font size="2">Product</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="22%"><b><font size="2">Model</font></b></td>
+            <td align="CENTER" bgcolor="#80FFFF" width="95%"><b><font size="2">Description</font></b></td></tr>
+
+        <tr><td width="150"><a href="stumpgrinder.html"><img src="../ts/sg1.jpg" width="200" height="150" border="0" alt="Photo of Stump Grinder"></a></td>
+            <td width="22%"><b><font size="2">SG-26A<br>SG-36A</font></b></td>
+            <td width="80%"><font size="2"><b>Stump Grinder - </b>Available January 2004 - Economical; Efficent PTO &amp; Hydraulic Powered Stump Grinders make stump removal fast and easy
+                <br><b>Click on picture for more details.<br><br><a href="26stumpman.html">Click Here for SG-26 Parts List</a><br><br><a href="36stumpman.html">Click Here for SG-36 Parts List</a><br><br><a href="26Hydstumpman.html">Click Here for SG-26H Parts List</a>
+                    <br><br><a href="../manuals/wssg26-36.pdf"><b>Download Manual</b></a></b></font></td></tr>
+
+    </tbody></table></p>
+
+<table align="CENTER" valign="TOP" width="75%" border="0">
+    <tbody><tr><td align="CENTER" colspan="3"><a href="bale-options.html"><img border="0" vspace="5" src="../bin/button/lmb.gif" alt="Optional Loader Brackets"></a><br><a href="haf-op.html"><img border="0" vspace="5" src="../bin/button/htlc.gif" alt="Optional Top-Link Cylinders"></a></td></tr>
+    </tbody></table><p></p>
