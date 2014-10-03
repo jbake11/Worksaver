8a2e4dc066c594d3371486f4d655cca44f15825a
jbake11 <jeffbake40@gmail.com>
2014-10-03 00:15:30 -0400

    asd

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -52,43 +52,40 @@
 <p></p>
 <table class='table table-responsive table-bordered table-striped'>
     <tbody><tr><td align="center" colspan="4"><b>SmoothCut Specifications</b></td></tr>
-        <tr><td align="CENTER" width="20%"><b></b></td><td align="CENTER" width="15%"><b>Model SG-26</b></td><td align="CENTER" width="15%"><b>Model SG-36</b></td><td align="CENTER" width="15%"><b>Model SG-26H</b></td></tr>
-        <tr><td align="left" width="20%"><b>Hitch</b></td><td align="CENTER" width="15%"><b>Cat. I</b></td><td align="CENTER" width="15%"><b>Cat. I/II</b></td><td align="CENTER" width="15%"><b>Univ. Skid Steer</b></td></tr>
-        <tr><td align="left" width="20%"><b>Overall Length (est.)</b></td><td align="CENTER" width="15%"><b>75-inches</b></td><td align="CENTER" width="15%"><b>80-inches</b></td><td align="CENTER" width="15%"><b>72-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Drive System</b></td><td align="CENTER" width="15%"><b>Slip Clutch PTO/Series 5</b></td><td align="CENTER" width="15%"><b>Slip Clutch PTO/Series 6</b></td><td align="CENTER" width="15%"><b>System Pressure Hydraulics</b></td></tr>
-        <tr><td align="left" width="20%"><b>PTO speed</b></td><td align="CENTER" width="15%"><b>540 rpm</b></td><td align="CENTER" width="15%"><b>540 rpm</b></td><td align="CENTER" width="15%"><b>540 rpm @ 19.4 gpm</b></td></tr>
-        <tr><td align="left" width="20%"><b>Drive system protection</b></td><td align="CENTER" width="15%"><b>Slip Clutch</b></td><td align="CENTER" width="15%"><b>slip clutch</b></td><td align="CENTER" width="15%"><b>System Pressure</b></td></tr>
-        <tr><td align="left" width="20%"><b>Gearbox rating/ratio</b></td><td align="CENTER" width="15%"><b>60 hp / 1:1.5</b></td><td align="CENTER" width="15%"><b>100 hp / 1:1.46</b></td><td align="CENTER" width="15%"><b>60 hp / 1:1.5</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting wheel construction</b></td><td align="CENTER" width="15%" colspan="3"><b>--Welded 2-piece laminated steel--</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting wheel thickness</b></td><td align="CENTER" width="15%" colspan="3"><font size="+2"><b>----------¾"----------</b></font></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting wheel diameter</b></td><td align="CENTER" width="15%"><b>26-inches</b></td><td align="CENTER" width="15%"><b>36-inches</b></td><td align="CENTER" width="15%"><b>26-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting wheel speed (approx.)<br>@ 540 PTO rpm</b></td><td align="CENTER" width="15%"><b>810 rpm<br>5,515 ft./min</b></td><td align="CENTER" width="15%"><b>788 rpm<br>7,430 ft./min</b></td><td align="CENTER" width="15%"><b>810 rpm<br>5515 ft./min</b></td></tr>
-        <tr><td align="left" width="20%"><b>Number of teeth</b></td><td align="CENTER" width="15%"><b>33</b></td><td align="CENTER" width="15%"><b>42</b></td><td align="CENTER" width="15%"><b>33</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting teeth construction</b></td><td align="CENTER" width="15%" colspan="3"><b>------Carbide Steel Bolt In------</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting height above ground</b></td><td align="CENTER" width="15%"><b>18-inches</b></td><td align="CENTER" width="15%"><b>18-inches</b></td><td align="CENTER" width="15%"><b>18-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Cutting depth below ground</b></td><td align="CENTER" width="15%"><b>10-inches</b></td><td align="CENTER" width="15%"><b>15-inches</b></td><td align="CENTER" width="15%"><b>10-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Max. cut depth per pass</b></td><td align="CENTER" width="15%"><b>5-inches</b></td><td align="CENTER" width="15%"><b>8-inches</b></td><td align="CENTER" width="15%"><b>5-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Maximum horizontal cut</b></td><td align="CENTER" width="15%"><b>28-inches</b></td><td align="CENTER" width="15%"><b>33-inches</b></td><td align="CENTER" width="15%"><b>28-inches</b></td></tr>
-        <tr><td align="left" width="20%"><b>Swing arc</b></td><td align="CENTER" width="15%"><b>45-degrees</b></td><td align="CENTER" width="15%"><b>45-degrees</b></td><td align="CENTER" width="15%"><b>45-degrees</b></td></tr>
-        <tr><td align="left" width="20%"><b>Min. power / flow required</b></td><td align="CENTER" width="15%"><b>22-PTO hp</b></td><td align="CENTER" width="15%"><b>45-PTO hp</b></td><td align="CENTER" width="15%"><b>15 gpm</b></td></tr>
-        <tr><td align="left" width="20%"><b>Max. power / flow</b></td><td align="CENTER" width="15%"><b>50-PTO hp</b></td><td align="CENTER" width="15%"><b>100-PTO hp</b></td><td align="CENTER" width="15%"><b>25 gpm</b></td></tr>
-        <tr><td align="left" width="20%"><b>Drive System</b></td><td align="CENTER" width="15%"><b>PTO Series 5p</b></td><td align="CENTER" width="15%"><b>PTO / Series 6</b></td><td align="CENTER" width="15%"><b>Hydraulic</b></td></tr>
-        <tr><td align="left" width="20%"><b>PTO Speed</b></td><td align="CENTER" width="15%"><b>540 rpm</b></td><td align="CENTER" width="15%"><b>540 rpm</b></td><td align="CENTER" width="15%"><b>540 rpm @ 19.4 gpm</b></td></tr>
-        <tr><td align="left" width="20%"><b>Weight (est.)</b></td><td align="CENTER" width="15%"><b>660-lbs.</b></td><td align="CENTER" width="15%"><b>880-lbs.</b></td><td align="CENTER" width="15%"><b>757 lbs.</b></td></tr>
-        <tr><td align="left" width="20%"><b>Computer product number</b></td><td align="CENTER" width="15%"><b>811500</b></td><td align="CENTER" width="15%"><b>811550</b></td><td align="CENTER" width="15%"><b>811640</b></td></tr>
+        <tr><td><b></b></td><td><b>Model SG-26</b></td><td><b>Model SG-36</b></td><td><b>Model SG-26H</b></td></tr>
+        <tr><td><b>Hitch</b></td><td><b>Cat. I</b></td><td><b>Cat. I/II</b></td><td><b>Univ. Skid Steer</b></td></tr>
+        <tr><td><b>Overall Length (est.)</b></td><td><b>75-inches</b></td><td><b>80-inches</b></td><td><b>72-inches</b></td></tr>
+        <tr><td><b>Drive System</b></td><td><b>Slip Clutch PTO/Series 5</b></td><td><b>Slip Clutch PTO/Series 6</b></td><td><b>System Pressure Hydraulics</b></td></tr>
+        <tr><td><b>PTO speed</b></td><td><b>540 rpm</b></td><td><b>540 rpm</b></td><td><b>540 rpm @ 19.4 gpm</b></td></tr>
+        <tr><td><b>Drive system protection</b></td><td><b>Slip Clutch</b></td><td><b>slip clutch</b></td><td><b>System Pressure</b></td></tr>
+        <tr><td><b>Gearbox rating/ratio</b></td><td><b>60 hp / 1:1.5</b></td><td><b>100 hp / 1:1.46</b></td><td><b>60 hp / 1:1.5</b></td></tr>
+        <tr><td><b>Cutting wheel construction</b></td><td colspan="3"><b>--Welded 2-piece laminated steel--</b></td></tr>
+        <tr><td><b>Cutting wheel thickness</b></td><td colspan="3"><font size="+2"><b>----------¾"----------</b></font></td></tr>
+        <tr><td><b>Cutting wheel diameter</b></td><td><b>26-inches</b></td><td><b>36-inches</b></td><td><b>26-inches</b></td></tr>
+        <tr><td><b>Cutting wheel speed (approx.)<br>@ 540 PTO rpm</b></td><td><b>810 rpm<br>5,515 ft./min</b></td><td><b>788 rpm<br>7,430 ft./min</b></td><td><b>810 rpm<br>5515 ft./min</b></td></tr>
+        <tr><td><b>Number of teeth</b></td><td><b>33</b></td><td><b>42</b></td><td><b>33</b></td></tr>
+        <tr><td><b>Cutting teeth construction</b></td><td colspan="3"><b>------Carbide Steel Bolt In------</b></td></tr>
+        <tr><td><b>Cutting height above ground</b></td><td><b>18-inches</b></td><td><b>18-inches</b></td><td><b>18-inches</b></td></tr>
+        <tr><td><b>Cutting depth below ground</b></td><td><b>10-inches</b></td><td><b>15-inches</b></td><td><b>10-inches</b></td></tr>
+        <tr><td><b>Max. cut depth per pass</b></td><td><b>5-inches</b></td><td><b>8-inches</b></td><td><b>5-inches</b></td></tr>
+        <tr><td><b>Maximum horizontal cut</b></td><td><b>28-inches</b></td><td><b>33-inches</b></td><td><b>28-inches</b></td></tr>
+        <tr><td><b>Swing arc</b></td><td><b>45-degrees</b></td><td><b>45-degrees</b></td><td><b>45-degrees</b></td></tr>
+        <tr><td><b>Min. power / flow required</b></td><td><b>22-PTO hp</b></td><td><b>45-PTO hp</b></td><td><b>15 gpm</b></td></tr>
+        <tr><td><b>Max. power / flow</b></td><td><b>50-PTO hp</b></td><td><b>100-PTO hp</b></td><td><b>25 gpm</b></td></tr>
+        <tr><td><b>Drive System</b></td><td><b>PTO Series 5p</b></td><td><b>PTO / Series 6</b></td><td><b>Hydraulic</b></td></tr>
+        <tr><td><b>PTO Speed</b></td><td><b>540 rpm</b></td><td><b>540 rpm</b></td><td><b>540 rpm @ 19.4 gpm</b></td></tr>
+        <tr><td><b>Weight (est.)</b></td><td><b>660-lbs.</b></td><td><b>880-lbs.</b></td><td><b>757 lbs.</b></td></tr>
+        <tr><td><b>Computer product number</b></td><td><b>811500</b></td><td><b>811550</b></td><td><b>811640</b></td></tr>
 
-        <tr><td align="CENTER" width="20%" colspan="4"><font size="2"><b><i>* Be Sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</i></b></font></td></tr>
-        <tr><td align="left" width="20%"><b></b></td><td align="CENTER" width="15%"><b><a href="26stumpman.html">SG-26 Part List</a></b></td><td align="CENTER" width="15%"><b><a href="36stumpman.html">SG-36 Part List</a></b></td><td align="CENTER" width="15%"><b><a href="26Hydstumpman.html">SG-26H Part List</a></b></td></tr>
-        <tr><td align="center" width="20%" colspan="4"><b><a href="../manuals/wssg26-36.pdf"><b>Download Manual</b></a></b></td></tr>
-
-    </tbody></table>
-
-<table align="center" border="0" cellpadding="3" cellspacing="0" width="85%">
-    <tbody><tr><td align="left" colspan="3"><b><font size="+1">&nbsp;<br>&nbsp;</font></b></td></tr>
-
-        <tr><td align="left" colspan="3"><b><font size="3"><img src="Caution2.gif" alt="Photo of Caution Sign" width="25" length="25">&nbsp;&nbsp;Read and understand Operator's Manual before operation.</font></b></td></tr>
-        <tr><td align="left" colspan="3"><b><font size="3"><img src="Caution2.gif" alt="Photo of Caution Sign" width="25" length="25">&nbsp;&nbsp;Stump Grinder will put a heavy load on the rear of your tractor.  Be sure front of tractor is properly counterbalanced with weights.</font></b></td></tr>
-        <tr><td align="left" colspan="3"><b><font size="3"><img src="Caution2.gif" alt="Photo of Caution Sign" width="25" length="25">&nbsp;&nbsp;Never allow children or bystanders around tractor or stump grinder when you are using equipment.</font></b></td></tr>
+        <tr><td><b><i>* Be Sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</i></b></td></tr>
+        <tr><td></td><td><b><a href="26stumpman.html">SG-26 Part List</a></b></td><td><b><a href="36stumpman.html">SG-36 Part List</a></b></td><td><b><a href="26Hydstumpman.html">SG-26H Part List</a></b></td></tr>
+        <tr><td colspan="4"><b><a href="../manuals/wssg26-36.pdf"><b>Download Manual</b></a></b></td></tr>
+    </tbody>
+</table>
 
 
-    </tbody></table>
+<ul class='caution-icon'>
+    <li>Read and understand Operator's Manual before operation.</li>
+    <li>Stump Grinder will put a heavy load on the rear of your tractor.  Be sure front of tractor is properly counterbalanced with weights.</li>
+    <li>Never allow children or bystanders around tractor or stump grinder when you are using equipment.</li>
+</ul>
