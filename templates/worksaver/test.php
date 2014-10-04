c8baec7b322bd1796340a96dcbae9158569ade7f
jbake11 <jeffbake40@gmail.com>
2014-10-04 14:02:10 -0400

    css

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,87 +1,137 @@
 <div class="row">
+    <div class="col-lg-5 col-md-5 col-sm-5">
+        <img class="img-responsive" src="/images/Skid-Steer-Grader-Blade_IMG_0452.jpg" alt=""/>
+    </div>
+    <div class="col-lg-7 col-md-7 col-sm-7">
+        <h3>Skid Steer Grader Blade</h3>
+        <p>The patented SSGB-8A Grader is designed to be used by asphalt, landscaping, and concrete flatwork contractors. The unit has a 8-foot, six-way hydraulically controlled moldboard operated by an in-cab remote. With an optional laser system, the grader will maintain grade to tight tolerances. To help prevent damage, the moldboard is shear pin protected. Unit comes complete with 1/2” “connect under pressure” flat-faced hydraulic couplers and a reversible beveled cutting edge for long life. Optional End Plate Kit (Pair, #811980) and Laser Pole Kit (Pair, #812110) available.</p>
+
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <img class="img-responsive" src="/images/IMG_0442.jpg" alt=""/>
+        Close-up of optional end plate -
+        sold in pairs.
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <img class="img-responsive" src="/images/IMG_0468.jpg" alt=""/>
+        Optional laser pole kit
+        - sold in pairs.
+    </div>
+
+</div>
+</div>
+<div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>SmoothCut PTO &amp; Hydraulic Powered Stump Grinders</h3>
-        <h4>Economical and Efficient PTO and Hydraulic powered stump grinders make stump removal fast and easy.  Operator easily controls the stump grinder from the comfort of the tractor or skid steer seat.</h4>
-    </div>
+        <h4>Grader Blades Specifications</h4>
+        <table class="table table-responsive table-bordered table-striped">
+            <tbody class="tbody-content">
+                <tr>
+                    <td>&nbsp;</td>
+                    <td class="">SSGB-8A</td>
+                </tr>
+                <tr>
+                    <td>Overall Height (in/mm)</td>
+                    <td>46-1/2 / 1181</td>
+                </tr>
+                <tr>
+                    <td>Overall Length (in/mm)</td>
+                    <td>98 / 2489</td>
+                </tr>
+                <tr>
+                    <td>Overall Width (in/mm)</td>
+                    <td>95-1/2 / 2426</td>
+                </tr>
+                <tr>
+                    <td>Weight (lbs/kg)</td>
+                    <td>1195 / 542</td>
+                </tr>
+                <tr>
+                    <td>Mount Type</td>
+                    <td>Universal Skid Steer</td>
+                </tr>
+                <tr>
+                    <td>Hydraulic Blade Adjustments</td>
+                    <td>Tilt / Angle / Height</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Width (in/mm)</td>
+                    <td>95-1/2 / 2426</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Height (in/mm)</td>
+                    <td>15 / 381 w/Cutting Edge</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Width, Fully Angled (in/mm)</td>
+                    <td>85 / 2159</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Rotation</td>
+                    <td>Left & Right 25 Degrees</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Tilt Up or Down</td>
+                    <td>25 Degrees</td>
+                </tr>
+                <tr>
+                    <td>Reversible Moldboard Cutting Edge (in/mm)</td>
+                    <td>1/2 x 6 / 13 x 152 Double Bevel</td>
+                </tr>
+                <tr>
+                    <td>Moldboard Shear Pin Protection (in/mm)</td>
+                    <td>5/16 / 8 Diameter Shear Pin (Soft)</td>
+                </tr>
+                <tr>
+                    <td>3 -Hydraulic Cylinders (in/cm) PSI/bar</td>
+                    <td>2 / 5 Bore x 12 / 30 Stroke 3500 / 241</td>
+                </tr>
+                <tr>
+                    <td>Flat-Faced Couplers Included (in/cm)</td>
+                    <td>1/2 / 13 Body - Connect Under Pressure</td>
+                </tr>
+                <tr>
+                    <td>Grader Control Valve</td>
+                    <td>Electric Solenoid Operated</td>
+                </tr>
+                <tr>
+                    <td>In-Cab Remote Control</td>
+                    <td>Standard</td>
+                </tr>
+                <tr>
+                    <td>Recommended Min. Skid Steer HP</td>
+                    <td>45 HP (34 kw)</td>
+                </tr>
+                <tr>
+                    <td>Wheel Bearings</td>
+                    <td>Greaseable Tapered Roller</td>
+                </tr>
+                <tr>
+                    <td>Tire Size (in/mm)</td>
+                    <td>18-1/2 x 8-1/2 x 8 / 470 x 216 x 203</td>
+                </tr>
+                <tr>
+                    <td>Product No.</td>
+                    <td>811901</td>
+                </tr>
 
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sg1.jpg" alt="Photo Close-up of Stump Grinder"/>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/SGH-26.jpg"/>
+            </tbody>
+        </table>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sg.jpg" alt="Photo of Stump Grinder"/>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sgfoot.jpg" alt="Photo of Stump Grinder Stabilizer Bar"/>
-    </div>
-</div>
-
 
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Features</h3>
 
-        <ul>
-            <li>Maneuverable - goes anywhere a tractor can; gets into tight places.</li>
-            <li>Higher productivity via it's unique cutting wheel design.</li>
-            <li>Cutting wheel design provides a smoother cutting action.</li>
-            <li>Easy to operate - hydraulic controls allow easy operation from the tractor seat using existing tractor hydraulic controls.</li>
-            <li>Durable carbide-tipped cutting teeth are economical and easy to replace.</li>
-            <li>Harden bushings used in major pivot points for increased reliability.</li>
-            <li>Grease zerks provided at major pivot points for extended service life.</li>
-            <li>Standard needle valve for precise control of cutting feed rate for best cutting action.</li>
-            <li>SureFoot stabilizer bar provides solid footing during operation.</li>
-            <li>Rubber deflector helps control chips and debris during grinding.</li>
-            <li>Parking stand for easy storage.</li>
-            <li>Tractor models require two pair of remote hydraulic outlets</li>
-            <li>Hydraulic model has quick attach universal skid steer hitch system.</li>
-            <li>skid steer unit has hydraulic solenoid valve requiring only one pair of remote hydraulic outlets.</li>
-        </ul>
     </div>
 </div>
-<p></p>
-<table class='table table-responsive table-bordered table-striped'>
-    <tbody><tr><td align="center" colspan="4"><b>SmoothCut Specifications</b></td></tr>
-        <tr><td><b></b></td><td><b>Model SG-26</b></td><td><b>Model SG-36</b></td><td><b>Model SG-26H</b></td></tr>
-        <tr><td><b>Hitch</b></td><td><b>Cat. I</b></td><td><b>Cat. I/II</b></td><td><b>Univ. Skid Steer</b></td></tr>
-        <tr><td><b>Overall Length (est.)</b></td><td><b>75-inches</b></td><td><b>80-inches</b></td><td><b>72-inches</b></td></tr>
-        <tr><td><b>Drive System</b></td><td><b>Slip Clutch PTO/Series 5</b></td><td><b>Slip Clutch PTO/Series 6</b></td><td><b>System Pressure Hydraulics</b></td></tr>
-        <tr><td><b>PTO speed</b></td><td><b>540 rpm</b></td><td><b>540 rpm</b></td><td><b>540 rpm @ 19.4 gpm</b></td></tr>
-        <tr><td><b>Drive system protection</b></td><td><b>Slip Clutch</b></td><td><b>slip clutch</b></td><td><b>System Pressure</b></td></tr>
-        <tr><td><b>Gearbox rating/ratio</b></td><td><b>60 hp / 1:1.5</b></td><td><b>100 hp / 1:1.46</b></td><td><b>60 hp / 1:1.5</b></td></tr>
-        <tr><td><b>Cutting wheel construction</b></td><td colspan="3"><b>--Welded 2-piece laminated steel--</b></td></tr>
-        <tr><td><b>Cutting wheel thickness</b></td><td colspan="3"><font size="+2"><b>----------¾"----------</b></font></td></tr>
-        <tr><td><b>Cutting wheel diameter</b></td><td><b>26-inches</b></td><td><b>36-inches</b></td><td><b>26-inches</b></td></tr>
-        <tr><td><b>Cutting wheel speed (approx.)<br>@ 540 PTO rpm</b></td><td><b>810 rpm<br>5,515 ft./min</b></td><td><b>788 rpm<br>7,430 ft./min</b></td><td><b>810 rpm<br>5515 ft./min</b></td></tr>
-        <tr><td><b>Number of teeth</b></td><td><b>33</b></td><td><b>42</b></td><td><b>33</b></td></tr>
-        <tr><td><b>Cutting teeth construction</b></td><td colspan="3"><b>------Carbide Steel Bolt In------</b></td></tr>
-        <tr><td><b>Cutting height above ground</b></td><td><b>18-inches</b></td><td><b>18-inches</b></td><td><b>18-inches</b></td></tr>
-        <tr><td><b>Cutting depth below ground</b></td><td><b>10-inches</b></td><td><b>15-inches</b></td><td><b>10-inches</b></td></tr>
-        <tr><td><b>Max. cut depth per pass</b></td><td><b>5-inches</b></td><td><b>8-inches</b></td><td><b>5-inches</b></td></tr>
-        <tr><td><b>Maximum horizontal cut</b></td><td><b>28-inches</b></td><td><b>33-inches</b></td><td><b>28-inches</b></td></tr>
-        <tr><td><b>Swing arc</b></td><td><b>45-degrees</b></td><td><b>45-degrees</b></td><td><b>45-degrees</b></td></tr>
-        <tr><td><b>Min. power / flow required</b></td><td><b>22-PTO hp</b></td><td><b>45-PTO hp</b></td><td><b>15 gpm</b></td></tr>
-        <tr><td><b>Max. power / flow</b></td><td><b>50-PTO hp</b></td><td><b>100-PTO hp</b></td><td><b>25 gpm</b></td></tr>
-        <tr><td><b>Drive System</b></td><td><b>PTO Series 5p</b></td><td><b>PTO / Series 6</b></td><td><b>Hydraulic</b></td></tr>
-        <tr><td><b>PTO Speed</b></td><td><b>540 rpm</b></td><td><b>540 rpm</b></td><td><b>540 rpm @ 19.4 gpm</b></td></tr>
-        <tr><td><b>Weight (est.)</b></td><td><b>660-lbs.</b></td><td><b>880-lbs.</b></td><td><b>757 lbs.</b></td></tr>
-        <tr><td><b>Computer product number</b></td><td><b>811500</b></td><td><b>811550</b></td><td><b>811640</b></td></tr>
-
-        <tr><td><b><i>* Be Sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</i></b></td></tr>
-        <tr><td></td><td><b><a href="26stumpman.html">SG-26 Part List</a></b></td><td><b><a href="36stumpman.html">SG-36 Part List</a></b></td><td><b><a href="26Hydstumpman.html">SG-26H Part List</a></b></td></tr>
-        <tr><td align="center" colspan="4"><b><a href="../manuals/wssg26-36.pdf"><b>Download Manual</b></a></b></td></tr>
-    </tbody>
-</table>
 
 
-<ul class='caution-icon'>
-    <li>Read and understand Operator's Manual before operation.</li>
-    <li>Stump Grinder will put a heavy load on the rear of your tractor.  Be sure front of tractor is properly counterbalanced with weights.</li>
-    <li>Never allow children or bystanders around tractor or stump grinder when you are using equipment.</li>
-</ul>
+
+
+
+
+
+
