6f5e47de04b64700b189e319557dfba9667b3b60
jbake11 <jeffbake40@gmail.com>
2014-10-05 23:46:09 -0400

    asd

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,137 +1,109 @@
-<div class="row">
-    <div class="col-lg-5 col-md-5 col-sm-5">
-        <img class="img-responsive" src="/images/Skid-Steer-Grader-Blade_IMG_0452.jpg" alt=""/>
-    </div>
-    <div class="col-lg-7 col-md-7 col-sm-7">
-        <h3>Skid Steer Grader Blade</h3>
-        <p>The patented SSGB-8A Grader is designed to be used by asphalt, landscaping, and concrete flatwork contractors. The unit has a 8-foot, six-way hydraulically controlled moldboard operated by an in-cab remote. With an optional laser system, the grader will maintain grade to tight tolerances. To help prevent damage, the moldboard is shear pin protected. Unit comes complete with 1/2” “connect under pressure” flat-faced hydraulic couplers and a reversible beveled cutting edge for long life. Optional End Plate Kit (Pair, #811980) and Laser Pole Kit (Pair, #812110) available.</p>
-
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/IMG_0442.jpg" alt=""/>
-        Close-up of optional end plate -
-        sold in pairs.
-    </div>
-
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/IMG_0468.jpg" alt=""/>
-        Optional laser pole kit
-        - sold in pairs.
-    </div>
-
-</div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h4>Grader Blades Specifications</h4>
-        <table class="table table-responsive table-bordered table-striped">
-            <tbody class="tbody-content">
-                <tr>
-                    <td>&nbsp;</td>
-                    <td class="">SSGB-8A</td>
-                </tr>
-                <tr>
-                    <td>Overall Height (in/mm)</td>
-                    <td>46-1/2 / 1181</td>
-                </tr>
-                <tr>
-                    <td>Overall Length (in/mm)</td>
-                    <td>98 / 2489</td>
-                </tr>
-                <tr>
-                    <td>Overall Width (in/mm)</td>
-                    <td>95-1/2 / 2426</td>
-                </tr>
-                <tr>
-                    <td>Weight (lbs/kg)</td>
-                    <td>1195 / 542</td>
-                </tr>
-                <tr>
-                    <td>Mount Type</td>
-                    <td>Universal Skid Steer</td>
-                </tr>
-                <tr>
-                    <td>Hydraulic Blade Adjustments</td>
-                    <td>Tilt / Angle / Height</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Width (in/mm)</td>
-                    <td>95-1/2 / 2426</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Height (in/mm)</td>
-                    <td>15 / 381 w/Cutting Edge</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Width, Fully Angled (in/mm)</td>
-                    <td>85 / 2159</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Rotation</td>
-                    <td>Left & Right 25 Degrees</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Tilt Up or Down</td>
-                    <td>25 Degrees</td>
-                </tr>
-                <tr>
-                    <td>Reversible Moldboard Cutting Edge (in/mm)</td>
-                    <td>1/2 x 6 / 13 x 152 Double Bevel</td>
-                </tr>
-                <tr>
-                    <td>Moldboard Shear Pin Protection (in/mm)</td>
-                    <td>5/16 / 8 Diameter Shear Pin (Soft)</td>
-                </tr>
-                <tr>
-                    <td>3 -Hydraulic Cylinders (in/cm) PSI/bar</td>
-                    <td>2 / 5 Bore x 12 / 30 Stroke 3500 / 241</td>
-                </tr>
-                <tr>
-                    <td>Flat-Faced Couplers Included (in/cm)</td>
-                    <td>1/2 / 13 Body - Connect Under Pressure</td>
-                </tr>
-                <tr>
-                    <td>Grader Control Valve</td>
-                    <td>Electric Solenoid Operated</td>
-                </tr>
-                <tr>
-                    <td>In-Cab Remote Control</td>
-                    <td>Standard</td>
-                </tr>
-                <tr>
-                    <td>Recommended Min. Skid Steer HP</td>
-                    <td>45 HP (34 kw)</td>
-                </tr>
-                <tr>
-                    <td>Wheel Bearings</td>
-                    <td>Greaseable Tapered Roller</td>
-                </tr>
-                <tr>
-                    <td>Tire Size (in/mm)</td>
-                    <td>18-1/2 x 8-1/2 x 8 / 470 x 216 x 203</td>
-                </tr>
-                <tr>
-                    <td>Product No.</td>
-                    <td>811901</td>
-                </tr>
-
-            </tbody>
-        </table>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-
-    </div>
-</div>
+<body background="../bin/bg/brownbk.jpg" link="#FF0000" vlink="#800080" alink="#00FF00" leftmargin="0" topmargin="0" bgcolor="">
 
 
+<center><table align="center" cellpadding="0" cellspacing="0" border="0">
+        <tbody><tr><td><script type="text/javascript">
+
+        //HV Menu v5.411- by Ger Versluis (http://www.burmees.nl/)
+        //Submitted to Dynamic Drive (http://www.dynamicdrive.com)
+        //Visit http://www.dynamicdrive.com for this script and more
+
+            function Go() {
+                return
+            }
+
+                    </script>
+                    <script type="text/javascript" src="prodmenu_var.js"></script>
+                    <script type="text/javascript" src="menu_com.js"></script>
+                    <noscript>Your browser does not support script</noscript></td><td><img src="../bin/image/no-pic.gif" width="130" height="60"></td><td align="CENTER"><img src="../bin/image/newworksaver.jpg" height="80"></td><td align="CENTER"></td><td><img src="../bin/image/no-pic.gif" width="50" height="60"></td><td><img src="../bin/ani/usf261.gif" height="60"></td></tr>
+        </tbody></table>
+    <!-- BEGIN WEBSIDESTORY CODE -->
+    <!-- COPYRIGHT 1997-2001 WEBSIDESTORY, INC. ALL RIGHTS RESERVED. U.S.PATENT PENDING. Privacy notice at: http://websidestory.com/privacy -->
 
 
+    <base font="" color="#000000" size="3">
 
+    <!-- *** Begin Header Table ***--->
+
+    <!-- *** End Header Table ***--->
+
+    <p><br></p>
+    <table align="CENTER" border="0" cellpadding="5" cellspacing="0" width="85%">
+
+        <tbody><tr align="CENTER" valign="TOP"><td align="CENTER" valign="TOP" nowrap=""><font color="#000000" size="+2"><b>3-PT. BALE SPEARS</b></font></td></tr>
+            <tr align="center"><td align="left"><font color="#000000" size="3"><b>&nbsp;&nbsp;&nbsp;&nbsp;Designed for use on tractor 3-pt. hitches only.  These bale spear feature tapered forged spring steel spears allowing easier spear penetration of and withdrawal from the bale.  Frames are made from rugged 3" x 3" Square Tubing.</b></font></td></tr>
+
+        </tbody></table>
+    <p>
+
+    <table align="CENTER" border="1" width="75%">
+        <tbody><tr><td widt1h="180" bgcolor="#80FFFF"><b><font size="2">Product</font></b></td>
+                <td width="18%" bgcolor="#80FFFF"><b><font size="2">Model</font></b> </td>
+                <td width="100%" bgcolor="#80FFFF"><b><font size="2">Description </font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="160" src="../bp/bsf-1523-lit.jpg" alt="Photo of BSF-1523"></td>
+                <td width="12%"><b><font size="2"><br>BSF-1523</font></b>
+                </td><td width="100%"><b><font size="2">Economical 3-pt. Bale Spear for handling round bales up to 1,800 lbs.  Unit features a main tapered spear with a useable length of 44" and two welded balance spears.  Fits Cat. I/II, sjipped with 7/8" pins.
+                        <br><br><a href="bsf1523man.html">Parts List Click Here</a> <br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="140" src="../bp/dbfs-30.gif" alt="Photo of HS-3000"></td>
+                <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-3000</font></b></td>
+                <td width="100%"><b><font size="2">Dual bolt-in spears with cast steel sockets (Pat. Pend.), 42" useable length.  Round bales maintain orientation.  Fits Cat I/II/Quick hitches.
+                        <br><br><a href="hs3000man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="140" src="../bp/Hs-2200.JPG" alt="Photo of HS-2200"></td>
+                <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-2200</font></b></td>
+                <td width="100%"><b><font size="2">3-pt. Bale Spear for round bales to 2,200 lbs.  Bolt-in spear (Useable 42")  with cast steel socket (pat. pend).  Two balance spikes.  Cat I/II and quick hitch compatible.  Ships UPS.
+                        <br><br><a href="hs2200man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+            <tr><td width="180"><a href="3pt-spear.html"><img width="200" height="140" src="../bp/hs-330.JPG" alt="Photo of HS-330"></a></td>
+                <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-330</font></b></td>
+                <td width="100%"><b><font size="2">3-pt. Triple bale spear rated 3,000 lbs.  Features bolt-in forged spear (26" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For mid-size square bales.<br><br>Click on picture for more information.
+                        <br><br><a href="man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+            <tr><td width="180"><a href="3pt-spear.html"><img width="200" height="140" src="../bp/dbh-449-2.JPG" alt="Photo of DBH-449"></a></td>
+                <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>DBH-449</font></b></td>
+                <td width="100%"><b><font size="2">3-pt. Four bale spear handler rated 4,000 lbs.  Features bolt-in forged spear (42" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For two round bales.  <br><br>Click on picture for more information.
+                        <br><br><a href="man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="140" src="../bp/bsfa-20-lit.jpg" alt="Photo of BSFA-20"></td>
+                <td width="12%"><b><font size="2"><br>BSFA-20</font></b></td>
+                <td width="100%"><b><font size="2">Adjustable height bale spear for handling round bales up to 2,000 lbs.  Utilizes a bolt-on spear with a useable length of 44".  The spear can be positioned up or down the vertical mast.  Fits Cat. I/II and is shipped with 7/8" pins.
+                        <br><br><a href="bsfa20man.html">Parts List Click Here</a> <br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+
+            <tr><td width="180"><img width="200" height="162" src="../bp/F-1511_2.jpg" alt="Photo of F-1511"></td>
+                <td width="12%"><b><font size="2">F-1511</font></b></td>
+                <td width="100%"><b><font size="2">3-Pt. Bale fork rated at 1,500 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 37.5" apart.  Unit fits Cat. I/II as well as Cat II Quick Hitches.  Shipped with 7/8" pins.</font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="160" src="../bp/F-3000.jpg" alt="Photo of F-3000"></td>
+                <td width="12%"><b><font size="2">F-3000</font></b></td>
+                <td width="100%"><b><font size="2">3-Pt. Bale fork rated at 3,000 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 46.5" apart.  Unit fits Cat. II as well as Cat II Quick Hitches.  Shipped with 1-1/8" pins.<br><br><a href="f3000man.html">Parts List Click Here</a></font></b></td></tr>
+
+            <tr><td width="180"><img width="200" height="115" src="../bp/hhu2045stand.jpg" alt="Photo of HHU-2045"></td>
+                <td width="12%"><b><font size="2">HHU-2045</font></b></td>
+                <td width="100%"><font size="2"><b>3-Pt Hay Handler / Unroller.  Rated at 2,000 lbs. and is designed to unroll or handle round bales either 4' or 5' in width up to 6' in diameter.  Unit Requires a 2.5" x 8" hydraulic cylinder (which is not included). The HHU-2045 now comes with a parking stand for allowing easy hook up with a Quick Hitch.
+                        <br><br><a href="hhu2045man.html">Parts List Click Here</a><br><br><a href="../manuals/hhu2045.pdf">Download Manual</a> </b></font><b> </b></td></tr>
+
+            <tr><td align="center" colspan="4"><b><br><a href="spearkit.html">Replacement Spear Kits<br> <br></a></b></td></tr>
+
+
+            <tr><td align="CENTER" colspan="3" width="100%"><a href="haf-op.html"><img border="0" vspace="5" src="../bin/button/htlc.gif" alt="OPTIONAL TOP LINK CYLINDERS"></a></td></tr>
+        </tbody></table>
+    <!-- End of Product Menu -->
+
+    <table align="CENTER" valign="TOP" width="75%" border="0">
+        <tbody><tr><td align="Left" width="65%" valign="center"><b><font size="-6">NOTICE:</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="-10">Worksaver Inc. has a corporate policy of continous product improvement and development:
+                        Therefore, specifications are subject to change without any advance notice.  Worksaver Inc. is not responsible for differences between specifications or illustrations contained in this site and the actual equipment.</font></b></td></tr>
+        </tbody></table>
+    <table align="center">
+        <tbody><tr><td align="center"><font size="1">To report problems or ask questions concerning Worksaver's web site, please contact the <a href="mailto:webmaster@worksaver.com">Webmaster</a>.</font><br>
+
+                    <font size="1">Copyright © 1996-2011 Worksaver, Inc. All Rights Reserved.  <a href="legal.html">Legal Notices</a></font><br>
+
+                    <font size="1">This document was last modified on
+                    <!--#echo var="LAST_MODIFIED" -->
+                    </font></td></tr></tbody></table>
 
 
 
