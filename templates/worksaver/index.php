94eaf5c5f6639697831177acbdf1b97ff142f0cb
Chris Bake <cbake6807@users.noreply.github.com>
2014-10-06 16:54:40 -0400

    Update index.php

--- templates/worksaver/index.php
+++ templates/worksaver/index.php
@@ -86,9 +86,22 @@
 
 <!--Body Content-->
 <div class="container">
-    <div id="index-thumbs" class="row">
-        <jdoc:include type="component" />
+    <div class="row">
+    <?php if ($this->countModules('sidebar')) : ?>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <jdoc:include type="modules" name="sidebar" style="none" />
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <jdoc:include type="component" />
+        </div>
     </div>
+    <?php else: ?>
+    <div class="col-lg-12">
+        <div id="index-thumbs">
+            <jdoc:include type="component" />
+        </div>
+    </div>
+    <?php endif; ?>
 </div>
 
 <!--Footer-->
