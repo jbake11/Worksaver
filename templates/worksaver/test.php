08070ba5bff4b378712ee2c9cb98ff0746a39fa5
jbake11 <jeffbake40@gmail.com>
2014-10-15 10:18:14 -0400

    asd

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -47,151 +47,58 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr>
-                    <td align="center" colspan="3"><font size="+1">SPECIFICATIONS</font></td>
-                </tr>
-                <tr>
-                    <td align="right"><font size="+1">&nbsp;&nbsp;&nbsp; </font></td>
-                    <td align="center"><font size="+1">Model# 580c</font></td>
-                    <td align="center"><font size="+1">Model# SS-590</font></td>
-                </tr>
-                <tr>
-                    <td>Capacity:</td>
-                    <td align="center" width="32%">Five (5) cu./ft. (1 bag mix)</td>
-                    <td align="center" width="32%">Five (5) cu./ft. (1 bag mix)</td>
-                </tr>
-                <tr>
-                    <td>Drum opening (in/mm):</td>
-                    <td align="center" width="32%">17.5 / (444 mm)</td>
-                    <td align="center" width="32%">20 (508)</td>
-                </tr>
-                <tr>
-                    <td>Overall drum length (in/mm):</td>
-                    <td align="center" width="32%">32 / (812 mm)</td>
-                    <td align="center" width="32%">32 / (813 mm)</td>
-                </tr>
-                <tr>
-                    <td>Diameter of drum (in/mm):</td>
-                    <td align="center" width="32%">32 / (812 mm)</td>
-                    <td align="center" width="32%">30 (762)</td>
-                </tr>
-                <tr>
-                    <td>Drum construction:</td>
-                    <td align="center" width="32%">2-piece bolted</td>
-                    <td align="center" width="32%">Welded Steel</td>
-                </tr>
-                <tr>
-                    <td>Drum thickness:</td>
-                    <td align="center" width="32%">11-gauge steel</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Cone thickness:</td>
-                    <td align="center" width="32%">14-gauge steel</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Drive system:</td>
-                    <td align="center" width="32%">Tractor PTO</td>
-                    <td align="center" width="32%">Hydraulic</td>
-                </tr>
-                <tr>
-                    <td>PTO driveline:</td>
-                    <td align="center" width="32%">Metric Series I</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>PTO speed:</td>
-                    <td align="center" width="32%">540 rpm</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Drum drive:</td>
-                    <td align="center" width="32%">#40 chain</td>
-                    <td align="center" width="32%">Gearbox Direct</td>
-                </tr>
-                <tr>
-                    <td>Drive ratio:</td>
-                    <td align="center" width="32%">8.77:1</td>
-                    <td align="center" width="32%">6.4:1</td>
-                </tr>
-                <tr>
-                    <td>Shield PTO drive:</td>
-                    <td align="center" width="32%">Standard</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Chain tightner:</td>
-                    <td align="center" width="32%">Standard</td>
-                    <td align="center" width="32%">Standard</td>
-                </tr>
-                <tr>
-                    <td>Bearing type:</td>
-                    <td align="center" width="32%">Sealed Ball Bearings</td>
-                    <td align="center" width="32%">Tapered Roller</td>
-                </tr>
-                <tr>
-                    <td>Hydraulic toplink cylinder type:
-                    </td><td align="center" width="32%">Single Acting</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Cylinder size (in/mm):</td>
-                    <td align="center" width="32%">1.75 (44 mm) x 13 (330 mm)</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Hitch requirement:</td>
-                    <td align="center" width="32%">Cat. I (7/8" Dia. Lift Pins)</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Hydraulic toplink positions:</td>
-                    <td align="center" width="32%">Three (3)</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Lower pull pin positions:</td>
-                    <td align="center" width="32%">Four (4)</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Parking stands:</td>
-                    <td align="center" width="32%">Standard</td>
-                    <td align="center" width="32%">Standard</td>
-                </tr>
-                <tr>
-                    <td>Tractor equipment (min. PTO HP)</td>
-                    <td align="center" width="32%">25 PTO HP*</td>
-                    <td align="center" width="32%">N/A</td>
-                </tr>
-                <tr>
-                    <td>Shipping weight (lb./kg)</td>
-                    <td align="center" width="32%">253 / (115 kg)</td>
-                    <td align="center" width="32%">395 (110)</td>
-                </tr>
-                <tr>
-                    <td>Product computer number:</td>
-                    <td align="center" width="32%">780620</td>
-                    <td align="center" width="32%">780950</td>
-                </tr>
-                <tr>
-                    <td align="center" colspan="3">* Be sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</td>
-                </tr>
-                <tr>
-                    <td>Parts List</td>
-                    <td align="center" width="32%"><a href="mixerman.html">#580c</a></td>
-                    <td align="center" width="32%"><a href=".html">SS-590  N/A</a></td>
-                </tr>
-                <tr>
-                    <td>Manual</td>
-                    <td align="center" width="32%"><a href="/manuals/580-mixer.pdf">#580c</a></td>
-                    <td align="center" width="32%"><a href="/manuals/WSss590.pdf">SS-590</a></td>
-                </tr>
-                <tr>
-                    <td align="center" width="38%" colspan="3"><a href="/mix/580 Instructions.pdf">580 Mixer Assembly Instructions</a></td>
-                </tr>
+            <tbody><tr><td widt1h="180" bgcolor="#80FFFF"><b><font size="2">Product</font></b></td>
+                    <td width="18%" bgcolor="#80FFFF"><b><font size="2">Model</font></b> </td>
+                    <td width="100%" bgcolor="#80FFFF"><b><font size="2">Description </font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="160" src="../bp/bsf-1523-lit.jpg" alt="Photo of BSF-1523"></td>
+                    <td width="12%"><b><font size="2"><br>BSF-1523</font></b>
+                    </td><td width="100%"><b><font size="2">Economical 3-pt. Bale Spear for handling round bales up to 1,800 lbs.  Unit features a main tapered spear with a useable length of 44" and two welded balance spears.  Fits Cat. I/II, sjipped with 7/8" pins.
+                            <br><br><a href="bsf1523man.html">Parts List Click Here</a> <br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="140" src="../bp/dbfs-30.gif" alt="Photo of HS-3000"></td>
+                    <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-3000</font></b></td>
+                    <td width="100%"><b><font size="2">Dual bolt-in spears with cast steel sockets (Pat. Pend.), 42" useable length.  Round bales maintain orientation.  Fits Cat I/II/Quick hitches.
+                            <br><br><a href="hs3000man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="140" src="../bp/Hs-2200.JPG" alt="Photo of HS-2200"></td>
+                    <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-2200</font></b></td>
+                    <td width="100%"><b><font size="2">3-pt. Bale Spear for round bales to 2,200 lbs.  Bolt-in spear (Useable 42")  with cast steel socket (pat. pend).  Two balance spikes.  Cat I/II and quick hitch compatible.  Ships UPS.
+                            <br><br><a href="hs2200man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+                <tr><td width="180"><a href="3pt-spear.html"><img width="200" height="140" src="../bp/hs-330.JPG" alt="Photo of HS-330"></a></td>
+                    <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>HS-330</font></b></td>
+                    <td width="100%"><b><font size="2">3-pt. Triple bale spear rated 3,000 lbs.  Features bolt-in forged spear (26" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For mid-size square bales.<br><br>Click on picture for more information.
+                            <br><br><a href="man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+                <tr><td width="180"><a href="3pt-spear.html"><img width="200" height="140" src="../bp/dbh-449-2.JPG" alt="Photo of DBH-449"></a></td>
+                    <td width="12%"><b><font size="2"><img src="../bin/ani/newanima.gif"><br>DBH-449</font></b></td>
+                    <td width="100%"><b><font size="2">3-pt. Four bale spear handler rated 4,000 lbs.  Features bolt-in forged spear (42" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For two round bales.  <br><br>Click on picture for more information.
+                            <br><br><a href="man.html">Parts List Click Here</a><br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="140" src="../bp/bsfa-20-lit.jpg" alt="Photo of BSFA-20"></td>
+                    <td width="12%"><b><font size="2"><br>BSFA-20</font></b></td>
+                    <td width="100%"><b><font size="2">Adjustable height bale spear for handling round bales up to 2,000 lbs.  Utilizes a bolt-on spear with a useable length of 44".  The spear can be positioned up or down the vertical mast.  Fits Cat. I/II and is shipped with 7/8" pins.
+                            <br><br><a href="bsfa20man.html">Parts List Click Here</a> <br><br><a href="../manuals/hsbdh.pdf">Download Manual</a> </font></b></td></tr>
+
+
+                <tr><td width="180"><img width="200" height="162" src="../bp/F-1511_2.jpg" alt="Photo of F-1511"></td>
+                    <td width="12%"><b><font size="2">F-1511</font></b></td>
+                    <td width="100%"><b><font size="2">3-Pt. Bale fork rated at 1,500 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 37.5" apart.  Unit fits Cat. I/II as well as Cat II Quick Hitches.  Shipped with 7/8" pins.</font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="160" src="../bp/F-3000.jpg" alt="Photo of F-3000"></td>
+                    <td width="12%"><b><font size="2">F-3000</font></b></td>
+                    <td width="100%"><b><font size="2">3-Pt. Bale fork rated at 3,000 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 46.5" apart.  Unit fits Cat. II as well as Cat II Quick Hitches.  Shipped with 1-1/8" pins.<br><br><a href="f3000man.html">Parts List Click Here</a></font></b></td></tr>
+
+                <tr><td width="180"><img width="200" height="115" src="../bp/hhu2045stand.jpg" alt="Photo of HHU-2045"></td>
+                    <td width="12%"><b><font size="2">HHU-2045</font></b></td>
+                    <td width="100%"><font size="2"><b>3-Pt Hay Handler / Unroller.  Rated at 2,000 lbs. and is designed to unroll or handle round bales either 4' or 5' in width up to 6' in diameter.  Unit Requires a 2.5" x 8" hydraulic cylinder (which is not included). The HHU-2045 now comes with a parking stand for allowing easy hook up with a Quick Hitch.
+                            <br><br><a href="hhu2045man.html">Parts List Click Here</a><br><br><a href="../manuals/hhu2045.pdf">Download Manual</a> </b></font><b> </b></td></tr>
+
+                <tr><td align="center" colspan="4"><b><br><a href="spearkit.html">Replacement Spear Kits<br> <br></a></b></td></tr>
+
+
+                <tr><td align="CENTER" colspan="3" width="100%"><a href="haf-op.html"><img border="0" vspace="5" src="../bin/button/htlc.gif" alt="OPTIONAL TOP LINK CYLINDERS"></a></td></tr>
             </tbody>
         </table>
     </div>
