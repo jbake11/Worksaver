e213c17600ef28e1bec92262ca1a8b6a141fd060
jbake11 <jeffbake40@gmail.com>
2014-10-21 10:56:37 -0400

    new gif

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -3,13 +3,6 @@
         <h4>PTO Driven</h4>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-    </div>
-</div>
-
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
