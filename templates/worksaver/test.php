453ff9367f97f326d0ee41ed222cc6d90dabf7e5
jbake11 <jeffbake40@gmail.com>
2014-10-22 14:37:14 -0400

    parts list images white bg

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,96 +1,34 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Rotary Brooms For Skid Steers &amp; 3-Pt. Tractor Mount</h3>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4>Hydraulic Driven</h4>
     </div>
 </div>
 <div class="row">
-    <div class="list-nopad col-lg-12 col-md-12 col-sm-12">
-        <h4>Features</h4>
-        <ul>
-            <li>Top quality rotary brooms for use by contractors, municipalities, institutions, and farmers .</li>
-            <li>Remove dirt, sand, gravel and trash from construction entrances, parking lots, bridges, driveways and sidewalks.</li>
-            <li>Remove thatch and prepare turf for the growing season.</li>
-            <li>Clean snow or leaves from streets, driveways, and buildings, and building entrances.</li>
-            <li>Excellent for general clean up before sealing or repairing pavement.</li>
-            <li><a href="/templates/worksaver/literature/wsr6481_Land Management Equipment brochure.pdf">Literature</a></li>
-        </ul>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img src="/images/Broom 3.jpg" alt="Photo of Rotary Broom" />
-    </div>
-    <div class="list-nopad col-lg-6 col-md-6 col-sm-6">
-        <ul>
-            <li>PTO driven 3-pt. tractor mount in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.</li>
-            <li>Double gearbox drive (Patented No. 8,117,705) for smooth power tranmission and easy reversal of brush rotation.</li>
-            <li>Broom angles 30° right and left.</li>
-            <li>Parking stands standard.</li>
-        </ul>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
     </div>
 
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <hr class="hor-line">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
     </div>
 </div>
-<div class="row">
-    <div class="list-nopad col-lg-6 col-md-6 col-sm-6">
-        <ul>
-            <li>Hydraulically driven skid steer models in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.</li>
-            <li>Variable flow control valve for optimum broom speed.</li>
-            <li>Step-through mount design for easy in/out of skid steer cab.</li>
-            <li>Broom angles 30° right or left.</li>
-            <li>Parking stands standard.</li>
-            <li>32" Diameter convoluted brushes available in Poly (P) or both Poly and Wire (PW).</li>
-            <li>Height adjustment chains maintain even operating position.</li>
-            <li>Parking stand included as standard equipment.</li>
-            <li>Manual or Hydraulic Angle Kits available.</li>
-        </ul>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img src="/images/Broom Lit.jpg" alt="Photo of Rotary Broom" />
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header"><td align="center" colspan="9">Skid Steer Rotary Broom Specifications</td></tr>
-                <tr class="table-col-modelnum"><td>&nbsp;</td><td>SSMB-325P</td><td>SSMB-325PW</td><td>SSMB-326P</td><td>SSMB-326PW</td><td>SSMB-327P</td><td>SSMB-327PW</td></tr>
-                <tr><td>Overall Width (in/cm)</td><td>69 / 175</td><td>69 / 175</td><td>81 / 206</td><td>81 / 206</td><td>93 / 236</td><td>93 / 236</td></tr>
-                <tr><td>Brush Diameter (in/cm)</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td></tr>
-                <tr><td>Sweeping Width (in/cm)</td><td>60 / 152</td><td>60 / 152</td><td>72 / 183</td><td>72 / 183</td><td>84 / 213</td><td>84 / 213</td></tr>
-                <tr><td>Width @ 30° Angle (in/cm)</td><td>53 / 135</td><td>53 / 135</td><td>64 / 163</td><td>64 / 163</td><td>76 / 193</td><td>76 / 193</td></tr>
-                <tr><td>Mounting System</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td></tr>
-                <tr><td>Brush Material</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td></tr>
-                <tr><td>Dirt Deflector</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>640 / 290</td><td>637 / 289</td><td>740 / 336</td><td>736 / 334</td><td>840 / 381</td><td>833 / 378</td></tr>
-                <tr><td>Product No.</td><td>610961</td><td>610960</td><td>610966</td><td>610965</td><td>610971</td><td>610970</td></tr>
-                <tr><td>&nbsp;</td>
-                    <td colspan="3"><a href="/3-pt-rotary-brooms-parts-list">3-Pt. Rotary Broom Parts List</a></td>
-                    <td colspan="3"><a href="/manuals/broom.pdf">3-Pt. Rotary Broom Manual</a></tr>
-            </tbody>
-        </table>
-        <br />
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody><tr class="table-col-header"><td align="center" colspan="9">3-Pt. Rotary Broom Specifications</td></tr>
-                <tr class="table-col-modelnum"><td>&nbsp;</td><td>RMB-325P</td><td>RMB-325PW</td><td>RMB-326P</td><td>RMB-326PW</td><td>RMB-327P</td><td>RMB-327PW</td></tr>
-                <tr><td>Overall Width (in/cm)</td><td>69 / 175</td><td>69 / 175</td><td>81 / 206</td><td>81 / 206</td><td>93 / 236</td><td>93 / 236</td></tr>
-                <tr><td>Brush Diameter (in/cm)</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td></tr>
-                <tr><td>Sweeping Width (in/cm)</td><td>60 / 152</td><td>60 / 152</td><td>72 / 183</td><td>72 / 183</td><td> 84 / 213</td><td> 84 / 213</td></tr>
-                <tr><td>Width @ 30° Angle (in/cm)</td><td>53 / 135</td><td>53 / 135</td><td>64 / 163</td><td>64 / 163</td><td>76 / 193</td><td>76 / 193</td></tr>
-                <tr><td>Mounting System</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td></tr>
-                <tr><td>Brush Material</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td></tr>
-                <tr><td>PTO RPM Range</td><td>540</td><td>540</td><td>540</td><td>540</td><td>540</td><td>540</td></tr>
-                <tr><td>Dirt Deflector</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td></tr>
-                <tr><td>Hydraulic Motor Rating</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td></tr>
-                <tr><td>Oil Flow Requirement (gpm/lpm)</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td> 12-30 / 45-114</td><td>12-30 / 45-114</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>720 / 326</td><td>714 / 324</td><td>780 / 354</td><td>774 / 351</td><td>840 / 381</td><td>833 / 378</td></tr>
-                <tr><td>Product No.</td><td>610705</td><td>610700</td><td>610755</td><td>610750</td><td>610805</td><td>610800</td></tr>
-            </tbody>
-        </table>
 
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
     </div>
 </div>
