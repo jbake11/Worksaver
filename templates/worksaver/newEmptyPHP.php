083631ddc2316221fbbfebab4d2bea11ba57b12f
jbake11 <jeffbake40@gmail.com>
2014-10-24 15:39:34 -0400

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,96 +1,67 @@
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>SmoothCut&trade; PTO Powered Stump Grinders</h3>
-        <h4>Economical and Efficient PTO powered stump grinders make stump removal fast and easy. Operator easily controls the stump grinder from the comfort of the tractor seat.</h4>
-    </div>
+<h3>Need help handling dry or wrapped bales?<br />
+    Worksaver has the answer</h3>
 
-</div>
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sg2.jpg" alt="Photo Close-up of Stump Grinder"/>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sg.jpg" alt="Photo of Stump Grinder"/>
+    <div class="col-lg-4 col-md-4"><a href="/3-pt-bale-spears"><img class="img-responsive" src="/images/bale-handling/3pt-Bale-Spears.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/3-pt-bale-spears"><strong>3-PT. Bale Spears</strong></a></h3>
+        <p>Our 3-pt. mounted bale spears are designed depending on model, to handle up to 3,000 lbs. bales. The line features forged spring steel spears designed for hard center bales allowing easier spear penetration of and withdrawal from the bale(s).
+        </p>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <h3>Features</h3>
-        <ul>
-            <li>Maneuverable - goes anywhere a tractor can; gets into tight places.</li>
-            <li>Higher productivity via it's unique cutting wheel design.</li>
-            <li>Cutting wheel design provides a smoother cutting action.</li>
-            <li>Easy to operate - hydraulic controls allow easy operation from the tractor seat using existing tractor hydraulic controls.</li>
-            <li>Durable carbide-tipped cutting teeth are economical and easy to replace.</li>
-            <li>Harden bushings used in major pivot points for increased reliability.</li>
-            <li>Grease zerks provided at major pivot points for extended service life.</li>
-            <li>SureFoot stabilizer bar provides solid footing during operation.</li>
-            <li>Rubber deflector helps control chips and debris during grinding.</li>
-            <li>Parking stand for easy storage.</li>
-            <li>Tractor models require two pair of remote hydraulic outlets</li>
-            <li><a href="http://worksaver.com/literature/land%20management.pdf">Download the literature pdf.</a></li>
-        </ul>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img class="img-responsive" src="/images/sgfoot.jpg" alt="Photo of Stump Grinder Stabilizer Bar"/>
+    <div class="col-lg-4 col-md-4"><a href="/front-loader-attachments"><img class="img-responsive" src="/images/bale-handling/FLBS-Series_LoaderAttachment.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/front-loader-attachments"><strong>Front Loader Attachments</strong></a></h3>
+        <p>Designed for Use on Tractor Front Loaders. These attachments require that the front loader bucket be removed to install. Ideal for the operator who is moving round bales on a daily basis.
+        </p>
     </div>
 </div>
-
-
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h3>Features</h3>
-        <ul>
-            <li>Maneuverable - goes anywhere a tractor can; gets into tight places.</li>
-            <li>Higher productivity via it's unique cutting wheel design.</li>
-            <li>Cutting wheel design provides a smoother cutting action.</li>
-            <li>Easy to operate - hydraulic controls allow easy operation from the tractor seat using existing tractor hydraulic controls.</li>
-            <li>Durable carbide-tipped cutting teeth are economical and easy to replace.</li>
-            <li>Harden bushings used in major pivot points for increased reliability.</li>
-            <li>Grease zerks provided at major pivot points for extended service life.</li>
-            <li>SureFoot stabilizer bar provides solid footing during operation.</li>
-            <li>Rubber deflector helps control chips and debris during grinding.</li>
-            <li>Parking stand for easy storage.</li>
-            <li>Tractor models require two pair of remote hydraulic outlets</li>
-            <li><a href="http://worksaver.com/literature/land%20management.pdf">Download the literature pdf.</a></li>
-        </ul>
+    <div class="col-lg-4 col-md-4"><a href="/bale-clamping-attachments"><img class="img-responsive" src="/images/bale-handling/UBH-Series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/bale-clamping-attachments"><strong>Bale Clamping Attachments</strong></a></h3>
+        <p>Specialty front loader hay handling attachments. These attachments require that the front loader bucket be removed to install unit.
+        </p>
     </div>
 </div>
-
-<table class='table table-responsive table-bordered table-striped'>
-    <tbody>
-        <tr class="table-col-header"><td align="center" colspan="3">Stump Grinder Specifications</td></tr>
-        <tr class="table-col-modelnum"><td></td><td>SG-26A</td><td>SG-36A</td></tr>
-        <tr><td>Hitch</td><td>Cat. I</td><td>Cat. I/II</td></tr>
-        <tr><td>Overall Estimated Length (in/cm)</td><td>75 / 191</td><td>80 / 230</td></tr>
-        <tr><td>Drive System Protection</td><td>Slip Clutch</td><td>Slip Clutch</td></tr>
-        <tr><td>Gearbox Rating/Ratio</td><td>60 hp / 1:1.5</td><td>100 hp / 1:1.46</td></tr>
-        <tr><td>Cutting Wheel Construction</td><td colspan="2">Welded 2-piece Laminated Steel</td></tr>
-        <tr><td>Cutting Wheel Thickness (in/cm)</td><td>3/4 / 1.91</td><td>7/8 / 2.22</td></tr>
-        <tr><td>Cutting Wheel Diameter (in/cm)</td><td>26 /66</td><td>36 / 91</td></tr>
-        <tr><td>Cutting Wheel Speed</td><td>810 RPM</td><td>788 RPM</td></tr>
-        <tr><td>@ 540 PTO RPM</td><td>5,515 ft. / min.</td><td>7,430 ft. / min.</td></tr>
-        <tr><td>Number of Teeth</td><td>33</td><td>42</td></tr>
-        <tr><td>Cutting Teeth Construction</td><td colspan="2">Carbide Steel Bolt-In</td></tr>
-        <tr><td>Cut Height Above Ground (in/cm)</td><td>18 / 46</td><td>18 / 46</td></tr>
-        <tr><td>Cut Depth Below Ground (in/cm)</td><td>10 / 25</td><td>15 / 38</td></tr>
-        <tr><td>Maximum Horizontal Cut (in/cm)</td><td>28 / 71</td><td>33 / 84</td></tr>
-        <tr><td>Swing Arc</td><td>45&deg;</td><td>45&deg;</td></tr>
-        <tr><td>Minimum Power / Flow Required</td><td>22-PTO HP</td><td>45-PTO HP</td></tr>
-        <tr><td>Maximum Power / Flow</td><td>50-PTO HP</td><td>100-PTO HP</td></tr>
-        <tr><td>Driveline</td><td>PTO / Series 5</td><td>PTO / Series 6</td></tr>
-        <tr><td>PTO Speed</td><td>540 RPM</td><td>540 RPM</td></tr>
-        <tr><td>Weight (lbs/kg)</td><td>660 / 299</td><td>880 / 399</td></tr>
-        <tr><td>Product No.</td><td>812200</td><td>812210</td></tr>
-        <tr><td>Parts List</td><td><a href="26stumpman.html">SG-26 Part List</a></td><td><a href="36stumpman.html">SG-36 Part List</a></td>
-        <tr><td align="center" colspan="3"><a href="/templates/worksaver/manuals/wssg26-36.pdf">Download Manual</a></td></tr>
-        <tr><td colspan="3"><i>* Be Sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</i></td></tr>
-    </tbody>
-</table>
-
-<ul class='caution-icon'>
-    <li>Read and understand Operator's Manual before operation.</li>
-    <li>Stump Grinder will put a heavy load on the rear of your tractor.  Be sure front of tractor is properly counterbalanced with weights.</li>
-    <li>Never allow children or bystanders around tractor or stump grinder when you are using equipment.</li>
-</ul>
-<br />
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/skid-steer-bale-attachments"><img class="img-responsive" src="/images/bale-handling/SkidSteer_FrontLoader.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/skid-steer-bale-attachments"><strong>Skid Steer Bale Attachments</strong></a></h3>
+        <p>Designed for use on Skid Steer Loaders. These attachments are manufactured to interface with the "universal" skid steer quick attach mounting system. They may also be used on tractor loaders that use the "universal" skid steer quick attach mounting system for their attachments
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/bucket-bale-attachments"><img class="img-responsive" src="/images/bale-handling/Bucket_Bale_Spears.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/bucket-bale-attachments"><strong>Bucket Bale Attachments</strong></a></h3>
+        <p>Designed for use on Front Loader Buckets. These attachments allow the operator to leave the bucket installed when handling round bales. Ideal for the operator who is looking for an economical way to move a few round bales.
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/integrated-frame-bale-spears"><img class="img-responsive" src="/images/bale-handling/Integrated_Frame_for_JD.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/integrated-frame-bale-spears"><strong>Integrated-Frame Bale Spears</strong></a></h3>
+        <p>    Integrated-Frame designed hay attachments. These attachments feature all welded frame design, that incorporates the interfacing quick attach brackets welded to the unit. The attachments are loader model specific.
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/round-bale-feeder"><img class="img-responsive" src="/images/bale-handling/RW_SpinOff.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/round-bale-feeder"><strong>Round Bale Feeder</strong></a></h3>
+        <p>Designed as an efficient hay management tool for moving and feeding round bales. The Spin Off hydraulically rotates the bale, unwinding the hay in to a windrow.
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/replacement-forged-spears"><img class="img-responsive" src="/images/bale-handling/Replacement_SpearKits.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/replacement-forged-spears"><strong>Replacement Forged Spears & Spear Kits</strong></a></h3>
+        <p>
+        </p>
+    </div>
+</div>
--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -5,20 +5,24 @@
 </div>
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+        <h4>Brush Assembly</h4>
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/SS Brush Assembly Dwg.gif">
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+        <h4>Brush Assembly Part List</h4>
+        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/SS Brush Assembly1Lst.gif">
     </div>
 </div>
 
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <h4>Skid Steer Broom Mount</h4>
         <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <h4></h4>
         <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
     </div>
 </div>
