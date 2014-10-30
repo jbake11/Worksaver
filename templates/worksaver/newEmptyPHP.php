e9d3e104a9e75e6dc606d59cded33efaba75a916
jbake11 <jeffbake40@gmail.com>
2014-10-30 15:30:49 -0400

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,67 +1,45 @@
-<h3>Need help handling dry or wrapped bales?<br />
-    Worksaver has the answer</h3>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4>Models SBFL-2790A and SBFL-27108A</h4>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
 
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/3-pt-bale-spears"><img class="img-responsive" src="/images/bale-handling/3pt-Bale-Spears.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/3-pt-bale-spears"><strong>3-PT. Bale Spears</strong></a></h3>
-        <p>Our 3-pt. mounted bale spears are designed depending on model, to handle up to 3,000 lbs. bales. The line features forged spring steel spears designed for hard center bales allowing easier spear penetration of and withdrawal from the bale(s).
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/front-loader-attachments"><img class="img-responsive" src="/images/bale-handling/FLBS-Series_LoaderAttachment.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/front-loader-attachments"><strong>Front Loader Attachments</strong></a></h3>
-        <p>Designed for Use on Tractor Front Loaders. These attachments require that the front loader bucket be removed to install. Ideal for the operator who is moving round bales on a daily basis.
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/bale-clamping-attachments"><img class="img-responsive" src="/images/bale-handling/UBH-Series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/bale-clamping-attachments"><strong>Bale Clamping Attachments</strong></a></h3>
-        <p>Specialty front loader hay handling attachments. These attachments require that the front loader bucket be removed to install unit.
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/skid-steer-bale-attachments"><img class="img-responsive" src="/images/bale-handling/SkidSteer_FrontLoader.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/skid-steer-bale-attachments"><strong>Skid Steer Bale Attachments</strong></a></h3>
-        <p>Designed for use on Skid Steer Loaders. These attachments are manufactured to interface with the "universal" skid steer quick attach mounting system. They may also be used on tractor loaders that use the "universal" skid steer quick attach mounting system for their attachments
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/bucket-bale-attachments"><img class="img-responsive" src="/images/bale-handling/Bucket_Bale_Spears.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/bucket-bale-attachments"><strong>Bucket Bale Attachments</strong></a></h3>
-        <p>Designed for use on Front Loader Buckets. These attachments allow the operator to leave the bucket installed when handling round bales. Ideal for the operator who is looking for an economical way to move a few round bales.
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/integrated-frame-bale-spears"><img class="img-responsive" src="/images/bale-handling/Integrated_Frame_for_JD.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/integrated-frame-bale-spears"><strong>Integrated-Frame Bale Spears</strong></a></h3>
-        <p>    Integrated-Frame designed hay attachments. These attachments feature all welded frame design, that incorporates the interfacing quick attach brackets welded to the unit. The attachments are loader model specific.
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/round-bale-feeder"><img class="img-responsive" src="/images/bale-handling/RW_SpinOff.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/round-bale-feeder"><strong>Round Bale Feeder</strong></a></h3>
-        <p>Designed as an efficient hay management tool for moving and feeding round bales. The Spin Off hydraulically rotates the bale, unwinding the hay in to a windrow.
-        </p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/replacement-forged-spears"><img class="img-responsive" src="/images/bale-handling/Replacement_SpearKits.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/replacement-forged-spears"><strong>Replacement Forged Spears & Spear Kits</strong></a></h3>
-        <p>
-        </p>
+                <tr><td class="table-col-img" colspan="4"><img class="img-responsive col-xs-12" src="/images/partslist/snowblades/SBFL-2790-27108.gif" /></td></tr>
+
+                <tr class="table-col-header"><td>Ref. No.</td><td>Part No.</td><td>Description</td><td>No. Reqd.</td></tr>
+                <tr><td>1</td><td>360001</td><td>Moldboard Assembly w/cutting edge - 90"</td><td>1</td></tr>
+                <tr><td>360071</td><td>Moldboard Assembly w/cutting edge -108"</td><td>1</td></tr>
+                <tr><td>2</td><td>360010</td><td>Cutting Edge - 90"</td><td>1</td></tr>
+                <tr><td>360076</td><td>Cutting Edge - 108"</td><td>1</td></tr>
+                <tr><td>3</td><td>2503102</td><td>Bolt 1/2" - 13NC x 1 1/2" Carriage Head</td><td>Va.</td></tr>
+                <tr><td>4</td><td>2502002</td><td>Washer 1/2" Springlock</td><td>Va.</td></tr>
+                <tr><td>5</td><td>2500003</td><td>Nut 1/2" - 13NC Full Hex</td><td>Va.</td></tr>
+                <tr><td>6</td><td>360046</td><td>Pivot Box Weldment</td><td>1</td></tr>
+                <tr><td>7</td><td>360018</td><td>Bolt, Blade Pivot (Special)</td><td>2</td></tr>
+                <tr><td>8</td><td>360095</td><td>Bushing, Oscillation Pivot</td><td>3</td></tr>
+                <tr><td>9</td><td>2500037</td><td>Nut, Hex Nyloc 3/4 - 10NC</td><td>5</td></tr>
+                <tr><td>10</td><td>2504200</td><td>Machinery Bushing, 1.25" ID x 10Ga</td><td>3</td></tr>
+                <tr><td>11</td><td>360093</td><td>A-Frame Sub-Weldment w/Plate</td><td>1</td></tr>
+                <tr><td>12</td><td>360060</td><td>Adjustment Pin Assembly</td><td>1</td></tr>
+                <tr><td>13</td><td>360062</td><td>Main Pivot Pin Assembly</td><td>1</td></tr>
+                <tr><td>14</td><td>2504247</td><td>Spring, Extension 2 3/4" OD x 18 3/4"Lg</td><td>2</td></tr>
+                <tr><td>15</td><td>2503327</td><td>Eye Bolt 1/2" - 13NC x 7"</td><td>2</td></tr>
+                <tr><td>16</td><td>346751</td><td>Skid Shoe Weldment</td><td>2</td></tr>
+                <tr><td>17</td><td>346746</td><td>Spacer, Adjustment 3/4"</td><td>6</td></tr>
+                <tr><td>18</td><td>2501034</td><td>Washer 1 1/4" Flat</td><td>8</td></tr>
+                <tr><td>19</td><td>590006</td><td>Linch Pin 7/16" (AK-14)</td><td>2</td></tr>
+                <tr><td>20</td><td>2504068</td><td>Cotter Pin 1/4" x 1 3/4"</td><td>3</td></tr>
+                <tr><td>21</td><td>2501014</td><td>Washer 1" Flat</td><td>3</td></tr>
+                <tr><td>22</td><td>2501003</td><td>Washer 1/2" Flat</td><td>2</td></tr>
+                <tr><td>23</td><td>590000</td><td>Clip, Hair Pin 1/4" (A-74740)</td><td>1</td></tr>
+                <tr><td>24</td><td>2504049</td><td>Drive Zerk 5/16"</td><td>3</td></tr>
+                <tr><td>25</td><td>360038</td><td>FL Frame Weld with Oscillation Plate</td><td>1</td></tr>
+                <tr><td>26</td><td>2503085</td><td>Bolt, Hex 3/4" - 10 x 2.50 G5</td><td>3</td></tr>
+            </tbody>
+        </table>
     </div>
 </div>
