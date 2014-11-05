d324b9d820f198f4ef42428c526cb761f237d369
jbake11 <jeffbake40@gmail.com>
2014-11-04 22:14:04 -0500

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,112 +1,34 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Rotary Brooms For Skid Steers &amp; 3-Pt. Tractor Mount</h3>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h4>Features</h4>
-        <ul>
-            <li>Top quality rotary brooms for use by contractors, municipalities, institutions, and farmers .</li>
-            <li>Remove dirt, sand, gravel and trash from construction entrances, parking lots, bridges, driveways and sidewalks.</li>
-            <li>Remove thatch and prepare turf for the growing season.</li>
-            <li>Clean snow or leaves from streets, driveways, and buildings, and building entrances.</li>
-            <li>Excellent for general clean up before sealing or repairing pavement.</li>
-            <li><a href="/templates/worksaver/literature/wsr6481_Land Management Equipment brochure.pdf">Literature</a></li>
-        </ul>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img src="/images/Broom 3.jpg" alt="Photo of Rotary Broom" />
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <ul>
-            <li>PTO driven 3-pt. tractor mount in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.</li>
-            <li>Double gearbox drive (Patented No. 8,117,705) for smooth power tranmission and easy reversal of brush rotation.</li>
-            <li>Broom angles 30° right and left.</li>
-            <li>Parking stands standard.</li>
-        </ul>
-    </div>
-
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <hr class="hor-line">
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <ul>
-            <li>Hydraulically driven skid steer models in 5-ft., 6-ft., &amp; 7-ft. widths with 32" diameter brushes.</li>
-            <li>Variable flow control valve for optimum broom speed.</li>
-            <li>Step-through mount design for easy in/out of skid steer cab.</li>
-            <li>Broom angles 30° right or left.</li>
-            <li>Parking stands standard.</li>
-            <li>32" Diameter convoluted brushes available in Poly (P) or both Poly and Wire (PW).</li>
-            <li>Height adjustment chains maintain even operating position.</li>
-            <li>Parking stand included as standard equipment.</li>
-            <li>Manual or Hydraulic Angle Kits available.</li>
-        </ul>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6">
-        <img src="/images/Broom Lit.jpg" alt="Photo of Rotary Broom" />
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr class="table-col-header"><td align="center" colspan="9">Skid Steer Rotary Broom Specifications</td></tr>
-                <tr class="table-col-modelnum"><td>&nbsp;</td><td>SSMB-325P</td><td>SSMB-325PW</td><td>SSMB-326P</td><td>SSMB-326PW</td><td>SSMB-327P</td><td>SSMB-327PW</td></tr>
-                <tr><td>Overall Width (in/cm)</td><td>69 / 175</td><td>69 / 175</td><td>81 / 206</td><td>81 / 206</td><td>93 / 236</td><td>93 / 236</td></tr>
-                <tr><td>Brush Diameter (in/cm)</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td></tr>
-                <tr><td>Sweeping Width (in/cm)</td><td>60 / 152</td><td>60 / 152</td><td>72 / 183</td><td>72 / 183</td><td>84 / 213</td><td>84 / 213</td></tr>
-                <tr><td>Width @ 30° Angle (in/cm)</td><td>53 / 135</td><td>53 / 135</td><td>64 / 163</td><td>64 / 163</td><td>76 / 193</td><td>76 / 193</td></tr>
-                <tr><td>Mounting System</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td><td>Skid Steer</td></tr>
-                <tr><td>Brush Material</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td></tr>
-                <tr><td>Hydraulic Motor Rating</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td><td>16 GPM</td></tr>
-                <tr><td>Oil Flow Requirement (gpm/lpm)</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td>12-30 / 45-114</td><td> 12-30 / 45-114</td><td>12-30 / 45-114</td></tr>
-                <tr><td>Dirt Deflector</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>640 / 290</td><td>637 / 289</td><td>740 / 336</td><td>736 / 334</td><td>840 / 381</td><td>833 / 378</td></tr>
-                <tr><td>Product No.</td><td>610961</td><td>610960</td><td>610966</td><td>610965</td><td>610971</td><td>610970</td></tr>
-                <tr><td>&nbsp;</td>
-                    <td colspan="3"><a href="/skid-steer-rotary-broom-parts-list">Skid Steer Rotary Broom Parts List</a></td>
-                    <td colspan="3"><a href="/templates/worksaver/manuals/SSBroom.pdf">Skid Steer Rotary Broom Manual</a></tr>
-            </tbody>
-        </table>
-        <br />
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody><tr class="table-col-header"><td align="center" colspan="9">3-Pt. Rotary Broom Specifications</td></tr>
-                <tr class="table-col-modelnum"><td>&nbsp;</td><td>RMB-325P</td><td>RMB-325PW</td><td>RMB-326P</td><td>RMB-326PW</td><td>RMB-327P</td><td>RMB-327PW</td></tr>
-                <tr><td>Overall Width (in/cm)</td><td>69 / 175</td><td>69 / 175</td><td>81 / 206</td><td>81 / 206</td><td>93 / 236</td><td>93 / 236</td></tr>
-                <tr><td>Brush Diameter (in/cm)</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td><td>32 / 81</td></tr>
-                <tr><td>Sweeping Width (in/cm)</td><td>60 / 152</td><td>60 / 152</td><td>72 / 183</td><td>72 / 183</td><td> 84 / 213</td><td> 84 / 213</td></tr>
-                <tr><td>Width @ 30° Angle (in/cm)</td><td>53 / 135</td><td>53 / 135</td><td>64 / 163</td><td>64 / 163</td><td>76 / 193</td><td>76 / 193</td></tr>
-                <tr><td>Mounting System</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td><td>3-pt. (Cat. I)</td></tr>
-                <tr><td>Brush Material</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td><td>Poly</td><td>Poly / Wire</td></tr>
-                <tr><td>PTO RPM Range</td><td>540</td><td>540</td><td>540</td><td>540</td><td>540</td><td>540</td></tr>
-                <tr><td>Dirt Deflector</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>720 / 326</td><td>714 / 324</td><td>780 / 354</td><td>774 / 351</td><td>840 / 381</td><td>833 / 378</td></tr>
-                <tr><td>Product No.</td><td>610705</td><td>610700</td><td>610755</td><td>610750</td><td>610805</td><td>610800</td></tr>
-                <tr><td>&nbsp;</td>
-                    <td colspan="3"><a href="/3-pt-rotary-brooms-parts-list">3-Pt. Rotary Broom Parts List</a></td>
-                    <td colspan="3"><a href="/templates/worksaver/manuals/broom.pdf">3-Pt. Rotary Broom Manual</a></tr>
 
-            </tbody>
-        </table>
-        <br />
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header"><td align="center" colspan="3">Angle Adjustment Kits</td></tr>
-                <tr><td>Item #610702</td><td>Manual Angle Adjustment Kit for either skid steers or tractor 3-pt.</td><td><a href="AdjustmentLinkPartsman.html">Parts List</a></td></tr>
-                <tr><td>Item #610703</td><td>3-pt. Hydraulic Angle Adjustment Kit - includes cylinder &amp; hoses less tips.</td><td><a href="optHydAngleKitPartsman.html">Parts List</a></td></tr>
-                <tr><td>Item #610940</td><td>Skid steer Hydraulic Angle Adjustment Kit - includes elec. hydraulic solenoid control valve.  For skid steer brooms used on units with only one set of remotes.  Includes cylinder, hoses, valve, &amp; flat-faced couplers.  Factory installed.. </td><td><a href="optsolenoidFactoryPartsman.html">Parts List</a></td></tr>
-                <tr class="table-col-header"><td align="center" colspan="3">Electric Solenoid Kit(Optional)</td></tr>
-                <tr><td>Item #610950</td><td>Field installed electric hydraulic solenoid valve kit - fits #610703 hydraulic angle kit above.</td><td><a href="optsolenoidElecPartsman.html">Parts List</a></td></tr>
+                <tr><td class="table-col-img" colspan="4"><img class="img-responsive col-xs-12" src="/images/partslist/rotarybrooms/optional/Adjustment Link.gif" /></td></tr>
+                <tr><td>Ref. No.</td><td>Part No.</td><td>Description</td><td>No. Req'd.</td></tr>
+                <tr><td>&nbsp;</td><td>610702</td><td>Complete Manual Angle Kit</td><td>1</td></tr>
+                <tr><td>1</td><td>610626</td><td>Link, Manual Angle Male</td><td>1</td></tr>
+                <tr><td>2</td><td>610831</td><td>Link, Manual Angle Female</td><td>1</td></tr>
+                <tr><td>3</td><td>590194</td><td>Lock Pin 5/16" (AK-2907)</td><td>1</td></tr>
+
+
+                <tr><td></td><td></td><td></td><td></td></tr>
+                <tr><td>&nbsp;</td><td></td><td></td><td></td></tr>
+                <tr><td></td><td></td><td></td><td></td></tr>
+                <tr><td></td><td></td><td></td><td></td></tr>
+                <tr><td></td><td></td><td></td><td></td></tr>
+
+
+                Ref. No.	Part No.	Description	No. Reqd.
+                610703	Hydraulic Angle Adjustment Kit	1
+                1	2504256	Hydraulic Cylinder 2 x 6	1
+                2	2505517	Reducer Bushing 3/8" - 1/4"	1
+                3	2505626	Reducer Bushing 3/8" - 1/4" w/.040 Orifice	1
+                4	2505715	90º Elbow 9/16" - 18 (JIC) to 1/4" NPTF	2
+                5	2505676	Hydraulic Hose 1/4" x 60"	1
+                6	2505678	Hydraulic Hose 1/4" x 72"	1
+
+
             </tbody>
         </table>
     </div>
 </div>
-
-
