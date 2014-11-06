4c945256b989c7d9a08032259b4a00abc8b6af75
jbake11 <jeffbake40@gmail.com>
2014-11-05 20:36:58 -0500

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,33 +1,33 @@
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <img class="img-responsive col-xs-12" src="/images/powerlandscaperakes/25series.jpg" alt="" />
+    </div>
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        Providing ingenious solutions for your site prep needs, the 40-Series of 3-pt. mounted Power Rakes are matched to trators of 35-75 HP. Standard features include PTO with slip clutch for sure power transfer, double #80 chain drive for long service life, rugged 4" x 4" x 1/4" frame, dual flip-up gauge wheel on 5-bolt hubs and heavy-duty adjustable material bar to control processed material flow.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-
-                <tr><td class="table-col-img" colspan="4"><img class="img-responsive col-xs-12" src="/images/partslist/rotarybrooms/optional/Adjustment Link.gif" /></td></tr>
-                <tr><td>Ref. No.</td><td>Part No.</td><td>Description</td><td>No. Req'd.</td></tr>
-                <tr><td>&nbsp;</td><td>610702</td><td>Complete Manual Angle Kit</td><td>1</td></tr>
-                <tr><td>1</td><td>610626</td><td>Link, Manual Angle Male</td><td>1</td></tr>
-                <tr><td>2</td><td>610831</td><td>Link, Manual Angle Female</td><td>1</td></tr>
-                <tr><td>3</td><td>590194</td><td>Lock Pin 5/16" (AK-2907)</td><td>1</td></tr>
-
-
-                <tr><td></td><td></td><td></td><td></td></tr>
-                <tr><td>&nbsp;</td><td></td><td></td><td></td></tr>
-                <tr><td></td><td></td><td></td><td></td></tr>
-                <tr><td></td><td></td><td></td><td></td></tr>
-                <tr><td></td><td></td><td></td><td></td></tr>
-
-
-                Ref. No.	Part No.	Description	No. Reqd.
-                610703	Hydraulic Angle Adjustment Kit	1
-                1	2504256	Hydraulic Cylinder 2 x 6	1
-                2	2505517	Reducer Bushing 3/8" - 1/4"	1
-                3	2505626	Reducer Bushing 3/8" - 1/4" w/.040 Orifice	1
-                4	2505715	90º Elbow 9/16" - 18 (JIC) to 1/4" NPTF	2
-                5	2505676	Hydraulic Hose 1/4" x 60"	1
-                6	2505678	Hydraulic Hose 1/4" x 72"	1
-
-
+                <tr><td align="center" colspan="7" class="table-col-header">Specifications - 40-Series for Tractors 35-75 HP
+                <tr><td>Model</td><td>PTHD-640</td><td>PTHD-740</td><td>PTHD-840</td><td>PTHD-640ST</td><td>PTHD-740ST</td><td>PTHD-840ST
+                <tr><td>Mount Type</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II
+                <tr><td>Main Frame (in)</td><td>4 x 4 x 1/4</td><td>4 x 4 x 1/4</td><td>4 x 4 x 1/4</td><td>4 x 4 x 1/4</td><td>4 x 4 x 1/4</td><td>4 x 4 x 1/4
+                <tr><td>Overall Width (in)</td><td>80</td><td>92</td><td>104</td><td>80</td><td>92</td><td>104
+                <tr><td>Working Width (in)</td><td>72</td><td>84</td><td>96</td><td>72</td><td>84</td><td>96
+                <tr><td>Working Depth (in)</td><td>0-2.5</td><td>0-2.5</td><td>0-2.5</td><td>0-2.5</td><td>0-2.5</td><td>0-2.5</td></tr>
+                <tr><td>Max. Angle</td><td>20° L/R</td><td>20° L/R</td><td>20° L/R</td><td>20° L/R</td><td>20° L/R</td><td>20° L/R</td></tr>
+                <tr><td>Hydraulic Angle</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td><td>Optional</td></tr>
+                <tr><td>Drive System</td><td>PTO</td><td>PTO</td><td>PTO</td><td>PTO</td><td>PTO</td><td>PTO</td></tr>
+                <tr><td>Rotor Drive</td><td>#80 Roller Chain</td><td>#80 Roller Chain</td><td>#80 Roller Chain</td><td>#80 Roller Chain</td><td>#80 Roller Chain</td><td>#80 Roller Chain</td></tr>
+                <tr><td>Chain Tensioner</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td></tr>
+                <tr><td>Rotor Tooth</td><td>Carbide</td><td>Carbide</td><td>Carbide</td><td>2" Sabre</td><td>2" Sabre</td><td>2" Sabre</td></tr>
+                <tr><td>Gauge Wheel</td><td>Dual</td><td>Dual</td><td>Dual</td><td>Dual</td><td>Dual</td><td>Dual</td></tr>
+                <tr><td>Flip-Up Side Shields</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td><td>Standard</td></tr>
+                <tr><td>Material Control Bar</td><td>Adjustable</td><td>Adjustable</td><td>Adjustable</td><td>Adjustable</td><td>Adjustable</td><td>Adjustable</td></tr>
+                <tr><td>Weight (lbs)</td><td>1065</td><td>1160</td><td>1255</td><td>1068</td><td>1162</td><td>1257
+                <tr><td>Product No.</td><td>360870</td><td>360875</td><td>360880</td><td>360885</td><td>360890</td><td>360895
             </tbody>
         </table>
     </div>
--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,38 +1,64 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Hydraulic Driven</h4>
+        <p>Powered rakes are ideal tool for the landscape contractor, large property owner, rental yard, park &amp; recreation department or turf management professional. They do it all, prep, smooth, set grade, level, till, remove rocks and create the perfect seedbed making this tool a game changer.</p>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <h4>Brush Assembly</h4>
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/SS Brush Assembly Dwg.gif">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+        <h5>Features</h5>
+        <ul>
+            <li>Flip-up side shields are self-storing on the unit allowing ease of windrowing rocks or debris.</li>
+            <li>Flip-up gauge wheels (height adjustable) allows accurate control of working depth keeping machine level for smoother surface finish. Easily flip up out of the way for transport on a trailer.</li>
+            <li>Replaceable bearing housings and triple seal bearings keep dirt out, providing more uptime.</li>
+            <li>Choice of rotors - 2" Sabre tooth or 1-3/4" Carbide Tooth.</li>
+            <li>Dry Chain Case, environmentally friendly reduces maintenance as there is no oil to contend with.</li>
+        </ul>
     </div>
-
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <h4>Brush Assembly Part List</h4>
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/SS Brush Assembly1Lst.gif">
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <h4>Skid Steer Broom Mount</h4>
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
-    </div>
-
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <h4></h4>
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img class="img-responsive" src="/images/powerlandscaperakes/Carbide rotor WS.jpg" alt="" />
+        <h5>Carbide Rotor</h5>
+        <img class="img-responsive" src="/images/powerlandscaperakes/Sabre rotor.jpg" alt="" />
+        <h5>Sabre Rotor</h5>
     </div>
 </div>
-
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/05_PowerRake.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>05 Series</strong></a></h3>
+        <p>Designed for use on Mini / Compact Skid Steers, the hydraulically driven 05-Series offers economy and performance.</p>
     </div>
-
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <img class="img-responsive" src="/images/partslist/rotarybrooms/ss/">
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/10series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>10 Series</strong></a></h3>
+        <p>Elevating productivity, the 10-Series Power Rakes for Mini / Compact Skid Steers features a #80 chain drive, flip-up side shields, dual gauge wheels, adjustable material bar and optional hydraulic angle.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/20series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>20 Series</strong></a></h3>
+        <p>Responsive and reliable, the 20-Series is designed for Skid Steers up to 65 HP.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/65series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>65 Series</strong></a></h3>
+        <p>Consistently delivering high performance, the 65-series is designed for Skid Steers over 65 HP.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/25series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>25 Series</strong></a></h3>
+        <p>Get more done with a compact tractor when using one of our 25-Series Power Rakes designed for tractors of 24-40 HP.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/40series.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>40 Series</strong></a></h3>
+        <p>Providing ingenious solutions for your site prep needs, the 40-Series of 3-pt. mounted Power Rakes are matched to trators of 35-75 HP.</p>
     </div>
 </div>
