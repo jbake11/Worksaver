a1829e10d91734f28dc4171d5d48b363fcebe7fc
jbake11 <jeffbake40@gmail.com>
2014-11-05 20:55:34 -0500

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,15 +1,72 @@
 <div class="row">
     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img class="img-responsive col-xs-12" src="/images/powerlandscaperakes/25series.jpg" alt="" />
+        <img class="img-responsive col-xs-12" src="/images/partslist/cementmixer/mixer1.gif" alt="" />
     </div>
     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-        Providing ingenious solutions for your site prep needs, the 40-Series of 3-pt. mounted Power Rakes are matched to trators of 35-75 HP. Standard features include PTO with slip clutch for sure power transfer, double #80 chain drive for long service life, rugged 4" x 4" x 1/4" frame, dual flip-up gauge wheel on 5-bolt hubs and heavy-duty adjustable material bar to control processed material flow.
+        <h4>Model #580 Cement Mixer</h4>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
+
+                <tr><td align="center" colspan="7" class="table-col-header">Ref. No	Part No.	Description	No. Req'd
+                <tr><td>1	780621	Drum, Main	1
+                <tr><td>2	780622	Drum, Cone	1
+                <tr><td>3	780623	Paddle, Mixing	3
+                <tr><td>4	2503277	Bolt, M14 x 30 Gr.8.8 Hex Head	2
+                <tr><td>5	2502021	Washer, M14 External Star	2
+                <tr><td>6	780627	Washer, Inner	1
+                <tr><td>7	2503269	Bolt, M12 x 25 Gr.8.8 Hex Head	6
+                <tr><td> 8	2501027	Washer, M12 Flat	3
+                <tr><td>9	2503269	Bolt, M12 x 25 Gr.8.8 Hex Head	6
+                <tr><td>10	2501027	Washer, M12 Flat	6
+                <tr><td>11	2500086	Nut, M12 Hex	8
+                <tr><td>12	2503278	Bolt, M8 x 16 Gr.8.8 Hex Head	18
+                <tr><td>13	2501029	Washer, M8 Flat	36
+                <tr><td>14	2500088	Nut, M8 Hex	18
+                <tr><td>15	780624	Chain, #40 (142 Pitches)	1
+                <tr><td>16	780625	Sprocket Assembly, Drum	1
+                <tr><td>17	780626	Key, 14 x 9 x 50mm	1
+                <tr><td>18	780628	Shaft, Drum	1
+                <tr><td>19	780629	Bearing, Ball 6209-2RS	2
+                <tr><td>20	780630	Frame	1
+                <tr><td>21	780631	Cotter Pin	4
+                <tr><td>22	780632	Pin, Cylinder	2
+                <tr><td>23	780633	Bar, Cylinder Attachment	2
+                <tr><td>24	780634	Bushing, Cylinder	1
+                <tr><td>25	780635	Cylinder, Hydraulic-Toplink	1
+                <tr><td>26	780636	Hose, Hydraulic	1
+                <tr><td>27	2503279	Bolt, M10 x 90 Gr.8.8 Hex Head	2
+                <tr><td>28	2502019	Washer, M10 x 30	4
+                <tr><td>29	2500089	Nut, M24 Springlock	2
+                <tr><td>30	2502022	Washer, M24 Springlock	2
+                <tr><td>31	780637	Pin, Pull	2
+                <tr><td>32	780638	Knob, M8 x 20	2
+                <tr><td>33	780639	Washer, Drum Shaft Retainer	1
+                <tr><td>34	780640	Snap Ring, E D 30	2
+                <tr><td>35	780641	Bearing, Ball 6200-2RS	2
+                <tr><td>36	780642	Support, Drive Shaft	1
+                <tr><td>37	780643	Shaft, Drive w/Sprocket	1
+                <tr><td>38	780644	Tube, Stand	2
+                <tr><td>39	780645	Plug, Stand Tube	2
+                <tr><td>40	780646	Guard, Drive Shaft	1
+                <tr><td>41	2503278	Bolt, M8 x 16 Gr.8.8 Hex Head	2
+                <tr><td>42	780647	PTO Shaft "CE" Standard	1
+                <tr><td>43	2503280	Bolt, M10 x 50 Socket Head	1
+                <tr><td>44	2500090	Nut, M10 Nylock Hex	1
+                <tr><td>45	2501027	Washer, M12 x 30 Flat	3
+                <tr><td>46	2501026	Washer, M24 Flat	2
+                <tr><td>47	2500091	Nut, M8 Whiz Flange Hex	2
+                <tr><td>48	780648	Guard, PTO	1
+                <tr><td>49	780649	Support, PTO Guard	2
+                <tr><td>50	2501029	Washer, M8 Flat	2
+                <tr><td>51	2503278	Bolt, M8 x 16 Gr.8.8 Hex Head	2
+                <tr><td>52	2503281	Bolt, M8 x 20 Gr.8.8 Hex Head	2
+                <tr><td>N/S	780776	Masterlink, #40 Chain	1
+
+
                 <tr><td align="center" colspan="7" class="table-col-header">Specifications - 40-Series for Tractors 35-75 HP
                 <tr><td>Model</td><td>PTHD-640</td><td>PTHD-740</td><td>PTHD-840</td><td>PTHD-640ST</td><td>PTHD-740ST</td><td>PTHD-840ST
                 <tr><td>Mount Type</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II</td><td>3-pt Cat.I/II
