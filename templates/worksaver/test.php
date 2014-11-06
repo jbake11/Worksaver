61a704a453619f532aaf78af2d210cd229465056
jbake11 <jeffbake40@gmail.com>
2014-11-05 21:01:14 -0500

    asd

--- templates/worksaver/test.php
+++ templates/worksaver/test.php
@@ -1,64 +1,171 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <p>Powered rakes are ideal tool for the landscape contractor, large property owner, rental yard, park &amp; recreation department or turf management professional. They do it all, prep, smooth, set grade, level, till, remove rocks and create the perfect seedbed making this tool a game changer.</p>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <h5>Model #580c Cement Mixer</h5>
+        <img class="img-responsive" src="/images/concrete-mixer1.jpg" alt=""/>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <h5>Model Model SS-590 Hyd. Cement Mixer</h5>
+        <img class="img-responsive" src="/images/SS_590_1.jpg" alt=""/>
     </div>
 </div>
+
 <div class="row">
-    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-        <h5>Features</h5>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4>Main Features</h4>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <h5>Model 580c</h5>
         <ul>
-            <li>Flip-up side shields are self-storing on the unit allowing ease of windrowing rocks or debris.</li>
-            <li>Flip-up gauge wheels (height adjustable) allows accurate control of working depth keeping machine level for smoother surface finish. Easily flip up out of the way for transport on a trailer.</li>
-            <li>Replaceable bearing housings and triple seal bearings keep dirt out, providing more uptime.</li>
-            <li>Choice of rotors - 2" Sabre tooth or 1-3/4" Carbide Tooth.</li>
-            <li>Dry Chain Case, environmentally friendly reduces maintenance as there is no oil to contend with.</li>
+            <li>Two piece steel drum construction with internal paddles for uniform mixing and clean dumping.</li>
+            <li>Compact, trouble free, long life design with few moving parts.</li>
+            <li>Ideal for mixing feed, fertilizer, inoculating seed, potting soil and other mixing jobs.</li>
+            <li>Five (5) cu./ft. capacity (1 bag mix).</li>
+            <li>Hydraulic top link cylinder provides positive dump control.</li>
+            <li>Parking stand for easy storage.</li>
+            <li>Number 40-roller chain drives mixing drum.</li>
+            <li>Adjustable Cat.I hitch allows easy hook up to most tractors (7/8" Dia. Lift pins).</li>
+            <li>Sealed ball bearings provide long service life with reduced maintenance.<br>
+        </ul>
+
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <h5>Model SS-590</h5>
+        <ul>
+            <li>All welded drum construction with internal paddles for uniform mixing and clean dumping.</li>
+            <li>Compact, trouble free, long life design with few moving parts.</li>
+            <li>Hydraulic motor drive for easy operation and low maintenance requirements.</li>
+            <li>Five (5) cu./ft. capacity (1 bag mix).</li>
+            <li>Direct gearbox drive of drum</li>
+            <li>Parking stand for easy storage.</li>
+            <li>Welded main frame with “universal” skid steer mount allows easy access to the operators seat.</li>
+            <li>Flow control valve allows easy control of speed of the mixer.</li>
         </ul>
     </div>
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img class="img-responsive" src="/images/powerlandscaperakes/Carbide rotor WS.jpg" alt="" />
-        <h5>Carbide Rotor</h5>
-        <img class="img-responsive" src="/images/powerlandscaperakes/Sabre rotor.jpg" alt="" />
-        <h5>Sabre Rotor</h5>
-    </div>
 </div>
+
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/05_PowerRake.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>05 Series</strong></a></h3>
-        <p>Designed for use on Mini / Compact Skid Steers, the hydraulically driven 05-Series offers economy and performance.</p>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr>
+                    <td colspan="3">SPECIFICATIONS</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;&nbsp;&nbsp; </td>
+                    <td>Model# 580c</td>
+                    <td>Model# SS-590</td>
+                </tr>
+                <tr>
+                    <td>Capacity:</td>
+                    <td>Five (5) cu./ft. (1 bag mix)</td>
+                    <td>Five (5) cu./ft. (1 bag mix)</td>
+                </tr>
+                <tr>
+                    <td>Drum opening (in/mm):</td>
+                    <td>17.5 / (444 mm)</td>
+                    <td>20 (508)</td>
+                </tr>
+                <tr>
+                    <td>Overall drum length (in/mm):</td>
+                    <td>32 / (812 mm)</td>
+                    <td>32 / (813 mm)</td>
+                </tr>
+                <tr>
+                    <td>Drum construction:</td>
+                    <td>2-piece bolted</td>
+                    <td>Welded Steel</td>
+                </tr>
+                <tr>
+                    <td>Drive system:</td>
+                    <td>Tractor PTO</td>
+                    <td>Hydraulic</td>
+                </tr>
+                <tr>
+                    <td>PTO driveline:</td>
+                    <td>Metric Series I</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>PTO speed:</td>
+                    <td>540 rpm</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>Drum drive:</td>
+                    <td>#40 chain</td>
+                    <td>Gearbox Direct</td>
+                </tr>
+                <tr>
+                    <td>Drive ratio:</td>
+                    <td>8.77:1</td>
+                    <td>3.18:1</td>
+                </tr>
+                <tr>
+                    <td>Shield PTO drive:</td>
+                    <td>Standard</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>Chain tightner:</td>
+                    <td>Standard</td>
+                    <td>Standard</td>
+                </tr>
+                <tr>
+                    <td>Bearing type:</td>
+                    <td>Sealed Ball Bearings</td>
+                    <td>Tapered Roller</td>
+                </tr>
+                <tr>
+                    <td>Hydraulic toplink cylinder type:
+                    </td><td>Single Acting</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>Hitch requirement:</td>
+                    <td>Cat. I (7/8" Dia. Lift Pins)</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>Parking stands:</td>
+                    <td>Standard</td>
+                    <td>Standard</td>
+                </tr>
+                <tr>
+                    <td>Horse Power Rating</td>
+                    <td>25 PTO HP*</td>
+                    <td>N/A</td>
+                </tr>
+                <tr>
+                    <td>Shipping weight (lb./kg)</td>
+                    <td>253 / (115 kg)</td>
+                    <td>395 (110)</td>
+                </tr>
+                <tr>
+                    <td>Product computer number:</td>
+                    <td>780620</td>
+                    <td>780950</td>
+                </tr>
+                <tr>
+                    <td colspan="3">* Be sure front of tractor is properly counterbalanced with front-end weights or front-end loader.</td>
+                </tr>
+                <tr>
+                    <td>Parts List</td>
+                    <td><a href="/model-580-cement-mixer-part-list">#580c</a></td>
+                    <td><a href=".html">SS-590  N/A</a></td>
+                </tr>
+                <tr>
+                    <td>Manual</td>
+                    <td><a href="/manuals/580-mixer.pdf">#580c</a></td>
+                    <td><a href="/manuals/WSss590.pdf">SS-590</a></td>
+                </tr>
+                <tr>
+                    <td width="38%" colspan="3"><a href="/mix/580 Instructions.pdf">580 Mixer Assembly Instructions</a></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/10series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>10 Series</strong></a></h3>
-        <p>Elevating productivity, the 10-Series Power Rakes for Mini / Compact Skid Steers features a #80 chain drive, flip-up side shields, dual gauge wheels, adjustable material bar and optional hydraulic angle.</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/20series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>20 Series</strong></a></h3>
-        <p>Responsive and reliable, the 20-Series is designed for Skid Steers up to 65 HP.</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/65series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>65 Series</strong></a></h3>
-        <p>Consistently delivering high performance, the 65-series is designed for Skid Steers over 65 HP.</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/25series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>25 Series</strong></a></h3>
-        <p>Get more done with a compact tractor when using one of our 25-Series Power Rakes designed for tractors of 24-40 HP.</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/powerlandscaperakes/40series.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>40 Series</strong></a></h3>
-        <p>Providing ingenious solutions for your site prep needs, the 40-Series of 3-pt. mounted Power Rakes are matched to trators of 35-75 HP.</p>
-    </div>
-</div>
+
+
+
