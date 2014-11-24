2bd38961b2a1e4eb7b879911577520b6d3c78fba
jbake11 <jeffbake40@gmail.com>
2014-11-24 11:53:07 -0500

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,68 +1,58 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>3-PT. Bale Spears</h4>
-        <p>    Designed for use on tractor 3-pt. hitches only. These bale spear feature tapered forged spring steel spears allowing easier spear penetration of and withdrawal from the bale. Frames are made from rugged square tubing.</p>
+        <h4>Some Features are:</h4>
+        <li>HS-330 Triple Spear model for handling a mid-size rectangular bale.</li>
+        (Spears have a useable length of 26")
+        <li>DBH-449 Four Spear model for handling two round bales.</li>
+        (Spears have useable length of 42")
+        <li>Bolt-in forged spears for increased reliability.</li>
+        <li>Cast Steel Weld-on sockets (patent pending) for superior strength.</li>
+        <li>Cat. II & III Quick hitch compatible.</li>
     </div>
 </div>
-
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive col-xs-12" src="/images/partslist/balehandling/dbh-449-2.jpg" alt="" />
+        <h4>Model DBH-449</h4>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive col-xs-12" src="/images/partslist/balehandling/hs-330.jpg" alt="" />
+        <h4>Model HS-330</h4>
+    </div>
+</div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
-            <tbody><tr><td>Product</b></td>
-                    <td>Model</b> </td>
-                    <td>Description</td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/bsf-1523-lit.jpg" alt="Photo of BSF-1523"></td>
-                    <td><br />BSF-1523</b>
-                    </td><td>3-pt. spear for handling round bales up to 1,800 lbs. Tapered  main spear has useable length of 44” with two welded balance spears. Fits Cat I/II hitches (shipped with 7/8” pins)
-                        <br /><br /><a href="bsf1523man.html">Parts List Click Here</a> <br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/dbfs-30.gif" alt="Photo of HS-3000"></td>
-                    <td><br />HS-3000</b></td>
-                    <td>Dual bolt-in forged spears with 42” usable length. Spears are spaced 26” apart allowing bale to be picked up & not lose its orientation. Rated at 3,000 lbs. Fits Cat I/II Quick Hitches (shipped with 7/8” pins)
-                        <br /><br /><a href="hs3000man.html">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/Hs-2200.jpg" alt="Photo of HS-2200"></td>
-                    <td><br />HS-2200</b></td>
-                    <td>3-pt. Bale Spear for round bales to 2,200 lbs.  Bolt-in spear (Useable 42")  with cast steel socket (pat. pend).  Two balance spikes.  Cat I/II and quick hitch compatible.  Ships UPS.
-                        <br /><br /><a href="hs2200man.html">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-                <tr><td><a href="3pt-spear.html"><img class="img-responsive" src="/images/bale-handling/hs-330.jpg" alt="Photo of HS-330"></a></td>
-                    <td><br />HS-330</b></td>
-                    <td>3-pt. Triple bale spear rated 3,000 lbs.  Features bolt-in forged spear (26" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For mid-size square bales.<br /><br />Click on picture for more information.
-                        <br /><br /><a href="man.html">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-                <tr><td><a href="3pt-spear.html"><img class="img-responsive" src="/images/bale-handling/dbh-449-2.jpg" alt="Photo of DBH-449"></a></td>
-                    <td><br />DBH-449</b></td>
-                    <td>3-pt. Four bale spear handler rated 4,000 lbs.  Features bolt-in forged spear (42" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For two round bales.  <br /><br />Click on picture for more information.
-                        <br /><br /><a href="man.html">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/bsfa-20-lit.jpg" alt="Photo of BSFA-20"></td>
-                    <td><br />BSFA-20</b></td>
-                    <td>Adjustable height bale spear for handling round bales up to 2,000 lbs.  Utilizes a bolt-on spear with a useable length of 44".  The spear can be positioned up or down the vertical mast.  Fits Cat. I/II and is shipped with 7/8" pins.
-                        <br /><br /><a href="bsfa20man.html">Parts List Click Here</a> <br /><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
-
-
-<!--                <tr><td><img class="img-responsive" src="/images/bale-handling/F-1511_2.jpg" alt="Photo of F-1511"></td>
-                    <td>F-1511</b></td>
-                    <td>3-Pt. Bale fork rated at 1,500 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 37.5" apart.  Unit fits Cat. I/II as well as Cat II Quick Hitches.  Shipped with 7/8" pins.</b></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/F-3000.jpg" alt="Photo of F-3000"></td>
-                    <td>F-3000</b></td>
-                    <td>3-Pt. Bale fork rated at 3,000 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 46.5" apart.  Unit fits Cat. II as well as Cat II Quick Hitches.  Shipped with 1-1/8" pins.<br /><br /><a href="f3000man.html">Parts List Click Here</a></b></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/hhu2045stand.jpg" alt="Photo of HHU-2045"></td>
-                    <td>HHU-2045</b></td>
-                    <td><font size="2"><b>3-Pt Hay Handler / Unroller.  Rated at 2,000 lbs. and is designed to unroll or handle round bales either 4' or 5' in width up to 6' in diameter.  Unit Requires a 2.5" x 8" hydraulic cylinder (which is not included). The HHU-2045 now comes with a parking stand for allowing easy hook up with a Quick Hitch.
-                            <br /><br /><a href="hhu2045man.html">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hhu2045.pdf">Download Manual</a> </b><b> </b></td></tr>-->
-
-                <tr><td align="center" colspan="4"><b><br /><a href="spearkit.html">Replacement Spear Kits<br /> <br /></a></b></td></tr>
-
-
-                <tr><td align="CENTER" colspan="3"><a href="haf-op.html"><img border="0" vspace="5" src="/bin/button/htlc.gif" alt="OPTIONAL TOP LINK CYLINDERS"></a></td></tr>
+            <tbody>
+                <tr><td>Model:</td><td>DBH-449</td><td>HS-330</td></tr>
+                <tr><td>Weight Capacity (lb/kg)</td><td>251 / 114</td><td>163 / 74</td></tr>
+                <tr><td>Overall Width (in/cm)</td><td>96 / 244</td><td>60 / 152</td></tr>
+                <tr><td>Bolt-in Forged Spear Length (in/cm)</td><td>42 / 107</td><td>26 / 66</td></tr>
+                <tr><td># of Spears</td><td>4</td><td>3</td></tr>
+                <tr><td>Spear Spacing (in/cm)</td><td>26 / 66 On Center</td><td>28.5 / 72 On Center</td></tr>
+                <tr><td>Cast Steel Weld-on Socket</td><td>Yes</td><td>Yes</td></tr>
+                <tr><td>Number / Type of Bales</td><td>2 Round Bales</td><td>1 Mid-size Rectangular Bale</td></tr>
+                <tr><td>Hitch Category</td><td>Cat. II / III</td><td>Cat. II / III</td></tr>
+                <tr><td>Item Number</td><td>832610</td><td>832615</td></tr>
+                <tr><td><a href="">DBH-449 Parts List</a></td><td><a href="">HS-330 Parts List</a></td><td><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
             </tbody>
         </table>
     </div>
 </div>
 
 
+
+
+
+<!-- Move to Bale Forks                <tr><td><img class="img-responsive" src="/images/bale-handling/F-1511_2.jpg" alt="Photo of F-1511"></td>
+                    <td>F-1511</b></td>
+                    <td>3-Pt. Bale fork rated at 1,500 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 37.5" apart.  Unit fits Cat. I/II as well as Cat II Quick Hitches.  Shipped with 7/8" pins.</b></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/F-3000.jpg" alt="Photo of F-3000"></td>
+                    <td>F-3000</b></td>
+                    <td>3-Pt. Bale fork rated at 3,000 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 46.5" apart.  Unit fits Cat. II as well as Cat II Quick Hitches.  Shipped with 1-1/8" pins.<br /><br /><a href="/3-pt-bale-spear-f-3000">Parts List Click Here</a></b></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/hhu2045stand.jpg" alt="Photo of HHU-2045"></td>
+                    <td>HHU-2045</b></td>
+                    <td><font size="2"><b>3-Pt Hay Handler / Unroller.  Rated at 2,000 lbs. and is designed to unroll or handle round bales either 4' or 5' in width up to 6' in diameter.  Unit Requires a 2.5" x 8" hydraulic cylinder (which is not included). The HHU-2045 now comes with a parking stand for allowing easy hook up with a Quick Hitch.
+                            <br /><br /><a href="/3-pt-bale-spear-hhu-2045">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hhu2045.pdf">Download Manual</a> </b><b> </b></td></tr>-->
--- templates/worksaver/newEmptyPHP1.php
+++ templates/worksaver/newEmptyPHP1.php
@@ -1,8 +1,63 @@
-<?php
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h4>Designed for use on tractor 3-pt. hitches only. These bale spear feature tapered forged spring steel spears allowing easier spear penetration of and withdrawal from the bale. Frames are made from rugged square tubing.</h4>
+    </div>
+</div>
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody><tr><td>Product</b></td>
+                    <td>Model</b> </td>
+                    <td>Description</td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/bsf-1523-lit.jpg" alt="Photo of BSF-1523"></td>
+                    <td><br />BSF-1523</b>
+                    </td><td>3-pt. spear for handling round bales up to 1,800 lbs. Tapered  main spear has useable length of 44” with two welded balance spears. Fits Cat I/II hitches (shipped with 7/8” pins)
+                        <br /><br /><a href="/3-pt-bale-spear-bsf-1523">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/Hs-2200.jpg" alt="Photo of HS-2200"></td>
+                    <td><br />HS-2200</b></td>
+                    <td>3-pt. Bale Spear for round bales to 2,200 lbs.  Bolt-in spear (Useable 42")  with cast steel socket (pat. pend).  Two balance spikes.  Cat I/II and quick hitch compatible.  Ships UPS.
+                        <br /><br /><a href="/3-pt-bale-spear-hs-2200">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/dbfs-30.gif" alt="Photo of HS-3000"></td>
+                    <td><br />HS-3000</b></td>
+                    <td>Dual bolt-in forged spears with 42” usable length. Spears are spaced 26” apart allowing bale to be picked up & not lose its orientation. Rated at 3,000 lbs. Fits Cat I/II Quick Hitches (shipped with 7/8” pins)
+                        <br /><br /><a href="./3-pt-bale-spear-hs-3000">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><a href="3pt-spear.html"><img class="img-responsive" src="/images/bale-handling/hs-330.jpg" alt="Photo of HS-330"></a></td>
+                    <td><br />HS-330</b></td>
+                    <td>3-pt. Triple bale spear rated 3,000 lbs.  Features bolt-in forged spear (26" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For mid-size square bales.
+                        <br /><br /><a href="man.html">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><a href="3pt-spear.html"><img class="img-responsive" src="/images/bale-handling/dbh-449-2.jpg" alt="Photo of DBH-449"></a></td>
+                    <td><br />DBH-449</b></td>
+                    <td>3-pt. Four bale spear handler rated 4,000 lbs.  Features bolt-in forged spear (42" useable length) in case steel sockets (pat. pend.). Cat II &amp; III.  For two round bales.
+                        <br /><br /><a href="man.html">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/bsfa-20-lit.jpg" alt="Photo of BSFA-20"></td>
+                    <td><br />BSFA-20</b></td>
+                    <td>Adjustable height bale spear for handling round bales up to 2,000 lbs.  Utilizes a bolt-on spear with a useable length of 44".  The spear can be positioned up or down the vertical mast.  Fits Cat. I/II and is shipped with 7/8" pins.
+                        <br /><br /><a href="/3-pt-bale-spear-bsfa-20">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-handling/bsf-1523-lit.jpg" alt="Photo of BSF-1523"></td>
+                    <td><br />BSF-1523</b>
+                    </td><td>Four bolt-in spears, square frame design
+                        allowing transport of two 6-ft. round bales
+                        at once. Rated at 6,000 lbs. Overall unit width
+                        of 76”. Fits Cat. II/III hitch & Quick Hitches
+                        <br /><br /><a href="/3-pt-bale-spear-bsf-1523">Parts List Click Here</a><br /><a href="/templates/worksaver/manuals/hsbdh.pdf">Download Manual</a></td></tr>
+
+
+                <tr><td align="center" colspan="4"><b><br /><a href="spearkit.html">Replacement Spear Kits<br /> <br /></a></b></td></tr>
+
+
+
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
 
