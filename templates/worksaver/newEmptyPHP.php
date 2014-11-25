7689e0cc06941946535d6528cd1641853c0f2049
jbake11 <jeffbake40@gmail.com>
2014-11-24 21:07:05 -0500

    Something

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -7,41 +7,23 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr class="table-col-header"><td align="center" colspan="5">HS-2200 Part List</td></tr>
-                <tr><td class="table-col-img" colspan="5"><img class="img-responsive col-xs-12" src="/images/partslist/balehandling/HS-2200.jpg" alt="" /></td></tr>
+                <tr class="table-col-header"><td align="center" colspan="5">BSFA-20 Part List</td></tr>
+                <tr><td class="table-col-img" colspan="5"><img class="img-responsive col-xs-12" src="/images/partslist/balehandling/BSFA-20.jpg" alt="" /></td></tr>
                 <tr><td>Ref.</td><td>Part No.</td><td>Description</td><td>Req'd.</td></tr>
-                1	830921	Main Frame Weldment	1
-                830925	Spear & Socket Assembly (includes #2 thru #9)	1
-                2	830926	Socket Weldment	2
-                3	830928	Forged Spear	1
-                4	2503054	Bolt, 7/16"-14NC x 3" Hex Head Grade 5	1
-                5	2502006	Washer, 7/16" Springlock	1
-                6	2500012	Nut, 7/16"-NC Full Hex	1
-                7	830214	U-Bolt, 5/8"-11NC 3" x 4-1/2"	2
-                8	2502007	Washer, 5/8" Springlock	4
-                9	2500013	Nut, 5/8"-11NC Full Hex	4
-                10	590108	Lift Pin (AK-578)	2
-                11	101026	Safety Sign - Caution (Instructions)	1
-                12	101041	Safety Sign - Notice (2000 Lb. Capacity)	1
-
+                <tr><td>1</td><td>830921</td><td>Main Frame Weldment</td><td>1</td></tr>
+                <tr><td>&nbsp </td><td>830925</td><td>pear & Socket Assembly (includes #2 thru #9)</td><td>1</td></tr>
+                <tr><td>2</td><td>830926</td><td>Socket Weldment</td><td>2</td></tr>
+                <tr><td>3</td><td>830928</td><td>Forged Spear</td><td>1</td></tr>
+                <tr><td>4</td><td>2503054</td><td>Bolt, 7/16"-14NC x 3" Hex Head Grade 5</td><td>1</td></tr>
+                <tr><td>5</td><td>2502006</td><td>Washer, 7/16" Springlock</td><td>1</td></tr>
+                <tr><td>6</td><td>2500012</td><td>Nut, 7/16"-NC Full Hex</td><td>1</td></tr>
+                <tr><td>7</td><td>830214</td><td>U-Bolt, 5/8"-11NC 3" x 4-1/2"</td><td>2</td></tr>
+                <tr><td>8</td><td>2502007</td><td>Washer, 5/8" Springlock</td><td>4</td></tr>
+                <tr><td>9</td><td>2500013</td><td>Nut, 5/8"-11NC Full Hex</td><td>4</td></tr>
+                <tr><td>10</td><td>590108</td><td>Lift Pin (AK-578)</td><td>2</td></tr>
+                <tr><td>11</td><td>101026</td><td>Safety Sign - Caution (Instructions)</td><td>1</td></tr>
+                <tr><td>12</td><td>101041</td><td>Safety Sign - Notice (2000 Lb. Capacity)</td><td>1</td></tr>
             </tbody>
         </table>
     </div>
 </div>
-
-
-
-
-
-<!-- Move to Bale Forks                <tr><td><img class="img-responsive" src="/images/bale-handling/F-1511_2.jpg" alt="Photo of F-1511"></td>
-                    <td>F-1511</b></td>
-                    <td>3-Pt. Bale fork rated at 1,500 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 37.5" apart.  Unit fits Cat. I/II as well as Cat II Quick Hitches.  Shipped with 7/8" pins.</b></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/F-3000.jpg" alt="Photo of F-3000"></td>
-                    <td>F-3000</b></td>
-                    <td>3-Pt. Bale fork rated at 3,000 lbs. designed for moving round bales.  Patented 3" x 2" internally reinforced forks are 55" on length and spaced 46.5" apart.  Unit fits Cat. II as well as Cat II Quick Hitches.  Shipped with 1-1/8" pins.<br /><br /><a href="/3-pt-bale-spear-f-3000">Parts List Click Here</a></b></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/hhu2045stand.jpg" alt="Photo of HHU-2045"></td>
-                    <td>HHU-2045</b></td>
-                    <td><font size="2"><b>3-Pt Hay Handler / Unroller.  Rated at 2,000 lbs. and is designed to unroll or handle round bales either 4' or 5' in width up to 6' in diameter.  Unit Requires a 2.5" x 8" hydraulic cylinder (which is not included). The HHU-2045 now comes with a parking stand for allowing easy hook up with a Quick Hitch.
-                            <br /><br /><a href="/3-pt-bale-spear-hhu-2045">Parts List Click Here</a><br /><br /><a href="/templates/worksaver/manuals/hhu2045.pdf">Download Manual</a> </b><b> </b></td></tr>-->
