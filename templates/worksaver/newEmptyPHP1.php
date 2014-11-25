48cef9c45ca2a98291ba8580275d7e510e60ad7e
jbake11 <jeffbake40@gmail.com>
2014-11-25 11:51:16 -0500

    asd

--- templates/worksaver/newEmptyPHP1.php
+++ templates/worksaver/newEmptyPHP1.php
@@ -1,80 +1,47 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr><td align="center" colspan="3"><h4>LS-Series Pin-on Type</h4></td></tr>
-                <tr><td>Product</td><td>Model</td><td>Description</td></tr>
-                <tr><td><img class="img-responsive" src="/images/bale-handling/lsf-22-lit.jpg" alt=""></td>
-                    <td>LSFB-22</td>
-                    <td>Designed for pin-type loaders only. This unit is rated at 2,200 lbs. Bolt-in spear has 42" useable length. For use with round bales. Includes pin-on brackets. Ships in one carton. (NOTE: Not Quick Attach Adaptable)
-                        <br /><br /><a href="/front-loader-bale-spear-lsfb-22">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/LSFB.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/lsf-28-lit.jpg" alt=""></td>
-                    <td>LSFB-28</td>
-                    <td>Designed for pin-type loaders only. This unit is rated at 3,000 lbs. Bolt-in spears have 42" useable length. For use with round bales. Includes pin-on brackets. Ships in one carton. (NOTE: Not Quick Attach Adaptable)
-                        <br /><br /><a href="/front-loader-bale-spear-lsfb-28">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/LSFB.pdf">Download Manual</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+        <h4>Designed for use on Front Loader Buckets. These attachments allow the operator to leave the bucket installed when handling round bales. Ideal for the operator who is looking for an economical way to move a few round bales.</h4>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <img class="img-responsive" src="/images/bale-handling/Bucket_Bale_Spears.jpg" alt="">
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td align="center" colspan="3"><h4>FLBS-Series</h4></td></tr>
                 <tr><td>Product</td><td>Model</td><td>Description</td></tr>
 
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLSB-2200.jpg" alt=""></td>
-                    <td>FLBS-2200</td>
-                    <td>Loader Bale Spear rated at 2,200 lbs. Unit has a tapered spear with a useable length of 42". Designed fo use on tractor front loaders. Features an all welded frame, cast steel weld-on spear sockets, and bolt-in spears for increased durability. Optional interfacing brackets for most loader quick attach systems are available.
-                        <br />(will not accept 831110 brackets).
-                        <br />Instead of 831110 brackets, consider SSB-2200.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-2200">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLBS-3000.jpg" alt=""></td>
-                    <td>FLBS-3000</td>
-                    <td>Loader Bale Spear rated at 3,000 lbs. Unit has Dual tapered spears with a useable length of 42". Designed fo use on tractor front loaders. Features an all welded frame, cast steel weld-on spear sockets, and bolt-in spears for increased durability. Optional interfacing brackets for most loader quick attach systems are available.
-                        <br />(will not accept 831110 brackets).
-                        <br />Instead of 831110 brackets, consider SSB-3000.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-3000">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/" alt=""></td>
+                    <td>BBSF-20</td>
+                    <td>
+                        <br /><br /><a href="/">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/">Download Manual</a>
+                    </td>
+                </tr>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/" alt=""></td>
+                    <td></td>
+                    <td>
+                        <br /><br /><a href="/">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/">Download Manual</a>
+                    </td>
+                </tr>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/" alt=""></td>
+                    <td></td>
+                    <td>
+                        <br /><br /><a href="/">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/">Download Manual</a>
+                    </td>
+                </tr>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/" alt=""></td>
+                    <td></td>
+                    <td>
+                        <br /><br /><a href="/">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/">Download Manual</a>
                     </td>
                 </tr>
 
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLBS-330.jpg" alt=""></td>
-                    <td>FLBS-330</td>
-                    <td>Loader Bale Spear rated at 3,000 lbs. Unit has three tapered spears spaced 26-1/2" apart with a useable length of 26". For Mid-Size Square bales. Designed fo use on tractor front loaders. Features an all welded frame, cast steel weld-on spear sockets, and bolt-in spears for increased durability. Optional upright kit (831095) for handling two bales. Optional interfacing brackets for most loader quick attach systems are available.
-                        <br />Instead of 831110 brackets, consider SSRB-330.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-330">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a></td></tr>
 
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLBS-340.jpg" alt=""></td>
-                    <td>FLBS-340</td>
-                    <td>Loader Bale Spear rated at 3,000 lbs. Unit has three tapered spears spaced 26-1/2" apart with a useable length of 42". For Large-Size Square bales. Designed fo use on tractor front loaders. Features an all welded frame, cast steel weld-on spear sockets, and bolt-in spears for increased durability. Optional upright kit (831095) for handling two bales. Optional interfacing brackets for most loader quick attach systems are available.
-                        <br />Instead of 831110 brackets, consider SSRB-340.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-340">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a>
-                    </td>
-                </tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLSB-432.jpg" alt=""></td>
-                    <td>FLBS-432</td>
-                    <td>Loader bale spear to handle Mid-Sized square bales. Features 79-1/2" wide frame with four tapered spears with useable length of 26", spaced 24" apart. Optional upright kit (831095) for handling two square bales. Optional interfacing brackets for most quick attach systems are available.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-432">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a></td></tr>
-
-                <tr><td><img class="img-responsive" src="/images/bale-handling/FLBS-449.jpg" alt=""></td>
-                    <td>FLBS-449</td>
-                    <td>Loader bale spear to handle Large-Sized square bales. Features 79-1/2" wide frame with four tapered spears with useable length of 42", spaced 24" apart. Optional upright kit (831095) for handling two square bales. Optional interfacing brackets for most quick attach systems are available.
-                        <br /><br /><a href="/front-loader-bale-spears-flbs-449">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/flbs.pdf">Download Manual</a>
-                    </td>
-                </tr>
             </tbody>
         </table>
     </div>
@@ -83,15 +50,13 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td align="center" colspan="2"><h4>Optional Kits</h4></td></tr>
-                <tr><td>Optional Upright Kit (831095)</td><td>Optional upright kit (831095) for handling two square bales. Fits models FLBS-330 / 340 / 432 / 449. <br />(Shown on FLBS-340 and 432 units.)</td></tr>
-                <tr><td>Optional Upright Kit</td><td>Optional bolt-on uprights for RBSF-3232 / 3333. Designed for handling two bales at once.
-                        <br /><a href="/front-loader-bale-spear-optional-upright-kit">Parts List Click Here</a></td></tr>
-                <tr><td>Optional Bushing Kit</td><td>Optional Cat II Quick Hitch Bushing Kit for RBSF-449 / 432. Kit includes bushing adapters to allow standard pin on brackets & Optional toplink bracket to be used with a Cat. II Quick Hitch.
-                        <br /><a href="/front-loader-bale-spear-optional-bushing-kit">Parts List Click Here</a></td></tr>
-                <tr><td>Optional Upright Kit</td><td>Optional bolt-on uprights for RBSF-449 / 432. Designed for handling two bales at once.
-                        <br /><a href="/front-loader-bale-spear-optional-bolt-on-upright-kit">Parts List Click Here</a></td></tr>
-                <tr><td>Optional 3-Pt. Bolt-on Top Link</td><td>Optional bolt-on assembly to allow USBF-series and RBSF-series to be use on a Cat. II 3-Pt. hitch.</td></tr>
+                <tr><td class="table-col-header" align="center" colspan="3">Skid Steer Bale Hand</td></tr>
+                <tr><td>Product</td><td>Model</td><td>Description</td></tr>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/" alt=""></td>
+                    <td>SSBH-2072</td>
+                    <td>Loader "Bale Hugger" Designed to handle 4' to 6' round bales, wrapped or dry, up to 2,000 lbs. Features one stationary arm, one opening arm. Includes hydraulic cylinder. Optional interfacing brackets for most loader quick attach systems are available - see options below.
+                        <br /><br /><a href="/bale-clamping-attachments-ssbh-2072">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/">Download Manual</a></td></tr>
             </tbody>
         </table>
     </div>
@@ -100,14 +65,26 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td align="center" colspan="3"><h4>Rectangular Bale Handler</h4></td></tr>
-                <tr><td><a href="/front-loader-bale-spear-rbh-4000-large-rectangular-bale-handler"><img class="img-responsive" src="/images/bale-handling/rbh4000.jpg" alt=""></a></td>
-                    <td><a href="/front-loader-bale-spear-rbh-4000-large-rectangular-bale-handler">RBH-4000<br />Shown with Optional Brackets</a></td>
-                    <td>Rectangular bale handler id designed for tractor front loaders. Rated at 4,000 lbs., this unit will handle either two 4'x4'x8' bales or three 3'x3'x8' bales. Five 26" long forged spears are placed along the bottom of the unit and six 18" long spears are supplied for the uprights. Each side features 5 hooks that are moveable by the operator. The Unit is 79.50" wide and 80" high. Click picture for more information.
-                        <br /><a href="/front-loader-bale-spear-rbh-4000">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/rbh4000.pdf">Download Manual</a></td>
+                <tr><td class="table-col-header" align="center" colspan="3">UBH-Series</td></tr>
+                <tr><td>Product</td><td>Model</td><td>Description</td></tr>
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/ubh2000.jpg" alt=""></td>
+                    <td>UBH-2000<br />Shown with Optional Brackets</td>
+                    <td>Loader "Bale Hugger" Designed to handle 4' to 6' round bales, wrapped or dry, up to 2,000 lbs. Features one stationary arm, one opening arm. Includes hydraulic cylinder. Optional interfacing brackets for most loader quick attach systems are available - see options below.
+                        <br /><br /><a href="/bale-clamping-attachments-ubh-2000">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/ubh-2000.pdf">Download Manual</a></td></tr>
+
+                <tr><td><img class="img-responsive" src="/images/bale-clamping/balehand.gif" alt=""></td>
+                    <td>UBH-2072<br />Shown with Optional Brackets</td>
+                    <td>Loader "Bale Hand" Designed to handle 4' to 6' round bales, wrapped or dry, up to 2,200 lbs. Both arms open to clamp the bale. Hydraulic cylinder is standard. Optional interfacing brackets for most loader quick attach systems are available - see options below.
+                        <br /><br /><a href="/bale-clamping-attachments-ubh-2072">Parts List Click Here</a>
+                        <br /><a href="/templates/worksaver/manuals/ubh-2072.pdf">Download Manual</a>
+                    </td>
                 </tr>
             </tbody>
         </table>
     </div>
 </div>
+
+
+
+
