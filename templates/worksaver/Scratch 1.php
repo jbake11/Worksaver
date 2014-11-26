f44cc53e4ef6dce5fd912190e055f0ca63402107
jbake11 <jeffbake40@gmail.com>
2014-11-25 23:52:50 -0500

    300*214 batch

---
+++ templates/worksaver/Scratch 1.php
@@ -0,0 +1,41 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr><td class="table-col-header" colspan="3">TECHNICAL SPECIFICATIONS</td></tr>
+                <tr><td></td></tr>
+
+                <tr><td>Model	FSO-906	FLSO-907	SSSO-908
+                <tr><td>Item No.	831000	831020	831030
+                <tr><td>Frame Size	4" x 4" Sq. Tubing	3" and 4" Sq. Tubing	3" and 4" Sq. Tubing
+                <tr><td>Spear	Tapered Forged	Tapered Forged	Tapered Forged
+                <tr><td>Drive
+                <tr><td>Attaching Method	3-pt Cat. II/III**	Front Loader Pin-on*	Skid Steer "universal"
+                <tr><td>Chain Type	#50 Roller Chain	#50 Roller Chain	#50 Roller Chain
+                <tr><td>Sprocket Ratio	3.75:1	3.75:1	3.75:1
+                <tr><td>Hydraulics
+                <tr><td>GPM - Requirements	8-10 GPM	8-10 GPM	8-10 GPM
+                <tr><td>PSI - Requirements	Min. 1500 psi	Min. 1500 psi	Min. 1500 psi
+                <tr><td>Hoses	Supplied (2) Less Ends	Less Hoses	Supplied (2) Less Ends
+                <tr><td>Cushion Valve
+                <tr><td>Factory Setting - Relief	1750 psi	1750 psi	1750 psi
+                <tr><td>Dimensions
+                <tr><td>Height	26¾"	23¼"	23¼"
+                <tr><td>Width	44¾"	60½"	45¼"
+                <tr><td>Length w/Spear	64¾"	64¾"	64¾"
+                <tr><td>Weight	250 lbs	370 lbs	325 lbs
+                <tr><td>* Optional interfacing brackets for most loader quick attach systems are available
+                        ** Optional pin & bushing kit available to allow use on Cat. I 3-pt hitch.
+
+
+                <tr><td>Other Information
+                <tr><td>SO-905 Parts	FSO-906 Parts	FLSO-907 Parts	SSSO-908 Parts
+                <tr><td>Trouble Shooting	Literature	FAQ	How to Operate
+                <tr><td>Operations Manuals (Feb 2002 - 2005)	Operations Manuals (Aug 2000 - Feb 2002)
+                <tr><td>Operations Manuals (Jan 2006 - Sept 2006)	Operations Manuals (Sept 2006 - Current Date)
+                <tr><td>Flat-Faced Coupler Set for Skid Steer Units
+
+            </tbody>
+        </table>
+    </div>
+</div>
