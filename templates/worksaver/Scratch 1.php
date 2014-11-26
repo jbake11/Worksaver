793bd1c73075c433bc220724070c107f6588f3c8
jbake11 <jeffbake40@gmail.com>
2014-11-26 00:34:43 -0500

    asd

--- templates/worksaver/Scratch 1.php
+++ templates/worksaver/Scratch 1.php
@@ -1,41 +1,97 @@
 <div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <img class="img-responsive col-xs-12" src="/images/Round Bale Feeder/FLFSO9071.jpg" alt="" />
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <img class="img-responsive col-xs-12" src="/images/Round Bale Feeder/SO 906 Lit.jpg" alt="" />
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <img class="img-responsive col-xs-12" src="/images/Round Bale Feeder/SSFSO908.jpg" alt="" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <ul>
+            <li>Eliminate wasted hay; only unroll what cattle will clean up in one hour
+            <li>Almost no manure buildup; by feeding in a different location each day.
+            <li>Easy operation from the cab of the tractor or Skid Steer.
+            <li>Models for 3-pt, Front Loaders & Skid Steers.
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <ul>
+            <li>Hydraulically driven for better operator control.
+            <li>Rotates in both directions for easy feeding.
+            <li>Cushion valve in the motor provides protection for hydraulic components.
+            <li>Forged tapered spear for easier spear penetration.
+        </ul>
+    </div>
+</div>
+<div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td class="table-col-header" colspan="3">TECHNICAL SPECIFICATIONS</td></tr>
-                <tr><td></td></tr>
-
-                <tr><td>Model	FSO-906	FLSO-907	SSSO-908
-                <tr><td>Item No.	831000	831020	831030
-                <tr><td>Frame Size	4" x 4" Sq. Tubing	3" and 4" Sq. Tubing	3" and 4" Sq. Tubing
-                <tr><td>Spear	Tapered Forged	Tapered Forged	Tapered Forged
-                <tr><td>Drive
-                <tr><td>Attaching Method	3-pt Cat. II/III**	Front Loader Pin-on*	Skid Steer "universal"
-                <tr><td>Chain Type	#50 Roller Chain	#50 Roller Chain	#50 Roller Chain
-                <tr><td>Sprocket Ratio	3.75:1	3.75:1	3.75:1
-                <tr><td>Hydraulics
-                <tr><td>GPM - Requirements	8-10 GPM	8-10 GPM	8-10 GPM
-                <tr><td>PSI - Requirements	Min. 1500 psi	Min. 1500 psi	Min. 1500 psi
-                <tr><td>Hoses	Supplied (2) Less Ends	Less Hoses	Supplied (2) Less Ends
-                <tr><td>Cushion Valve
-                <tr><td>Factory Setting - Relief	1750 psi	1750 psi	1750 psi
-                <tr><td>Dimensions
-                <tr><td>Height	26¾"	23¼"	23¼"
-                <tr><td>Width	44¾"	60½"	45¼"
-                <tr><td>Length w/Spear	64¾"	64¾"	64¾"
-                <tr><td>Weight	250 lbs	370 lbs	325 lbs
-                <tr><td>* Optional interfacing brackets for most loader quick attach systems are available
-                        ** Optional pin & bushing kit available to allow use on Cat. I 3-pt hitch.
-
-
-                <tr><td>Other Information
-                <tr><td>SO-905 Parts	FSO-906 Parts	FLSO-907 Parts	SSSO-908 Parts
-                <tr><td>Trouble Shooting	Literature	FAQ	How to Operate
-                <tr><td>Operations Manuals (Feb 2002 - 2005)	Operations Manuals (Aug 2000 - Feb 2002)
-                <tr><td>Operations Manuals (Jan 2006 - Sept 2006)	Operations Manuals (Sept 2006 - Current Date)
-                <tr><td>Flat-Faced Coupler Set for Skid Steer Units
-
+                <tr><td class="table-col-header" align="center" colspan="4">TECHNICAL SPECIFICATIONS</td></tr>
+                <tr><td>Model</td><td>FSO-906</td><td>FLSO-907</td><td>SSSO-908</td></tr>
+                <tr><td>Item No.</td><td>831000</td><td>831020</td><td>831030</td></tr>
+                <tr><td>Frame Size</td><td>4" x 4" Sq. Tubing</td><td>3" and 4" Sq. Tubing</td><td>3" and 4" Sq. Tubing</td></tr>
+                <tr><td>Spear</td><td>Tapered Forged</td><td>Tapered Forged</td><td>Tapered Forged</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Drive</td></tr>
+                <tr><td>Attaching Method</td><td>3-pt Cat. II/III**</td><td>Front Loader Pin-on*</td><td>Skid Steer "universal"</td></tr>
+                <tr><td>Chain Type</td><td>#50 Roller Chain</td><td>#50 Roller Chain</td><td>#50 Roller Chain</td></tr>
+                <tr><td>Sprocket Ratio</td><td>3.75:1</td><td>3.75:1</td><td>3.75:1</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Hydraulics</td></tr>
+                <tr><td>GPM - Requirements</td><td>8-10 GPM</td><td>8-10 GPM</td><td>8-10 GPM</td></tr>
+                <tr><td>PSI - Requirements</td><td>Min. 1500 psi</td><td>Min. 1500 psi</td><td>Min. 1500 psi</td></tr>
+                <tr><td>Hoses</td><td>Supplied (2) Less Ends</td><td>Less Hoses</td><td>Supplied (2) Less Ends</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Cushion Valve</td></tr>
+                <tr><td>Factory Setting - Relief</td><td>1750 psi</td><td>1750 psi</td><td>1750 psi</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Dimensions</td></tr>
+                <tr><td>Height</td><td>26¾"</td><td>23¼"</td><td>23¼"</td></tr>
+                <tr><td>Width</td><td>44¾"</td><td>60½"</td><td>45¼"</td></tr>
+                <tr><td>Length w/Spear</td><td>64¾"</td><td>64¾"</td><td>64¾"</td></tr>
+                <tr><td>Weight</td><td>250 lbs</td><td>370 lbs</td><td>325 lbs</td></tr>
+                <tr><td class="table-col-modelnum align="center" colspan="4"">* Optional interfacing brackets for most loader quick attach systems are available
+                        <br />** Optional pin & bushing kit available to allow use on Cat. I 3-pt hitch.</td></tr>
             </tbody>
         </table>
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr><td class="table-col-header" align="center" colspan="4">Other Information</td></tr>
+                <tr><td><a href="/round-bale-feeder-so-905">SO-905 Parts</a></td>
+                    <td><a href="/round-bale-feeder-fso-906"><a href="">FSO-906 Parts</a></td>
+                    <td><a href="/round-bale-feeder-flso-907">FLSO-907 Parts</a></td>
+                    <td><a href="/round-bale-feeder-ssso-908">SSSO-908 Parts</a></td>
+                </tr><tr><td><a href="/round-bale-feeder-trouble-shooting">Trouble Shooting</a></td>
+                    <td><a href="/templates/worksaver/wsr6359_Bale Handling Equipment brochure_final">Literature</a></td>
+                    <td><a href="/round-bale-feeder-faq">FAQ</a></td>
+                    <td><a href="/round-bale-feeder-how-to-operate">How to Operate</a></td>
+                </tr><tr><td><a href="/templates/worksaver/Manuals/so_operation_manual_(02-02).pdf">Operations Manuals (Feb 2002 - 2005)</a></td>
+                    <td><a href="/templates/worksaver/Manuals/so_operation_manual_(08-00).pdf">Operations Manuals (Aug 2000 - Feb 2002)</a></td>
+                </tr><tr><td><a href="/templates/worksaver/Manuals/wsso905-6.pdf">Operations Manuals (Jan 2006 - Sept 2006)</a></td>
+                    <td><a href="/templates/worksaver/Manuals/FSO678.pdf">Operations Manuals (Sept 2006 - Current Date)</a></td>
+                </tr>
+                <tr><td align="center" colspan="4"><a href="">Flat-Faced Coupler Set for Skid Steer Units</a></td></tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+============================CSS Reference DO NOT INCLUDE=========================
+
+table-col-header
+
+table-col-modelnum
+
+.table-col-img{
+background: white;
+text-align: center;
+
+}
