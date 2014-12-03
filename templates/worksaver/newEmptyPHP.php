cedf658366b933b27ec0b781aa3c8346239eaf4e
jbake11 <jeffbake40@gmail.com>
2014-12-03 10:00:33 -0500

    updated LM pdf

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,33 +1,88 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <a href="/" ><img class="img-responsive col-xs-12" src="/images/bucket-bale-handling/so_logo_small.gif" alt="" /></a>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        Designed as an efficient hay management tool for moving and feeding round bales, the RW Spin Off hydraulically rotates the bale, unwinding the hay into a windrow. Rotates in both directions for easy feeding and allows the operator to unroll what the cattle will clean up in one hour, eliminating wasted hay. Also, by changing the feeding location each day the operator can eliminate manure buildup. Available in three models and designed to fit Category II or Category III 3-pt hitches (FSO-906), front loaders (FLSO-907) and skid steers (SSSO908). All models are hydraulically driven for better operator control, provide easy operation from the cab of tractors or skid steers and feature a forged tapered spear for easier bale penetration. All have usable spear length of 53”.
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive col-xs-12" src="/images/round-bale-feeder/SSFSO908.jpg" alt="" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive col-xs-12" src="/images/round-bale-feeder/FLFSO9071.jpg" alt="" />
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive col-xs-12" src="/images/round-bale-feeder/SO 906 Lit.jpg" alt="" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <ul>
+            <li>Eliminate wasted hay; only unroll what cattle will clean up in one hour</li>
+            <li>Almost no manure buildup; by feeding in a different location each day.</li>
+            <li>Easy operation from the cab of the tractor or Skid Steer.</li>
+            <li>Models for 3-pt, Front Loaders & Skid Steers.</li>
+        </ul>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+
+        <ul>
+            <li>Hydraulically driven for better operator control.</li>
+            <li>Rotates in both directions for easy feeding.</li>
+            <li>Cushion valve in the motor provides protection for hydraulic components.</li>
+            <li>Forged tapered spear for easier spear penetration.</li>
+        </ul>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-        <h4>Chain or Sprocket Breakage</h4>
-
-        <ul>
-            <li>The chain is too tight. Adjust the chain to allow 3/16" deflection. When reversing the direction of rotation, allow the bale to almost come to a complete stop before reversing.
-            <li>The RW SpinOff must be run with the chain guard in place to prevent foreign objects from getting caught in the chain and causing damage.
-        </ul>
-
-        <h4>Chain Coming Off</h4>
-
-        <ul>
-            <li>The small sprocket and large sprocket are not aligned properly. Align the small sprocket with the large sprocket on the spinner assembly using a straight edge.
-            <li>The chain may be too loose. Adjust the chain to allow 3/16" deflection.
-
-        </ul>
-        <h4>Bale Won’t Unwind</h4>
-        <ul>
-            <li>The twine may still be on the bale. If it is, take the twine completely off to avoid tangling on the RW SpinOff when the bale is rotating.
-            <li>The bale is frozen. Try bouncing the bale lightly on the ground to loosen it up. You may have to use the ground to slough the first layer off the bale and get it unrolling.
-            <li>The bale is wrapped too tightly. Make certain that all the twine is cut. Try bouncing the bale lightly on the ground to loosen it up.
-        </ul>
-
-
-
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr><td class="table-col-header" align="center" colspan="4">TECHNICAL SPECIFICATIONS</td></tr>
+                <tr><td>Model</td><td>FSO-906</td><td>FLSO-907</td><td>SSSO-908</td></tr>
+                <tr><td>Item No.</td><td>831000</td><td>831020</td><td>831030</td></tr>
+                <tr><td>Frame Size</td><td>4" x 4" Sq. Tubing</td><td>3" and 4" Sq. Tubing</td><td>3" and 4" Sq. Tubing</td></tr>
+                <tr><td>Spear</td><td>Tapered Forged</td><td>Tapered Forged</td><td>Tapered Forged</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Drive</td></tr>
+                <tr><td>Attaching Method</td><td>3-pt Cat. II/III**</td><td>Front Loader Pin-on*</td><td>Skid Steer "universal"</td></tr>
+                <tr><td>Chain Type</td><td>#50 Roller Chain</td><td>#50 Roller Chain</td><td>#50 Roller Chain</td></tr>
+                <tr><td>Sprocket Ratio</td><td>3.75:1</td><td>3.75:1</td><td>3.75:1</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Hydraulics</td></tr>
+                <tr><td>GPM - Requirements</td><td>8-10 GPM</td><td>8-10 GPM</td><td>8-10 GPM</td></tr>
+                <tr><td>PSI - Requirements</td><td>Min. 1500 psi</td><td>Min. 1500 psi</td><td>Min. 1500 psi</td></tr>
+                <tr><td>Hoses</td><td>Supplied (2) Less Ends</td><td>Less Hoses</td><td>Supplied (2) Less Ends</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Cushion Valve</td></tr>
+                <tr><td>Factory Setting - Relief</td><td>1750 psi</td><td>1750 psi</td><td>1750 psi</td></tr>
+                <tr><td class="table-col-modelnum" align="center" colspan="4">Dimensions</td></tr>
+                <tr><td>Height</td><td>26¾"</td><td>23¼"</td><td>23¼"</td></tr>
+                <tr><td>Width</td><td>44¾"</td><td>60½"</td><td>45¼"</td></tr>
+                <tr><td>Length w/Spear</td><td>64¾"</td><td>64¾"</td><td>64¾"</td></tr>
+                <tr><td>Weight</td><td>250 lbs</td><td>370 lbs</td><td>325 lbs</td></tr>
+                <tr><td class="table-col-modelnum align="center" colspan="4"">* Optional interfacing brackets for most loader quick attach systems are available
+                        <br />** Optional pin & bushing kit available to allow use on Cat. I 3-pt hitch.</td></tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr><td class="table-col-header" align="center" colspan="4">Other Information</td></tr>
+                <tr><td><a href="/round-bale-feeder-so-905">SO-905 Parts</a></td>
+                    <td><a href="/round-bale-feeder-fso-906">FSO-906 Parts</a></td>
+                    <td><a href="/round-bale-feeder-flso-907">FLSO-907 Parts</a></td>
+                    <td><a href="/round-bale-feeder-ssso-908">SSSO-908 Parts</a></td>
+                </tr><tr><td><a href="/round-bale-feeder-trouble-shooting">Trouble Shooting</a></td>
+                    <td><a href="/templates/worksaver/wsr6359_Bale Handling Equipment brochure_final.pdf">Literature</a></td>
+                    <td><a href="/round-bale-feeder-faq">FAQ</a></td>
+                    <td><a href="/round-bale-feeder-how-to-operate">How to Operate</a></td>
+                </tr><tr><td align="center" colspan="2"><a href="/templates/worksaver/Manuals/so_operation_manual_(02-02).pdf">Operations Manuals (Feb 2002 - 2005)</a></td>
+                    <td align="center" colspan="2"><a href="/templates/worksaver/Manuals/so_operation_manual_(08-00).pdf">Operations Manuals (Aug 2000 - Feb 2002)</a></td>
+                </tr><tr><td align="center" colspan="2"><a href="/templates/worksaver/Manuals/wsso905-6.pdf">Operations Manuals (Jan 2006 - Sept 2006)</a></td>
+                    <td align="center" colspan="2"><a href="/templates/worksaver/Manuals/FSO678.pdf">Operations Manuals (Sept 2006 - Current Date)</a></td>
+                </tr>
+                <tr><td align="center" colspan="4"><a href="">Flat-Faced Coupler Set for Skid Steer Units</a></td></tr>
+            </tbody>
+        </table>
     </div>
 </div>
