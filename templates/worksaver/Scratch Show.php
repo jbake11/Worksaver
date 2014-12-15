d885902daa5825e86d5a7c3210bd977ce1d13b78
jbake11 <jeffbake40@gmail.com>
2014-12-15 16:49:42 -0500

    asd

--- templates/worksaver/Scratch Show.php
+++ templates/worksaver/Scratch Show.php
@@ -1,29 +1,7 @@
-<div class="row">
-    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
-        <img class="img-responsive" src="/images/loader-brackets-line-drawings/830490.gif" alt="" />
-    </div>
-</div>
 
-<div class="row">
-    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
-        <img class="img-responsive" src="/images/loader-brackets-line-drawings/830476.gif" alt="" />
-    </div>
-</div>
 
-<div class="row">
-    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
-        <img class="img-responsive" src="/images/loader-brackets-line-drawings/tba.gif" alt="" />
-    </div>
-</div>
 
-<div class="row">
-    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
-        <img class="img-responsive" src="/images/loader-brackets-line-drawings/831790.gif" alt="" />
-    </div>
-</div>
 
-<div class="row">
-    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
-        <img class="img-responsive" src="/images/loader-brackets-line-drawings/831110.gif" alt="" />
-    </div>
-</div>
+
+
+
