68cae24faca46acb5a1b00922ddea13bbdb2c8c6
jbake11 <jeffbake40@gmail.com>
2014-12-14 21:58:22 -0500

    update

--- templates/worksaver/Scratch 1.php
+++ templates/worksaver/Scratch 1.php
@@ -36,33 +36,45 @@
         <p>Powered rakes are ideal tool for the landscape contractor, large property owner, rental yard, park &amp; recreation department or turf management professional. They do it all, prep, smooth, set grade, level, till, remove rocks and create the perfect seedbed making this tool a game changer.</p>
     </div>
 </div>
+
 <div class="row">
-
-
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive" src="/images/powerlandscaperakes/40series.jpg" alt="" />
     </div>
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive" src="/images/powerlandscaperakes/PPoint.jpg" alt="" />
     </div>
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+</div>
+
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <h5>Features</h5>
+        <ul>
+            <li>Flip-up side shields are self-storing on the unit allowing ease of windrowing rocks or debris.</li>
+            <li>Flip-up gauge wheels (height adjustable) allows accurate control of working depth keeping machine level for smoother surface finish. Easily flip up out of the way for transport on a trailer.</li>
+            <li>Replaceable bearing housings and triple seal bearings keep dirt out, providing more uptime.</li>
+            <li>Choice of rotors - 2" Sabre tooth or 1-3/4" Carbide Tooth.</li>
+            <li>Dry Chain Case, environmentally friendly reduces maintenance as there is no oil to contend with.</li>
+            <li><a href="/templates/worksaver/literature/wsr6481_Land Management Equipment brochure.pdf" style="color: #005580; text-decoration: underline; outline-width: 0px; outline-style: initial; outline-color: initial;">Literature</a></li>
+        </ul>
     </div>
-    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive" src="/images/powerlandscaperakes/PPoint.jpg" alt="" />
     </div>
-    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-    </div>
+</div>
 
 
 
 
-
-    <h5>Features</h5>
-    <ul>
-        <li>Flip-up side shields are self-storing on the unit allowing ease of windrowing rocks or debris.</li>
-        <li>Flip-up gauge wheels (height adjustable) allows accurate control of working depth keeping machine level for smoother surface finish. Easily flip up out of the way for transport on a trailer.</li>
-        <li>Replaceable bearing housings and triple seal bearings keep dirt out, providing more uptime.</li>
-        <li>Choice of rotors - 2" Sabre tooth or 1-3/4" Carbide Tooth.</li>
-        <li>Dry Chain Case, environmentally friendly reduces maintenance as there is no oil to contend with.</li>
-        <li><a href="/templates/worksaver/literature/wsr6481_Land Management Equipment brochure.pdf" style="color: #005580; text-decoration: underline; outline-width: 0px; outline-style: initial; outline-color: initial;">Literature</a></li>
-    </ul>
+<h5>Features</h5>
+<ul>
+    <li>Flip-up side shields are self-storing on the unit allowing ease of windrowing rocks or debris.</li>
+    <li>Flip-up gauge wheels (height adjustable) allows accurate control of working depth keeping machine level for smoother surface finish. Easily flip up out of the way for transport on a trailer.</li>
+    <li>Replaceable bearing housings and triple seal bearings keep dirt out, providing more uptime.</li>
+    <li>Choice of rotors - 2" Sabre tooth or 1-3/4" Carbide Tooth.</li>
+    <li>Dry Chain Case, environmentally friendly reduces maintenance as there is no oil to contend with.</li>
+    <li><a href="/templates/worksaver/literature/wsr6481_Land Management Equipment brochure.pdf" style="color: #005580; text-decoration: underline; outline-width: 0px; outline-style: initial; outline-color: initial;">Literature</a></li>
+</ul>
 </div>
 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img class="img-responsive" src="/images/powerlandscaperakes/Carbide rotor WS.jpg" alt="" />
     <h5>Carbide Rotor</h5>
