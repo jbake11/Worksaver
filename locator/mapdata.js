6a77c8928a423dd46fae6b687587c3b3bbb6ff32
Chris Bake <cbake7806@gmail.com>
2015-01-08 01:54:36 -0500

    Debug

--- locator/mapdata.js
+++ locator/mapdata.js
@@ -2,7 +2,7 @@ var simplemaps_namap_mapdata = {
     main_settings: {
         //General settings
         width: 'responsive', //or 'responsive'
-        background_color: '#FFFFFF',
+        background_color: '#c6c4c2',
         background_transparent: 'no',
         border_color: '#ffffff',
         pop_ups: 'detect', //on_click, on_hover, or detect
