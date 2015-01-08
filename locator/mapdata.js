fc1a3b60ca396bf2b0f43ca834520c4da64adda0
Chris Bake <cbake7806@gmail.com>
2015-01-08 01:49:29 -0500

    Debug

--- locator/mapdata.js
+++ locator/mapdata.js
@@ -336,7 +336,8 @@ var simplemaps_namap_mapdata = {
             name: 'default',
             color: 'default',
             hover_color: 'default',
-            url: 'default'
+            url: 'default',
+            hide: 'yes'
         },
         OR: {
             name: 'default',
@@ -453,7 +454,7 @@ var simplemaps_namap_mapdata = {
             color: 'default',
             hover_color: 'default',
             url: 'default',
-            hide: 'no'
+            hide: 'yes'
         }
     },
     locations: {
