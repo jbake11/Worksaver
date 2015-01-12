0c1a5d33a89f07d6accdd2f9f70bddc2d45ca001
jbake11 <jeffbake40@gmail.com>
2015-01-11 22:17:39 -0500

    tons of new pics and literature

--- templates/worksaver/newEmptyPHP1.php
+++ templates/worksaver/newEmptyPHP1.php
@@ -1,31 +1,31 @@
+<h2><span style="font-size: 17pt;">Helps you care for your land. In your wildlife food plots…in your gardens…on your acreage.</span></h2>
+<p>Elevate your ATV's productivity by converting to Worksaver ATV Attachments. Great for wildlife food plots, gardens and large acreage. Worksaver provides quality designed and innovative products that work the way you live!</p>
+<p>&nbsp;</p>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Integrated-Frame designed hay attachments. These attachments feature all welded frame design, that incorporates the interfacing quick attach brackets welded to the unit. The attachments are loader model specific.</h4>
+    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-atvk-400"><img class="img-responsive" src="/images/atv-attachments/ATVK-400.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><strong><a href="/atv-attachments-atvk-400">ATVK-400 Electric Seeder/Spreader</a><br /></strong></h3>
+        Worksaver’s model ATVK-400 helps get a head start early in the season. Excellent for seeding pastures and wildlife food plots. Features a lower profile mount that allows the unit to fit most all brands of ATV’s. Durable, maintenance-free 1-bushel polyethylene hopper is translucent for easy monitoring of hopper material level.
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr><td>Product</td><td>Model</td><td>Description</td></tr>
-                <tr><td align="center" colspan="3">For John Deere loader models: 300, 300CX, 400X, 410, 420, 430, 440, 460, 520, 521, 540, and 541</td></tr>
-                <tr><td><img class="img-responsive" src="/images/integrated-frame-bale-spears/Jdbs-412.jpg" alt=""></td>
-                    <td>JDBS-412</td>
-                    <td>Single bolt-in forged spear with useable length of 42". Rated at 2,200 lbs. Two balance spikes, 1" x 8-1/2" useable.
-                        <br /><br /><a href="/integrated-frame-bale-spears-jdbs-412">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/JDBS412-633.pdf">Download Manual</a>
-                    </td>
-                </tr>
-                <tr><td><img class="img-responsive" src="/images/integrated-frame-bale-spears/Jdbs-423.jpg" alt=""></td>
-                    <td>JDBS-423</td>
-                    <td>Dual bolt-in forged spears spaced 26-1/2" apart, with useable length of 42". Rated at 3,000 lbs.
-                        <br /><br /><a href="/integrated-frame-bale-spears-jdbs-423">Parts List Click Here</a>
-                        <br /><a href="/templates/worksaver/manuals/JDBS412-633.pdf">Download Manual</a>
-                    </td>
-                </tr>
-
-
-            </tbody>
-        </table>
+    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-plr-48-60-72"><img class="img-responsive" src="/images/atv-attachments/PLR-48_60_72.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/atv-attachments-plr-48-60-72"><strong>PLR-48/60/72&nbsp;Landscape Rakes</strong></a></h3>
+        Designed for quick and easy cleanup of leaves and debris from large areas of lawn, pastures and roadsides – keeps your property neatly groomed. Features spring steel tines spaced at 1” intervals for thorough raking; debris is “windrowed” for easy collection and pick-up. Rakes are available in 4’, 5’ and 6’ widths.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-fod-16"><img class="img-responsive" src="/images/atv-attachments/FOD-16.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/atv-attachments-fod-16"><strong>FOD-16&nbsp;Flip-Over Disc</strong></a></h3>
+        Ideal for tilling wildlife food plots and larger gardens. Patented design allows for easy “flip-over” from transport to cultivation mode. Features eight (8) 16” notched blades designed for efficient trash cutting and optimum soil penetration.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-foc-48"><img class="img-responsive" src="/images/atv-attachments/FOC_48.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/atv-attachments-foc-48"><strong>FOC-48&nbsp;Flip-Over Culti-Packer</strong></a></h3>
+        Perfect for final compaction after seeding your food plot. Reduces air pockets and increases seed germination. Helps prevent erosion. Unique design allows for easy “flip-over” from transport to operation mode. Can also be used for breaking the crust of rain-hardened soil, to allow emerging seedlings to break through and grow.
     </div>
 </div>
