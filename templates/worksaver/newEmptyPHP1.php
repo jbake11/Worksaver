1df416de18892090b6f7000578c2105ccfce4e44
jbake11 <jeffbake40@gmail.com>
2015-01-11 22:34:28 -0500

    asd

--- templates/worksaver/newEmptyPHP1.php
+++ templates/worksaver/newEmptyPHP1.php
@@ -1,31 +1,77 @@
-<h2><span style="font-size: 17pt;">Helps you care for your land. In your wildlife food plots…in your gardens…on your acreage.</span></h2>
-<p>Elevate your ATV's productivity by converting to Worksaver ATV Attachments. Great for wildlife food plots, gardens and large acreage. Worksaver provides quality designed and innovative products that work the way you live!</p>
+<h3>Need to find quality tractor blades or landscape rake?<br /> Worksaver Has the part for you!<br /></h3>
 <p>&nbsp;</p>
+
+
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-atvk-400"><img class="img-responsive" src="/images/atv-attachments/ATVK-400.jpg" alt="" /></a></div>
+    <div class="col-lg-4 col-md-4"><a href="/rear-blades-dc-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/DC-5.jpg" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><strong><a href="/atv-attachments-atvk-400">ATVK-400 Electric Seeder/Spreader</a><br /></strong></h3>
-        Worksaver’s model ATVK-400 helps get a head start early in the season. Excellent for seeding pastures and wildlife food plots. Features a lower profile mount that allows the unit to fit most all brands of ATV’s. Durable, maintenance-free 1-bushel polyethylene hopper is translucent for easy monitoring of hopper material level.
+        <h3><a href="/rear-blades-dc-series"><strong>Rear Blades DC Series</strong></a></h3>
+        <p>Designed for Cat. 0/I tractors, the model DC series rear blades has the strength you need to handle most jobs.
+            The DC Series has mechanical angle adjustments that allows the operator to simply change the pin position to
+            allow the blade move material easily and quickly.</p>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-plr-48-60-72"><img class="img-responsive" src="/images/atv-attachments/PLR-48_60_72.jpg" alt="" /></a></div>
+    <div class="col-lg-4 col-md-4"><a href="/rear-blades-mcrb-72"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/MCRB-72.jpg" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/atv-attachments-plr-48-60-72"><strong>PLR-48/60/72&nbsp;Landscape Rakes</strong></a></h3>
-        Designed for quick and easy cleanup of leaves and debris from large areas of lawn, pastures and roadsides – keeps your property neatly groomed. Features spring steel tines spaced at 1” intervals for thorough raking; debris is “windrowed” for easy collection and pick-up. Rakes are available in 4’, 5’ and 6’ widths.
+        <h3><a href="/rear-blades-mcrb-72"><strong>Rear Blades MCRB 72</strong></a></h3>
+        <p>An economical 6-ft blade for tractors up to 35 hp, with Cat. I 3-pt. hitch. The MCRB-72 only offers mechanical angle adjustment.
+            The main frame is formed with 3" x 2.5" channel; replaceable straps are used for the "A" frame.
+            The formed moldboard includes a 1/2" x 6" reversible cutting edge.</p>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-fod-16"><img class="img-responsive" src="/images/atv-attachments/FOD-16.jpg" alt="" /></a></div>
+    <div class="col-lg-4 col-md-4"><a href="/rear-blades-rb-1272-and-rb-1284"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/RB-1272.jpg" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/atv-attachments-fod-16"><strong>FOD-16&nbsp;Flip-Over Disc</strong></a></h3>
-        Ideal for tilling wildlife food plots and larger gardens. Patented design allows for easy “flip-over” from transport to cultivation mode. Features eight (8) 16” notched blades designed for efficient trash cutting and optimum soil penetration.
+        <h3><a href="/rear-blades-rb-1272-and-rb-1284"><strong>Rear Blades RB-1272 and RB-1284</strong></a></h3>
+        <p> Designed to fit Cat.I and Cat.I Quick Hitch. These standard duty blades are ideal for the small acreage owner.
+            The main frame consist of an all welded mast and 3" x 3" square tubing. The RB-Series features mechanical offset
+            and angle adjustment. Angle adjustment is done by simply removing a pin, angleing the blade and reinstalling the pin.
+            All models have a reversible cutting edge and are supplied with a parking stand for convenient storage.
+            Available as an option are skid shoes to assist in cutting height adjustment to reduce the chance of surface damage during snow removal.</p>
     </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-foc-48"><img class="img-responsive" src="/images/atv-attachments/FOC_48.jpg" alt="" /></a></div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/rear-blades-rbhd-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/RBHD-7-HD-Blade.jpg" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/atv-attachments-foc-48"><strong>FOC-48&nbsp;Flip-Over Culti-Packer</strong></a></h3>
-        Perfect for final compaction after seeding your food plot. Reduces air pockets and increases seed germination. Helps prevent erosion. Unique design allows for easy “flip-over” from transport to operation mode. Can also be used for breaking the crust of rain-hardened soil, to allow emerging seedlings to break through and grow.
+        <h3><a href="/rear-blades-rbhd-series"><strong>Rear Blades RBHD-Series</strong></a></h3>
+        <p>Designed to fit Cat.I and Cat.I Quick Hitch. These heavy-duty models have the strength required to handle the tough jobs.
+            The main frame consist of an all welded mast and 4" x 4" square tubing. The RBHD-Series features mechanical offset and angle adjustment.
+            Angle adjustment is done by simply removing a pin, angling the blade and reinstalling the pin. The RBHD-6-T and RBHD-7-T utilizes a
+            3-position tilt assembly that is easy to use. All models have a reversible cutting edge and are supplied with a parking stand for
+            convenient storage. Availabel options are skid shoes to assist in cutting height adjustment to reduce the chance of surface amage during
+            snow removal. End plates provide a boxed area for grading.</p>
+    </div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-mcr-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/mcr-6.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/landscape-rakes-mcr-series"><strong>Landscape Rakes MCR Series</strong></a></h3>
+        <p>6 Ft. Standard Duty Landscape Rake Cat.I (7/8" Pins) and 7 Ft. Standard Duty Landscape Rake Cat.I (7/8" Pins)</p>
+    </div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-lrhd-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/LRHD-6.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/landscape-rakes-lrhd-series"><strong>Landscape Rakes LRHD Series</strong></a></h3>
+        <p>6 Ft. Heavy Duty Landscape Rake Cat.I, 7 Ft. Heavy Duty Landscape Rake Cat.I, and 8 Ft. Heavy Duty Landscape Rake Cat.I</p>
+    </div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-plr-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/PLR48_60_72.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/landscape-rakes-plr-series"><strong>Landscape Rakes PLR Series</strong></a></h3>
+        <p>ATV - Landscape Rakes
+            Nothing handles clean up and grooming jobs better than our ATV landscape rakes.
+            Spring steel tines are spaced at 1" intervals for exceptional performance in pick-up, grooming, and material distribution.
+            Chassis utilizes a clevis-type hitch for easy hook up.</p>
+    </div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-sgw-10"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/sgw-10.gif" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/landscape-rakes-sgw-10"><strong>Landscape Rakes SGW-10</strong></a></h3>
+        <p>Gauge Wheel Package (For DCR Series)</p>
+    </div>
+</div><div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/rear-blades-&-landscape-rakes"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/dgw-20.gif" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/landscape-rakes-dgw-20"><strong>Landscape Rakes DGW-20</strong></a></h3>
+        <p>Dual Gauge Wheel Package (For MCR/LRHD Series</p>
     </div>
 </div>
