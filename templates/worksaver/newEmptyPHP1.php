17a848b2b37b3161b927e948909af61775a783b5
jbake11 <jeffbake40@gmail.com>
2015-01-14 22:19:28 -0500

    material handling

--- templates/worksaver/newEmptyPHP1.php
+++ templates/worksaver/newEmptyPHP1.php
@@ -1,77 +1,105 @@
-<h3>Need to find quality tractor blades or landscape rake?<br /> Worksaver Has the part for you!<br /></h3>
-<p>&nbsp;</p>
+
+
+
+
+
+
+
+
+• Unique tine design with weld-on tine points.
+• Tine design allows operator to go under logs/brush for
+easy clean pick up.
+• Interfacing quick-attach brackets available for most tractor
+loader models including John Deere, AGCO, CNH,
+Bush Hog and others.
+• Greaseable pivot points on upper clamps for long
+service life.
+• Shield Plate in upper clamps provides protection for
+hydraulic cylinders.
+
+
+Split-Top Grapple Rake Specifications
+Choose Worksaver For All Your Material Handling
+FLGR-262 / SSGR-262 / FLGR-262-G FLGR-277 / SSGR-277 / FLGR-277-G
+Overall Width (in/mm) 62 / 1575 77 / 1956
+Overall Depth (in/mm) 25 / 635 25 / 635
+Overall Height Closed (in/mm) 54 / 1372 54 / 1372
+Grapple Opening (in/mm) 50 / 1270 50 / 1270
+Upper Grapple Widths (in/mm) 19.25 / 489 19.25 / 489
+Tine Spacing (in/mm) 15 / 381 15 / 381
+Number of Tines/Grapples (lower/upper) 5 / 4 6 / 4
+Reinforcing Tubes Between Lower Tines Yes Yes
+Number of Hydraulic Cylinders 2 2
+Cylinder Bore (in/mm) 2 / 50.8 2 / 50.8
+Cylinder Stroke (in/mm) 10 / 254 10 / 254
+Cylinder Rod Diameter (in/mm) 1.125 / 28.57 1.125 / 28.57
+Cylinder Pressure Rating (psi/bar) 3000 / 206.9 3000 / 206.9
+Mounting for Tractor Loader (FLGR) Mgt. Brt. Available* Mgt. Brt. Available*
+Weight FLGR Model (lbs/kg) 704 / 319 804 / 365
+Mounting for Skid Steer (SSGR) “Universal” Skid Steer “Universal” Skid Steer
+Weight SSGR Model (lbs/kg) 784 / 356 964 / 437
+Mounting for FLGR–G Models Euro / Global Euro / Global
+Weight FLGR–G Models (lbs/kg) 800 / 363 990 / 449
+Horsepower Rating (maximum) 95 PTO HP 150 PTO HP
+Product No. FLGR Models (loader) 811380 811400
+Product No. SSGR Models (skid steer) 811435 811440
+Product No. FLGR–G Models (Euro/Global) 811495 811490
+
+
 
 
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/rear-blades-dc-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/DC-5.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/rear-blades-dc-series"><strong>Rear Blades DC Series</strong></a></h3>
-        <p>Designed for Cat. 0/I tractors, the model DC series rear blades has the strength you need to handle most jobs.
-            The DC Series has mechanical angle adjustments that allows the operator to simply change the pin position to
-            allow the blade move material easily and quickly.</p>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+
+
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img src="images/material-handling/" alt="" />
+    </div>
+</div>
+<br /><br />
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+
+
+
+        <div>
+            <ul>
+                <li>Designed to handle either two 4' x 4' x 8' square bales or three 3' x 3' x 8' square bales.</li>
+                <li>Bale support is provided on the bottom by five (5) 26" long forged steel spears.</li>
+                <li>Six (6) 18" forged spear are provided for the uprights to help secure the bales in transport.</li>
+                <li>Each side utilizes five (5) bale hooks that are moveable to allow the operator to position the in the most convenient location.</li>
+                <li>Bale hooks rotate hydraulically on square shafts supported by UHMW-polyethylene bushings.</li>
+            </ul>
+        </div>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/rear-blades-mcrb-72"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/MCRB-72.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/rear-blades-mcrb-72"><strong>Rear Blades MCRB 72</strong></a></h3>
-        <p>An economical 6-ft blade for tractors up to 35 hp, with Cat. I 3-pt. hitch. The MCRB-72 only offers mechanical angle adjustment.
-            The main frame is formed with 3" x 2.5" channel; replaceable straps are used for the "A" frame.
-            The formed moldboard includes a 1/2" x 6" reversible cutting edge.</p>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr class="table-col-header">
+                    <td colspan="4" align="center">Models RBH-4000 Specifications</td>
+                </tr>
+                <tr><td>Model</td><td>Item No.</td><td>Description</td><td>Weight</td></tr>
+                <tr><td>RBH-4000</td><td>831460</td><td>Large Rectangular Bale Handler, rated at 4,000 lbs.
+                        Designed for tractor front loaders. Optional pin-on brackets
+                        or interfacing brackets for most popular quick-attach front
+                        loaders are available (loader arm distance of 54" or less)
+                    </td><td>881 lbs.</td></tr>
+
+                <tr><td>vRBH-4000G</td><td>831487</td><td>Large Rectangular Bale Handler for tractor front loaders
+                        with Euro/Global carrier system. Rated at 4,000 lbs.
+                    </td><td>870 lbs.</td></tr>
+
+
+                </tr>
+                <tr>
+                    <td colspan="4" align="center"><a href="/templates/worksaver/manuals/rbh4000.pdf">Download Manual Here</a></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/rear-blades-rb-1272-and-rb-1284"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/RB-1272.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/rear-blades-rb-1272-and-rb-1284"><strong>Rear Blades RB-1272 and RB-1284</strong></a></h3>
-        <p> Designed to fit Cat.I and Cat.I Quick Hitch. These standard duty blades are ideal for the small acreage owner.
-            The main frame consist of an all welded mast and 3" x 3" square tubing. The RB-Series features mechanical offset
-            and angle adjustment. Angle adjustment is done by simply removing a pin, angleing the blade and reinstalling the pin.
-            All models have a reversible cutting edge and are supplied with a parking stand for convenient storage.
-            Available as an option are skid shoes to assist in cutting height adjustment to reduce the chance of surface damage during snow removal.</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/rear-blades-rbhd-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/RBHD-7-HD-Blade.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/rear-blades-rbhd-series"><strong>Rear Blades RBHD-Series</strong></a></h3>
-        <p>Designed to fit Cat.I and Cat.I Quick Hitch. These heavy-duty models have the strength required to handle the tough jobs.
-            The main frame consist of an all welded mast and 4" x 4" square tubing. The RBHD-Series features mechanical offset and angle adjustment.
-            Angle adjustment is done by simply removing a pin, angling the blade and reinstalling the pin. The RBHD-6-T and RBHD-7-T utilizes a
-            3-position tilt assembly that is easy to use. All models have a reversible cutting edge and are supplied with a parking stand for
-            convenient storage. Availabel options are skid shoes to assist in cutting height adjustment to reduce the chance of surface amage during
-            snow removal. End plates provide a boxed area for grading.</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-mcr-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/mcr-6.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/landscape-rakes-mcr-series"><strong>Landscape Rakes MCR Series</strong></a></h3>
-        <p>6 Ft. Standard Duty Landscape Rake Cat.I (7/8" Pins) and 7 Ft. Standard Duty Landscape Rake Cat.I (7/8" Pins)</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-lrhd-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/LRHD-6.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/landscape-rakes-lrhd-series"><strong>Landscape Rakes LRHD Series</strong></a></h3>
-        <p>6 Ft. Heavy Duty Landscape Rake Cat.I, 7 Ft. Heavy Duty Landscape Rake Cat.I, and 8 Ft. Heavy Duty Landscape Rake Cat.I</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-plr-series"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/PLR48_60_72.jpg" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/landscape-rakes-plr-series"><strong>Landscape Rakes PLR Series</strong></a></h3>
-        <p>ATV - Landscape Rakes
-            Nothing handles clean up and grooming jobs better than our ATV landscape rakes.
-            Spring steel tines are spaced at 1" intervals for exceptional performance in pick-up, grooming, and material distribution.
-            Chassis utilizes a clevis-type hitch for easy hook up.</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/landscape-rakes-sgw-10"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/sgw-10.gif" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/landscape-rakes-sgw-10"><strong>Landscape Rakes SGW-10</strong></a></h3>
-        <p>Gauge Wheel Package (For DCR Series)</p>
-    </div>
-</div><div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/rear-blades-&-landscape-rakes"><img class="img-responsive" src="/images/rear-blades-landscape-rakes/dgw-20.gif" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/landscape-rakes-dgw-20"><strong>Landscape Rakes DGW-20</strong></a></h3>
-        <p>Dual Gauge Wheel Package (For MCR/LRHD Series</p>
-    </div>
 </div>
