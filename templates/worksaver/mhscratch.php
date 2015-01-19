bd5b81e42eb62a33d1fdb96f9645995b816bb79e
jbake11 <jeffbake40@gmail.com>
2015-01-19 16:41:27 -0500

    asd

--- templates/worksaver/mhscratch.php
+++ templates/worksaver/mhscratch.php
@@ -1,52 +1,82 @@
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <img src="/images/material-handling/Low-Profile-Tine-Grapple.jpg" alt="" />
+        Revolutionizing the way rock
+        grapples work, the SARG
+        (Sweep Action Rock Grapple)
+        from Worksaver makes life easier.
+        With its patent pending design,
+        the SARG allows the operator to
+        own one tool to sort large or small
+        debris, resulting in low cost of
+        ownership. The front grapple
+        operates in a sweeping action to
+        pull rock or debris into the unit.
+        Grapple design retains the rocks
+        or debris, allowing the operator to
+        rock the attachment to sift dirt out
+        of the material. Rocks, brush, logs,
+        stumps, debris or scrap, the SARG
+        handles them all.
     </div>
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        Designed for skid steers, the
-        LPTG series tine grapple easily
-        moves rocks, brush, wood,
-        metal and other materials while
-        leaving the dirt on the ground.
-        The grapple is ideal for various applications
-        including construction site demolition and cleanup,
-        storm clean-up, and landscaping. Features
-        include 5-½” tine spacing, 3,000 psi cylinders
-        with cylinder guards to protect the cylinders and
-        components. Hoses are equipped with “connect
-        under pressure” flat-faced couplers.
-        <br />
-        <a href="/templates/worksaver/literature/wsr6480_Material Handling.pdf">Download the literature</a>
+        <img class="img-responsive" src="/images/material-handling/SARG_withrock.jpg" alt="" />
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <div>
+            <img class="img-responsive" src="/images/material-handling/Bolt-on_sideplates.jpg" alt="" />
+            <div style="max-width:300px; word-wrap:break-word;">
+                Bolt-on side plates hold smaller rocks in the bucket. They can be removed from the bucket to allow the SARG to handle logs and stumps.
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <div>
+            <img class="img-responsive" src="/images/material-handling/SARG_lineart.jpg" alt="" />
+        </div>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr class="table-col-header"><td colspan="4" align="center">Low Profile Tine Grapple</td></tr>
-                <tr><td>&nbsp;</td><td>LPTG-72</td><td>LPTG-78</td><td>LPTG-84</td></tr>
-                <tr><td>Overall Width (in/mm)</td><td>72 / 1829</td><td>78 / 1981</td><td>84 / 2134</td></tr>
-                <tr><td>Tine Spacing (in/mm)</td><td>5.5 / 140</td><td>5.5 / 140</td><td>5.5 / 140</td></tr>
-                <tr><td>Number of Tines</td><td>14 </td><td>15</td><td>16</td></tr>
-                <tr><td>Tine Length (in/mm)</td><td>29.5 / 750</td><td>29.5 / 750</td><td>29.5 / 750</td></tr>
-                <tr><td>Grapple Opening (in/mm)</td><td>36.5 / 927</td><td>36.5 / 927</td><td>36.5 / 927</td></tr>
-                <tr><td>Cylinder Pressure Rating (psi/bar)</td><td>3000 / 207</td><td>3000 / 207</td><td>3000 / 207</td></tr>
-                <tr><td>Couplers</td><td colspan="3" align="center">1/2 / 13 Body – Connect Under Pressure</td></tr>
-                <tr><td>Mount Type</td><td colspan="3" align="center">“Universal Skid Steer”</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>820 / 372</td><td>864 / 392</td><td>896 / 406</td></tr>
-                <tr><td>Product No.</td><td>811750</td><td>811800</td><td>811840</td></tr>
-                <tr><td><a href="/templates/worksaver/manuals/lptsb.pdf">Download Manual Here</a></td>
-                    <td><a href="/low-profile-tine-grapple-lptg">Main Unit Parts List Click Here</a></td>
-                    <td colspan="2"><a href="/low-profile-tine-grapple-lptg-hyd">LPTG-72, 78, 84 Hyd. Parts List Click Here</a></td>
-                </tr>
+
+                <tr class="table-col-header"><td colspan="3" align="center">SARG Specifications</td></tr>
+                <tr><td>Model</td><td>SARG-72</td><td>SARG-84</td></tr>
+                <tr><td>Overall Width (in/mm)</td><td>72 / 1829</td><td>84 / 2134</td></tr>
+                <tr><td>Bucket Depth (in/mm)</td><td>34 / 864</td><td>34 / 864</td></tr>
+                <tr><td>Upper Grapple Width (in/mm)</td><td>34 / 864</td><td>40 / 1016</td></tr>
+                <tr><td>Grapple Opening (in/mm)</td><td>45 / 1143</td><td>45 / 1143</td></tr>
+                <tr><td>Tine Diameter (in/mm)</td><td>1.25 / 31.75</td><td>1.25 / 31.75</td></tr>
+                <tr><td>Weight (lbs/kg)</td><td>1194 / 542</td><td>1375 / 624</td></tr>
+                <tr><td>Product No.</td><td>812120</td><td>812160</td></tr>
+<!--                <tr><td><a href="/templates/worksaver/manuals/.pdf">Download Manual Here</a></td>
+                    <td><a href="/sarg-sweep-action-rock-grapple-sarg-72">Parts List Click Here</a></td>
+                    <td><a href="/sarg-sweep-action-rock-grapple-sarg-84">Parts List Click Here</a></td>
+                </tr>-->
             </tbody>
         </table>
     </div>
 </div>
+
 <div>
     <i class="fa fa-lg fa-exclamation-triangle"> </i>
     <strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer
     is properly counterbalanced. Damage to equipment or personal injury could occur.
 </div>
 <br />
+
+
+
+
+
+
+
+
+
+
+
+
 
--- templates/worksaver/video.php
+++ templates/worksaver/video.php
@@ -1,24 +1,27 @@
 <div class="row">
     <div class="video-align col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Worksaver Post Hole Digger 500</h4>
+        <h3>Worksaver Post Hole Digger 500</h3>
         <iframe width="560" height="315" src="//www.youtube.com/embed/3zrKLmg5s5o" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>
+<br />
 <div class="row">
     <div class="video-align col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Worksaver Skid Steer Post Hole Driver</h4>
+        <h3>Worksaver Skid Steer Post Hole Driver</h3>
         <iframe width="560" height="315" src="//www.youtube.com/embed/SagxCgNX1co" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>
+<br />
 <div class="row">
     <div class="video-align col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Worksaver Post Driver with Q Technology</h4>
+        <h3>Worksaver Post Driver with Q Technology</h3>
         <iframe width="560" height="315" src="//www.youtube.com/embed/57Ua1eOpKBU" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>
+<br />
 <div class="row">
     <div class="video-align col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h4>Stump Grinder from Worksaver</h4>
+        <h3>Stump Grinder from Worksaver</h3>
         <iframe width="560" height="315" src="//www.youtube.com/embed/j1FDsvnZNvc" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>
