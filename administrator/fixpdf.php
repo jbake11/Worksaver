cc42d6846178ad8eba7d4ab857cececbaef984d4
Chris Bake <cbake7806@gmail.com>
2015-01-20 14:22:58 -0500

    Please enter the fommt message for your changes. Lines starting

--- administrator/fixpdf.php
+++ administrator/fixpdf.php
@@ -9,7 +9,7 @@ $select = "SELECT * from c35su_content";
 
 $result = mysql_query($select);
 
-while ($row = mysql_fetch_array($result)):
+while ($row = mysql_fetch_assoc($result)):
 
 
     $find = array(
@@ -26,6 +26,6 @@ while ($row = mysql_fetch_array($result)):
     $intro = str_replace($find, $replace, $row['introtext']);
 
     $update = "UPDATE c35su_content SET `introtext` = '" . $intro . "', `fulltext` = '" . $full . "' WHERE `id` = " . $row['id'] . " LIMIT 1";
-//    mysql_query($update, $db2);
+    mysql_query($update, $db2);
 
 endwhile;
