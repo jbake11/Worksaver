8fa62e5c991753d058dfd26c46534b026a8281c6
jbake11 <jeffbake40@gmail.com>
2015-01-21 21:14:13 -0500

    asd

--- templates/worksaver/mhscratch.php
+++ templates/worksaver/mhscratch.php
@@ -1,5 +1,8 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <img class="img-responsive" src="/images/material-handling/SkidSteer_CraneBoom.jpg" alt="" />
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
         The SSTB-3500 Crane Boom is ideal
         for lifting or moving objects such as
         balled trees, bundles of materials, or
@@ -15,28 +18,64 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr class="table-col-header"><td colspan="3" align="center">Specifications</td></tr>
-                <tr><td>Model</td><td>BF-1000</td><td>BF-2000</td><td>BF-4000</td></tr>
-                SSTB-3500
-                Lifting Capacity at Farthest Attaching Point (lbs/kg) 3200 / 1451
-                Lifting Capacity at Middle Attaching Point (lbs/kg) 3500 / 1587
-                Lifting Capacity at Closest Attaching Point (lbs/kg) 3700 / 1678
-                Overall Boom Length (in/mm) 59.25 / 1505
-                Boom Construction 3” Square Steel Tubing
-                Mounting Type “Universal” Skid Steer
-                Weight (lbs/kg) 182 / 83
-                Product Item No. 81140
+                <tr class="table-col-header"><td colspan="2" align="center">Specifications</td></tr>
+                <tr><td>Model</td><td>SSTB-3500</td></tr>
+                <tr><td>Lifting Capacity at Farthest Attaching Point (lbs/kg)</td><td>3200 / 1451</td></tr>
+                <tr><td>Lifting Capacity at Middle Attaching Point (lbs/kg)</td><td>3500 / 1587</td></tr>
+                <tr><td>Lifting Capacity at Closest Attaching Point (lbs/kg)</td><td>3700 / 1678</td></tr>
+                <tr><td>Overall Boom Length (in/mm)</td><td>59.25 / 1505</td></tr>
+                <tr><td>Boom Construction</td><td>3” Square Steel Tubing</td></tr>
+                <tr><td>Mounting Type</td><td>“Universal” Skid Steer</td></tr>
+                <tr><td>Weight (lbs/kg)</td><td>182 / 83</td></tr>
+                <tr><td>Product Item No.</td><td>81140</td></tr>
                 <tr>
-                    <td><a href="/templates/worksaver/manuals/wsBF1000.pdf">Download Manual Here</a></td>
-                    <td><a href="/clamp-on-bucket-forks-bf-1000">Parts List Click Here</a></td>
-                    <td><a href="/clamp-on-bucket-forks-bf-2000">Parts List Click Here</a></td>
-                    <td><a href="/clamp-on-bucket-forks-bf-4000">Parts List Click Here</a></td>
+                    <td>&nbsp;</td>
+                    <td><a href="/skid-steer-crane-boom-sstb-3500">Parts List Click Here</a></td>
                 </tr>
+                <tr><td colspan="2">*Optional interfacing quick attach brackets available for most popular loaders.</td></tr>
             </tbody>
         </table>
     </div>
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">*Optional interfacing quick attach brackets available for most popular loaders.
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <div><strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer is properly counterbalanced. Damage to equipment or personal injury could occur.</div>
     <p>&nbsp;</p>
+</div>
+
+
+
+
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <table class="table table-bordered table-responsive table-striped">
+            <tbody>
+                <tr class="table-col-header"><td align="center" colspan="4">Specifications</td></tr>
+                <tr class="table-col-img"><td colspan="4" align="center"><img class="img-responsive" src="/images/material-handling/FLGR-4062.gif" alt="" /></td></tr>
+                <tr><td>Ref. No</td><td>Part No.</td><td>Description</td><td>No. Req'd</td></tr>
+                <tr><td>1</td><td>811461</td><td>Main Frame Weldment (FLGR-4062)</td><td>1</td></tr>
+                <tr><td>2</td><td>811471</td><td>Grapple Clamp Weldment</td><td>1</td></tr>
+                <tr><td>3</td><td>341338</td><td>Point, Replaceable Grapple (Weld-On)</td><td>5</td></tr>
+                <tr><td>4</td><td>2503257</td><td>Bolt, Hex 1"-8NC x 6" Gr. 5</td><td>1</td></tr>
+                <tr><td>5</td><td>2503309</td><td>Bolt, 1"-8NC x 4" Hex Head Gr.5</td><td>2</td></tr>
+                <tr><td>6</td><td>2500045</td><td>Nut, 1"-8NC Hex Nylock</td><td>3</td></tr>
+                <tr><td>7</td><td>2504049</td><td>Zerk, Drive 5/16"</td><td>3</td></tr>
+                <tr><td>8</td><td>2504230</td><td>Cylinder, Hydraulic 2 x 8 ARH</td><td>1</td></tr>
+                <tr><td>9</td><td>2504007</td><td>Pin, Cotter 3/16" x 1-3/4"</td><td>2</td></tr>
+                <tr><td>10</td><td>2505035</td><td>Spring Bushing, 1.25 x 1 x 1</td><td>2</td></tr>
+                <tr><td>11</td><td>2505609</td><td>Hyd. Adapter 90° 3/4-16 ORB/F 1/4-18 NPSM</td><td>2</td></tr>
+                <tr><td>12</td><td>811236</td><td>Bracket, RH Lift (1" Holes)</td><td>2</td></tr>
+                <tr><td>13</td><td>811237</td><td>Bracket, LH Lift (1" Holes)</td><td>2</td></tr>
+                <tr><td>14</td><td>811238</td><td>Lift Pin 1" x 6"</td><td>4</td></tr>
+                <tr><td>15</td><td>590034</td><td>Linch Pin, 1/4" (AK-61)</td><td>8</td></tr>
+                <tr><td>16</td><td>830214</td><td>U-Bolt, 5/8"-11NC 3" x 4-1/2"</td><td>8</td></tr>
+                <tr><td>17</td><td>2502007</td><td>Washer, 5/8" Springlock)</td><td>16</td></tr>
+                <tr><td>18</td><td>2500013</td><td>Nut, 5/8"-11NC Full Hex</td><td>16</td></tr>
+                <tr><td>19</td><td>101026</td><td>Safety Sign - Caution (Instructions)</td><td>1</td></tr>
+                <tr><td>20</td><td>101027</td><td>Safety Sign - Warning (Stand Clear)</td><td>2</td></tr>
+                <tr><td>21</td><td>101122</td><td>Safety Sign - Warning (Hi-Pressure)</td><td>1</td></tr>
+                <tr><td>22</td><td>101204</td><td>Safety Sign - Notice (Grapples)</td><td>1</td></tr>
+                <tr><td>23</td><td>101129</td><td>Safety Sign - Warning (Grapples)</td><td>1</td></tr>
+            <tbody>
+        </table>
+    </div>
 </div>
