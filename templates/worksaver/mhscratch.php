8c5a40cd66d9cb5821692e2caa95117429dd4fa1
jbake11 <jeffbake40@gmail.com>
2015-01-23 09:41:30 -0500

    cd

--- templates/worksaver/mhscratch.php
+++ templates/worksaver/mhscratch.php
@@ -1,95 +1,64 @@
-<h2>Consistently delivering a different approach</h2><br />
+<h2>Our Post Hole Diggers Dig Deeper, Faster & Smarter</h2><br />
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        Worksaver with over two decades of post driver innovations now offers two series
-        of post drivers. Our standard duty model HPD-16 incorporates many of the
-        innovations first brought to the market by Worksaver: “convertible” hydraulic valve,
-        grease zerks at all major pivot locations, plated manual cranks, plastic slide blocks,
-        adjustable legs, bubble level, and SplitCap™. The HPD-16 (32,000 lbs. of force) is
-        ideal for the small property owner with limited fence posts to drive.
-        Our Q-series of hydraulic drivers, HPD-22Q (80,000 lbs. of force) and HPD-26Q
-        (110,000 lbs. of force) offer ingenious solutions to routine maintenance to
-        keep the operator building fence. Q-series drivers feature two time savings
-        QuickChange™ system. Worksaver’s SplitCap™ with enhanced cylinder rod design
-        allows the operator to perform routine maintenance on the rod cap seal
-        without removing the cylinder from the driver. Our Q-spring assembly reduces
-        spring change over from hours to minutes. This design offers increased service
-        life when compared to conventional post driver springs. The integrated spring/
-        spring-anchor design when mounted to the cushion mounts reduces vibration
-        which further provides increased spring service life. Increasing fence-building
-        performance, making the Q-series a game changer.
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        The Q-series of hydraulic post drivers continues Worksaver’s innovation
-        with post drivers. In the past two decades Worksaver has brought the following
-        enhancements to the market:
-        <br /><br />
-        <ul>
-            <li>First to replace metal rollers with plastic slide blocks</li>
-            <li>Patented skid steer return lines on skid steer post drivers</li>
-            <li>First to install a rubber deflector guard around driving head assembly</li>
-            <li>Self-contain post driver for skid steer loaders</li>
-            <li>First to develop front loader mounted post drivers</li>
-            <li>First to develop “universal” return line fitting</li>
-            <li>First with circular buddle level on all models</li>
-            <li>First with adjustable legs on all models</li>
-            <li>First with “convertible” valve for open center/closed center hydraulic systems</li>
-        </ul>
-    </div>
-    <div id="quick-change-adjust" class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <h4>New QuickChange system:</h4>
-        Worksaver has introduced a new Feature called The QuickChange System for replacing the rod seal in the field. <br />
-        <a href="/hydraulic-post-drivers-quickchange-system">New QuickChange System</a>&nbsp;
-        <a href="/HPD-20quick-20change.pdf">Download Instructions to change cyl. rod seal</a>
+        Engineered to allow the operator to dig deeper, faster and smarter, Worksaver’s complete
+        line of PTO powered diggers offer time saving convenience with outstanding reliability.
+        Shear-bolt protected driveline is coupled to a rugged gearbox featuring tapered roller bearings
+        and spring-loaded seals for extended service life. Large 2” output shaft has two-bolt auger
+        connection for reliable power transfer. All job matched augers utilize fishtail center points
+        with double flighting on the first turn for quick, positive digging action right from the start.
     </div>
 </div>
 <br />
 <div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-4 col-md-4"><a href="/model-300-digger-for-sub-compact-tractors"><img class="img-responsive" src="/images/fence-equipment/Model_300.jpg" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q Manual Adjust, 3-pt. Hydraulic Post Drivers</strong></a></h3>
-        The HPD-16/22Q models with manual adjust quickly deliver an air tight post set for wood or steel posts. Base model offers manual adjustment cranks and bubble level for prefect vertical alignment on flatland or hillsides.
+        <h3><a href="/model-300-digger-for-sub-compact-tractors"><strong>Model 300 Digger for Sub-compact Tractors</strong></a></h3>
+        Ideal for small acreage owners, model 300 mates to most sub-compact tractors with Cat. 0 or limited Cat. I three point hitches. Standard positioning handle allows the operator to place the digger from the tractor seat.</div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/model-500-standard-duty-post-hole-digger"><img class="img-responsive" src="/images/fence-equipment/Model_500.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/model-500-standard-duty-post-hole-digger"><strong>Model 500 Standard Duty Post Hole Digger</strong></a></h3>
+        Our most popular digger, the model 500 standard duty offers 3-position boom adjustment allowing it to adapt to most tractors. Tubular steel stock of 2.875" diameter is used for the boom for increased durability.</div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/model-720-heavy-duty-post-hole-digger"><img class="img-responsive" src="/images/fence-equipment/Model_720.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/model-720-heavy-duty-post-hole-digger"><strong>Model 720 Heavy-duty Post Hole Digger</strong></a></h3>
+        Increased performance is offered in the model 720 heavy-duty digger. Designed for Cat. I/II  tractors, this model offers 3-position boom adjustment, rugged gearbox and a 3-1/2" diameter boom for dependable operation.</div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/model-925-industrial-duty-post-hole-digger"><img class="img-responsive" src="/images/fence-equipment/Model_925.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/model-925-industrial-duty-post-hole-digger"><strong>Model 925 Industrial Duty Post Hole Digger</strong></a></h3>
+        Industrial duty workhorse, the model 925 provides dependable operation when mated with any auger from 6" to 30" in diameter. Fits and Cat. I/II tractor with 540 rpm PTO.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/models-914hc-924hc-skid-steer-mounted-hydraulic-post-hole-digger"><img class="img-responsive" src="/images/fence-equipment/Models_914H_924HC.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/models-914hc-924hc-skid-steer-mounted-hydraulic-post-hole-digger"><strong>Models 914HC, 924HC Skid Steer Mounted Hydraulic Post Hole Digger</strong></a></h3>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q/26Q SHC Hydraulic Adjust, 3-pt. Hydraulic Post Drivers</strong></a></h3>
-        Designed and engineered for those who have serious fencing to do. These center mounted units offer all the quality features of the basic HPD-16 & HPD22Q plus effortless hydraulic driver alignment controlled by two 3" x 6" hydraulic cylinders.
+        <h3><a href="/"><strong></strong></a></h3>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q/26Q SWO Offset 3-pt. Hydraulic Post Drivers</strong></a></h3>
-        Driving posts along existing fence lines, ditch banks, or corners is easily done with a SWO model. Heavy-duty 3-angle swivel mount pivots the driver 45°, 90° or rear facing for increased versatility.
+        <h3><a href="/"><strong></strong></a></h3>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q HFL Front Loader Mounted. Hydraulic Post Drivers</strong></a></h3>
-        Front loader mounted hydraulic adjust post drivers offer increased visibility and maneuverability for improved access in hard-to-reach areas. Available as either a HPD-16 or HPD-22Q, these drivers can be positioned to the center or side of the loader for easier fence building.
+        <h3><a href="/"><strong></strong></a></h3>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q MSS/HSS, HPD-26Q HSS Skid Steer Mounted, Hydraulic Post Drivers</strong></a></h3>
-        For greater multi-task performance from your skid steer, connect it to a hydraulic skid steer mounted post driver. The HPD-16 & HPD-22 are offered either with manual crank adjustment (MSS) or hydraulic adjustment (HSS) for quicker productivity.
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
-    <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong>HPD-16/22Q/26Q HSS/P Self-contained, Skid Steer Mounted, Hydraulic Post Drivers</strong></a></h3>
-        Save time by simplifying driver installation and removal with a self-contained hydraulic post driver. Features include a hydraulic pump, motor and reservoir. No special return line fittings are required.
-    </div>
-</div>
-
-
 <div>
     <i class="fa fa-lg fa-exclamation-triangle"> </i>
     <strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer
