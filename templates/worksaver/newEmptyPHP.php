3747e3efed822f1b5221d187e7eb10c80deaac92
jbake11 <jeffbake40@gmail.com>
2015-01-23 10:03:06 -0500

    qc-sys-img

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,12 +1,10 @@
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
         <img class="img-responsive" src="/images/fence-equipment/QuickChange.jpg" alt="" />
         <br /><br />
         <a href="/images/fence-equipment/HPD-20quick-20change.pdf">Download Instructions to change cyl. rod seal</a>
     </div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
         Worksaver is introducing the QuickChange system featuring a new End Cap (Splitcap) for our HPD-series of Post Drivers. This new SplitCap will allow the end user to easily replace the rod seal in the field.
         The QuickChange system also involves a change to the cylinder rod and attaching hardware. The serial number break and parts information is listed below.
 
@@ -16,13 +14,13 @@
     <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td class="table-col-header" colspan="2" align="center"></td></tr>
-                HPD-16 Effective with S/N 4077
-                Description	New Item Number
-                Cylinder Rod (now 1-inch dia.)	815166
-                End Cap w/Rod Seal	815170
-                Shoulder Nut	815205
-                New Top Nut	2500017
+                <tr><td class="table-col-header" colspan="2" align="center">HPD-16 Effective with S/N 4077</td></tr>
+
+                <tr><td class="table-col-modelnum">Description</td><td>New Item Number</td></tr>
+                <tr><td>Cylinder Rod (now 1-inch dia.)</td><td>815166</td></tr>
+                <tr><td>End Cap w/Rod Seal</td><td>815170</td></tr>
+                <tr><td>Shoulder Nut</td><td>815205</td></tr>
+                <tr><td>New Top Nut</td><td>2500017</td></tr>
             </tbody>
         </table>
     </div>
@@ -31,13 +29,12 @@
     <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td class="table-col-header" colspan="2" align="center"></td></tr>
-                HPD-20 Effective with S/N 4531
-                Description	New Item Number
-                Cylinder Rod	815204
-                End Cap w/Rod Seal	815170
-                Shoulder Nut	815205
-                New Top Nut	2500017
+                <tr><td class="table-col-header" colspan="2" align="center">HPD-20 Effective with S/N 4531</td></tr>
+                <tr><td class="table-col-modelnum">Description</td><td>New Item Number</td></tr>
+                <tr><td>Cylinder Rod</td><td>815204</td></tr>
+                <tr><td>End Cap w/Rod Seal</td><td>815170</td></tr>
+                <tr><td>Shoulder Nut</td><td>815205</td></tr>
+                <tr><td>New Top Nut</td><td>2500017</td></tr>
             </tbody>
         </table>
     </div>
@@ -46,19 +43,16 @@
     <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
-                <tr><td class="table-col-header" colspan="2" align="center"></td></tr>
-                HPD-24 Effective with S/N 1284
-                Description	New Item Number
-                Cylinder Rod	815204
-                End Cap w/Rod Seal	815170
-                Shoulder Nut	815205
-                New Top Nut	2500017
+                <tr><td class="table-col-header" colspan="2" align="center">HPD-24 Effective with S/N 1284</td></tr>
+                <tr><td class="table-col-modelnum">Description</td><td>New Item Number</td></tr>
+                <tr><td>Cylinder Rod</td><td>815204</td></tr>
+                <tr><td>End Cap w/Rod Seal</td><td>815170</td></tr>
+                <tr><td>Shoulder Nut</td><td>815205</td></tr>
+                <tr><td>New Top Nut</td><td>2500017</td></tr>
             </tbody>
         </table>
     </div>
 </div>
-
-
 <div>
     <i class="fa fa-lg fa-exclamation-triangle"> </i>
     <strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer
