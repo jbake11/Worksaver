138206675bc06abf5cafeafa5b39b1c71bdc23a2
jbake11 <jeffbake40@gmail.com>
2015-01-22 22:40:01 -0500

    QuickChange

--- templates/worksaver/mhscratch.php
+++ templates/worksaver/mhscratch.php
@@ -1,176 +1,91 @@
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <img class="img-responsive" src="/images/material-handling/SkidSteer_CraneBoom.jpg" alt="" />
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        The SSTB-3500 Crane Boom is ideal
-        for lifting or moving objects such as
-        balled trees, bundles of materials, or
-        equipment on the job site. Features
-        three lifting points for increased
-        flexibility in loading and lifting. All
-        welded steel construction for long life.
-        Boom is 3” square tube stock providing
-        strength and durability.
-    </div>
-</div>
+<h2>Consistently delivering a different approach</h2><br />
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header"><td colspan="2" align="center">Specifications</td></tr>
-                <tr><td>Model</td><td>SSTB-3500</td></tr>
-                <tr><td>Lifting Capacity at Farthest Attaching Point (lbs/kg)</td><td>3200 / 1451</td></tr>
-                <tr><td>Lifting Capacity at Middle Attaching Point (lbs/kg)</td><td>3500 / 1587</td></tr>
-                <tr><td>Lifting Capacity at Closest Attaching Point (lbs/kg)</td><td>3700 / 1678</td></tr>
-                <tr><td>Overall Boom Length (in/mm)</td><td>59.25 / 1505</td></tr>
-                <tr><td>Boom Construction</td><td>3” Square Steel Tubing</td></tr>
-                <tr><td>Mounting Type</td><td>“Universal” Skid Steer</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>182 / 83</td></tr>
-                <tr><td>Product Item No.</td><td>81140</td></tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td><a href="/skid-steer-crane-boom-sstb-3500">Parts List Click Here</a></td>
-                </tr>
-                <tr><td colspan="2">*Optional interfacing quick attach brackets available for most popular loaders.</td></tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div><strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer is properly counterbalanced. Damage to equipment or personal injury could occur.</div>
-    <br />
-</div>
-
-
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        Designed for skid steer or tractor
-        front loaders, our standard pallet
-        forks have rated capacities from
-        4,000 lbs. to 5,500 lbs. All models
-        feature solid steel alloy forks for long
-        life, with spring loaded latches for
-        easy adjustment spacing.
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <img class="img-responsive" src="/images/material-handling/PalletForks.jpg" alt="" />
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        Worksaver with over two decades of post driver innovations now offers two series
+        of post drivers. Our standard duty model HPD-16 incorporates many of the
+        innovations first brought to the market by Worksaver: “convertible” hydraulic valve,
+        grease zerks at all major pivot locations, plated manual cranks, plastic slide blocks,
+        adjustable legs, bubble level, and SplitCap™. The HPD-16 (32,000 lbs. of force) is
+        ideal for the small property owner with limited fence posts to drive.
+        Our Q-series of hydraulic drivers, HPD-22Q (80,000 lbs. of force) and HPD-26Q
+        (110,000 lbs. of force) offer ingenious solutions to routine maintenance to
+        keep the operator building fence. Q-series drivers feature two time savings
+        QuickChange™ system. Worksaver’s SplitCap™ with enhanced cylinder rod design
+        allows the operator to perform routine maintenance on the rod cap seal
+        without removing the cylinder from the driver. Our Q-spring assembly reduces
+        spring change over from hours to minutes. This design offers increased service
+        life when compared to conventional post driver springs. The integrated spring/
+        spring-anchor design when mounted to the cushion mounts reduces vibration
+        which further provides increased spring service life. Increasing fence-building
+        performance, making the Q-series a game changer.
+        <br /><br />
+        The Q-series of hydraulic post drivers continues Worksaver’s innovation
+        with post drivers. In the past two decades Worksaver has brought the following
+        enhancements to the market:
+        <br /><br />
         <ul>
-
-            <li>Skid steer pallet forks (4,000 lbs.
-                are offered with a one piece backstop
-                or a step through design
-                for easy cab access and excellent
-                visibility.</li>
-            <li>Skid steer models mount with the
-                “Universal” quick-attach mounting
-                system.</li>
-            <li>Premium forks include spring
-                loaded latches for easy adjustment
-                spacing.</li>
-            <li>Models available with integrated
-                brackets for skid steers, John
-                Deere 400/500/600/700 series
-                loaders, and loaders with euro/
-                global attaching method.</li>
-            <li>Optional interfacing quick-attach
-                brackets available for most other
-                loader models.</li>
-            <li>Provides versatility for your skid
-                steer or tractor front loader.</li>
+            <li>First to replace metal rollers with plastic slide blocks</li>
+            <li>Patented skid steer return lines on skid steer post drivers</li>
+            <li>First to install a rubber deflector guard around driving head assembly</li>
+            <li>Self-contain post driver for skid steer loaders</li>
+            <li>First to develop front loader mounted post drivers</li>
+            <li>First to develop “universal” return line fitting</li>
+            <li>First with circular buddle level on all models</li>
+            <li>First with adjustable legs on all models</li>
+            <li>First with “convertible” valve for open center/closed center hydraulic systems</li>
         </ul>
     </div>
+    <br /><br />
+    <h4><a href="/hydraulic-post-drivers-quickchange-system">New QuickChange system:</a></h4><br /> Worksaver has introduced a new Feature called The QuickChange System for replacing the rod seal in the field.    Download Instructions to change cyl. rod seal
 </div>
+<br />
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header">
-                    <td colspan="4" align="center">For Skid Steer & Tractor Front Loaders the “Universal” Quick Attach System</td>
-                    <td>For Tractor Loaders</td>
-                </tr>
-                <tr><td>Model</td><td>SSPF-442</td><td>SSPF-448S</td><td>SSPF-3748</td><td>PF-448</td></tr>
-                <tr><td>Equipment Used On</td><td>Skid Steer Loader</td><td>Skid Steer Loader</td><td>Skid Steer Loader</td><td>Tractor Loader</td></tr>
-                <tr><td>Capacity (lbs/kg)</td><td>4,000 / 1,814</td><td>4,000 / 1,814</td><td>4,000 / 1,814</td><td>4,000 / 1,814</td></tr>
-                <tr><td>Frame Construction</td><td>Welded Steel</td><td>Welded Steel</td><td>Welded Steel</td><td>Welded Steel</td></tr>
-                <tr><td>Backstop</td><td>Step-through</td><td>Step-through</td><td>Single Piece</td><td>N/A</td></tr>
-                <tr><td>Mounting</td><td>“Universal” Skid Steer</td><td>“Universal” Skid Steer</td><td>“Universal” Skid Steer</td><td>Mgt. Brt. Available*</td></tr>
-                <tr><td>Fork Spacing (in/mm)</td><td>6 – 40 / 152 – 1016</td><td>6 – 40 / 152 – 1016</td><td>6 – 40 / 152 – 1016</td><td>6 – 40 / 152 – 1016</td></tr>
-                <tr><td>Fork Carrier</td><td>Notched Rail</td><td>Notched Rail</td><td>Notched Rail</td><td>Notched Rail</td></tr>
-                <tr><td>Forks</td><td>1.375” x 4” w/Latch</td><td>1.375” x 4” w/Latch</td><td>1.375” x 4” w/Latch</td><td>1.375” x 4” w/Latch</td></tr>
-                <tr><td>Fork Length (in/mm)</td><td>42 / 1067</td><td>48 / 1219</td><td>48 / 1219</td><td>48 / 1219</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>345 / 157</td><td>359 / 163</td><td>315/143</td><td>438 / 199</td></tr>
-                <tr><td>Product No.</td><td>832400</td><td>832415</td><td>831370</td><td>831830</td></tr>
-                <tr>
-                    <td><a href="/images/manuals/JDSSGLPF.pdf">Download Manual Here</a></td>
-                    <td><a href="/pallet-forks-sspf-442">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-sspf-448">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-sspf-3748">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-pf-448">Parts List Click Here</a></td>
-                </tr>
-                <tr><td colspan="5">*Optional interfacing quick attach brackets available for most popular loaders.</td></tr>
-            </tbody>
-        </table>
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q Manual Adjust, 3-pt. Hydraulic Post Drivers</strong></a></h3>
+        The HPD-16/22Q models with manual adjust quickly deliver an air tight post set for wood or steel posts. Base model offers manual adjustment cranks and bubble level for prefect vertical alignment on flatland or hillsides.
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header"><td colspan="4" align="center">John Deere Integrated Frame Pallet Forks</td></tr>
-                <tr><td>Model</td><td>JDPF-5442</td><td>JDPF-5448</td><td>JDPF-6748</td></tr>
-                <tr><td>Equipment Used On </td><td>D Front Loader</td><td>JD Front Loader</td><td>JD Front Loader</td></tr>
-                <tr><td>Capacity (lbs/kg)</td><td>4,000 / 1,814</td><td>4,000 / 1,814</td><td>5,500 / 2,495</td></tr>
-                <tr><td>Frame Construction</td><td>Welded Steel</td><td>Welded Steel</td><td>Welded Steel</td></tr>
-                <tr><td>Mounting</td><td>400 / 500 Series JD</td><td>400 / 500 Series</td><td>JD 600 / 700 Series JD</td></tr>
-                <tr><td>Fork Spacing (in/mm)</td><td>6 – 40 / 152 – 1016</td><td>6 – 40 / 152 – 1016</td><td>6 – 40 / 152 – 1016</td></tr>
-                <tr><td>Fork Carrier</td><td>Notched Rail</td><td>Notched Rail</td><td>Notched Rail</td></tr>
-                <tr><td>Forks</td><td>1.375” x 4” w/Latch</td><td>1.375” x 4” w/Latch</td><td>1.57” x 4” w/Latch</td></tr>
-                <tr><td>Fork Length (in/mm)</td><td>42 / 1067</td><td>48 / 1219</td><td>48 / 1219</td></tr>
-                <tr><td>Weight (lbs/kg)</td><td>345 / 157</td><td>359 / 163</td><td>373 / 169</td></tr>
-                <tr><td>Product No.</td><td>832420</td><td>832435</td><td>832440</td></tr>
-                <tr>
-                    <td><a href="/images/manuals/JDSSGLPF.pdf">Download Manual Here</a></td>
-                    <td><a href="/pallet-forks-jdpf-5442">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-jdpf-5448">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-jdpf-6748">Parts List Click Here</a></td>
-                </tr>
-                <tr><td colspan="4">*Optional interfacing quick attach brackets available for most popular loaders.</td></tr>
-            </tbody>
-        </table>
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q/26Q SHC Hydraulic Adjust, 3-pt. Hydraulic Post Drivers</strong></a></h3>
+        Designed and engineered for those who have serious fencing to do. These center mounted units offer all the quality features of the basic HPD-16 & HPD22Q plus effortless hydraulic driver alignment controlled by two 3" x 6" hydraulic cylinders.
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table class="table table-bordered table-responsive table-striped">
-            <tbody>
-                <tr class="table-col-header"><td colspan="4" align="center">Global (Euro Hook Style) Integrated Frame Pallet Forks</td></tr>
-                <tr><td>Model</td><td>GLPF-442</td><td>GLPF-448</td><td>GLPF-5548</td></tr>
-                <tr><td>Equipment Used On</td><td>Tractor Front Loader</td><td>Tractor Front Loader</td><td>Tractor Front Loader</td></tr>
-                <tr><td>Capacity (lbs/kg)</td><td>4,000 / 1,814</td><td>4,000 / 1,814</td><td>5,500 / 2,495</td></tr>
-                <tr><td>Frame Construction</td><td>Welded Steel</td><td>Welded Steel</td><td>Welded Steel</td></tr>
-                <tr><td>Mounting</td><td>Euro / Global Carrier</td><td>Euro / Global Carrier</td><td>Euro / Global Carrier</td></tr>
-                <tr><td>Fork Spacing</td><td>6” to 40”</td><td>6” to 40”</td><td>6” to 40”</td></tr>
-                <tr><td>Fork Carrier</td><td>Notched Rail</td><td>Notched Rail</td><td>Notched Rail</td></tr>
-                <tr><td>Forks</td><td>1.375” x 4” w/Latch</td><td>1.375” x 4” w/Latch</td><td>1.57” x 4” w/Latch</td></tr>
-                <tr><td>Fork Length (in/mm)</td><td>42 / 1067</td><td>48 / 1219</td><td>48 / 1219</td></tr>
-                <tr><td>Weight (lbs)</td><td>345 / 157</td><td>359 / 163</td><td>373 / 169</td></tr>
-                <tr><td>Product No.</td><td>832452</td><td>832458</td><td>832460</td></tr>
-                <tr>
-                    <td><a href="/images/manuals/JDSSGLPF.pdf">Download Manual Here</a></td>
-                    < <td><a href="/pallet-forks-glpf-442">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-glpf-448">Parts List Click Here</a></td>
-                    <td><a href="/pallet-forks-glpf-5548">Parts List Click Here</a></td>
-                <tr><td colspan="4">*Optional interfacing quick attach brackets available for most popular loaders.</td></tr>
-                </tr>
-            </tbody>
-        </table>
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q/26Q SWO Offset 3-pt. Hydraulic Post Drivers</strong></a></h3>
+        Driving posts along existing fence lines, ditch banks, or corners is easily done with a SWO model. Heavy-duty 3-angle swivel mount pivots the driver 45°, 90° or rear facing for increased versatility.
     </div>
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">    <div><strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer is properly counterbalanced. Damage to equipment or personal injury could occur.</div>
-    <p>&nbsp;</p>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q HFL Front Loader Mounted. Hydraulic Post Drivers</strong></a></h3>
+        Front loader mounted hydraulic adjust post drivers offer increased visibility and maneuverability for improved access in hard-to-reach areas. Available as either a HPD-16 or HPD-22Q, these drivers can be positioned to the center or side of the loader for easier fence building.
+    </div>
 </div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q MSS/HSS, HPD-26Q HSS Skid Steer Mounted, Hydraulic Post Drivers</strong></a></h3>
+        For greater multi-task performance from your skid steer, connect it to a hydraulic skid steer mounted post driver. The HPD-16 & HPD-22 are offered either with manual crank adjustment (MSS) or hydraulic adjustment (HSS) for quicker productivity.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/"><img class="img-responsive" src="/images/fence-equipment/" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><a href="/"><strong>HPD-16/22Q/26Q HSS/P Self-contained, Skid Steer Mounted, Hydraulic Post Drivers</strong></a></h3>
+        Save time by simplifying driver installation and removal with a self-contained hydraulic post driver. Features include a hydraulic pump, motor and reservoir. No special return line fittings are required.
+    </div>
+</div>
+
+
+<div>
+    <i class="fa fa-lg fa-exclamation-triangle"> </i>
+    <strong>WARNING:</strong> Read and understand the operator’s manual before operation. Be sure tractor/skid steer
+    is properly counterbalanced. Damage to equipment or personal injury could occur.
+</div>
+<br />
