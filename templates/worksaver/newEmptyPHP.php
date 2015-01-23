385473fff44680719cf053190e04d14112ab5e40
jbake11 <jeffbake40@gmail.com>
2015-01-23 10:56:38 -0500

    qc

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -1,21 +1,22 @@
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <img class="img-responsive" src="/images/fence-equipment/QuickChange.jpg" alt="" />
+    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
+        <img class="qc-sys-img img-responsive" src="/images/fence-equipment/QuickChange.jpg" alt="" />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
+        Worksaver is introducing the QuickChange system featuring a new End Cap (Splitcap) for our HPD-series of Post Drivers. This new SplitCap will allow the end user to easily replace the rod seal in the field.
+        The QuickChange system also involves a change to the cylinder rod and attaching hardware. The serial number break and parts information is listed below.
         <br /><br />
         <a href="/images/fence-equipment/HPD-20quick-20change.pdf">Download Instructions to change cyl. rod seal</a>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        Worksaver is introducing the QuickChange system featuring a new End Cap (Splitcap) for our HPD-series of Post Drivers. This new SplitCap will allow the end user to easily replace the rod seal in the field.
-        The QuickChange system also involves a change to the cylinder rod and attaching hardware. The serial number break and parts information is listed below.
-
-    </div>
+</div>
 </div>
 <div class="row">
     <div class="col-lg-12 col-lg-12 col-sm-12 col-xs-12">
         <table class="table table-bordered table-responsive table-striped">
             <tbody>
                 <tr><td class="table-col-header" colspan="2" align="center">HPD-16 Effective with S/N 4077</td></tr>
-
                 <tr><td class="table-col-modelnum">Description</td><td>New Item Number</td></tr>
                 <tr><td>Cylinder Rod (now 1-inch dia.)</td><td>815166</td></tr>
                 <tr><td>End Cap w/Rod Seal</td><td>815170</td></tr>
