d3decec5f2df28143aac521937d94dd370b6db55
jbake11 <jeffbake40@gmail.com>
2015-01-26 13:00:18 -0500

    asd

--- templates/worksaver/newEmptyPHP.php
+++ templates/worksaver/newEmptyPHP.php
@@ -5,17 +5,66 @@
 </div>
 <div class="row">
     <div class="col-lg-4 col-md-4"><a href="/">
-            <img class="img-responsive" src="/images/" alt="" /></a>
+            <a href="/3-pt-pto-driven-seeder-spreader"><img class="img-responsive" src="/images/seeders/cs694.gif" alt="" /></a>
     </div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong></strong></a></h3>
+        <h3><a href="/3-pt-pto-driven-seeder-spreader"><strong>3-PT. PTO Driven Seeder/Spreader</strong></a></h3>
+        The Worksaver models CS-694, CS-1094, and CSP-1094 are built for the farmer who requires a quality, economical broadcast seeder/spreader. It easily handles all grass seed, wheat, oats, and fertilizers producing a uniform spread pattern. These models offer three spread contols with choice spreads of: Left side only, Right side only, or Full 180° spread by simply moving a lever.
     </div>
 </div>
 <div class="row">
     <div class="col-lg-4 col-md-4"><a href="/">
-            <img class="img-responsive" src="/images/" alt="" /></a>
+            <a href="/electric-driven-seeder-spreader"><img class="img-responsive" src="/images/seeders" alt="" /></a>
     </div>
     <div class="col-lg-8 col-md-8">
-        <h3><a href="/"><strong></strong></a></h3>
+        <h3><a href="/electric-driven-seeder-spreader"><strong>Electric Driven Seeder/Spreader</strong></a></h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"><a href="/atv-attachments-atvk-400"><img class="img-responsive" src="/images/atv-attachments/ATVK-400.jpg" alt="" /></a></div>
+    <div class="col-lg-8 col-md-8">
+        <h3><strong><a href="/atv-attachments-atvk-400">ATVK-400 Electric Seeder/Spreader</a><br /></strong></h3>
+        Worksaver’s model ATVK-400 helps get a head start early in the season. Excellent for seeding pastures and wildlife food plots. Features a lower profile mount that allows the unit to fit most all brands of ATV’s. Durable, maintenance-free 1-bushel polyethylene hopper is translucent for easy monitoring of hopper material level.
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h3>Copyright © 1996-2011 Worksaver, Inc. All Rights Reserved.</h3>
+
+        Worksaver retains all copyrights in any text or graphic images owned by Worksaver
+        and hereby authorizes you to electronically copy documents published herein solely
+        for th purpose of transmitting or viewing the information. Any file or image rebroadcast
+        or mirroring, modification, or otherwise alter any files or images, or print the information
+        contained herein, without written permission from Worksaver is strictly prohibited.
+
+        Worksaver, Terra-Max, Hugger, Grabber, and Claw names and logos are registered trademarks.
+        Stack-Max registration is pending. Microsoft, Internet Explorer, and Netscape are registered
+        logos and trademarks with respect to their companies. Worksaver has permission to provide
+        customer access to their respective web sites to obtain a current software version to better
+        view Worksaver web pages. All logos are registered trademarks of their respective companies.
+        They are offered strictly to provide access to their web sites for agricultural information.
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h3>Legal Notices</h3>
+
+        The information is this publication, including text, images, and links, are provides
+        "as is" by Worksaver, Inc. solely as a convenience to customers, dealers, and distributors
+        without warranty of any kind, either expressed or implied, including but not limited to,
+        the implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
+        Worksaver, Inc. assumes no responsibility for errors or omissions in this publication or other
+        documents which are referenced by or linked to this publication. This publication could include
+        technical or other inaccuracies, and not all products or services referenced herein are available in all areas. Changes are periodically added to the information, and Worksaver may change products or services described in this publication at any time.
+
+        Should you choose to respond directly to Worksaver with any information including comments, questions,
+        suggestions, ideas, etc. relating to this web site or Worksaver products, and services, you agree that
+        such information shall be deemed as non-confidential and Worksaver shall have no obligation to respond
+        and be free to reproduce, use, disclose, and distribute the information to others without limitation,
+        including but not limited to developing, manufacturing, and marketing products incorporating such information.
+
     </div>
 </div>
