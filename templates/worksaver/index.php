8f00c9e53e07faa10f8e71108a2580456869fbf8
jbake11 <jeffbake40@gmail.com>
2015-01-28 13:32:32 -0500

    copyright adjust

--- templates/worksaver/index.php
+++ templates/worksaver/index.php
@@ -178,10 +178,12 @@
                         <?php if ($this->countModules('mainmenu')) : ?>
                             <div class="container">
                                 <div class="row">
-                                    <div class="col-lg-12">
+                                    <div class="col-lg-7 col-md-7 col-sm-12">
                                         <nav class="navigation" role="navigation">
                                             <jdoc:include type="modules" name="footermenu" style="none" />
                                         </nav>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12">
                                         <div class="copyright-text" style="float: right;">
                                             Copyright &copy; <?php echo date('Y'); ?> Worksaver, Inc. All Rights Reserved.
                                         </div>
