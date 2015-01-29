0602bc0752aa8702ab5a64da2f638b6daf946c9f
Chris Bake <cbake7806@gmail.com>
2015-01-29 02:23:42 -0500

    fwfe f#	deleted:    images/manuals/foh5-ws.pdf fefe#	deleted:    images/manuals/lsf20-30.pdf

--- locator/parse.php
+++ locator/parse.php
@@ -6,14 +6,53 @@
  * and open the template in the editor.
  */
 
+$files = array();
+
 $dom = new DOMDocument;
-$dom->loadHTML(file_get_contents('mans.html'));
+$dom->loadHTML(file_get_contents('../../locator/mans.html'));
 foreach ($dom->getElementsByTagName('a') as $node) {
     $filename = basename($node->getAttribute("href"));
     $filename = str_replace("%20", " ", $filename);
 
-    if (stristr($filename, "#") === FALSE && !file_exists('../images/manuals/' . $filename)):
-        echo 'http://www.worksaver.com/Manuals/' . $filename . ' -o ../images/manuals/' . $filename . "\n";
-        shell_exec('curl "http://www.worksaver.com/Manuals/' . $filename . '" -o "../images/manuals/' . $filename . '"');
+    if (stristr($filename, "#") === FALSE && stristr($node->getAttribute("href"), "literature") === FALSE && !file_exists('./' . $filename)):
+
+        if (in_array($filename, $files))
+            continue;
+
+        $files[] = $filename;
+
+        $ch = curl_init();
+        $source = "http://www.worksaver.com/Manuals/" . $filename;
+        echo $source . "\n";
+
+        $header = array(
+            'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: en-us,en;q=0.5',
+            'Accept-Encoding: gzip,deflate',
+            'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+            'Keep-Alive: 115',
+            'Connection: keep-alive',
+        );
+
+
+
+        curl_setopt($ch, CURLOPT_URL, urlencode($source));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        $data = curl_exec($ch);
+        var_dump($data);
+        curl_close($ch);
+//        $filename = str_replace(" ", "%20", $filename);
+        $destination = $filename;
+        $file = fopen($destination, "w+");
+        fputs($file, $data);
+        fclose($file);
+
+
+        sleep(1);
     endif;
 }
